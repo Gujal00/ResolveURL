0e01425a89c6b127a152090fe21fd11fafdf6147
jsergio123 <12888802+jsergio123@users.noreply.github.com>
2018-08-26 15:24:21 -0400

    Update powvideo.py

--- lib/resolveurl/plugins/powvideo.py
+++ lib/resolveurl/plugins/powvideo.py
@@ -23,8 +23,8 @@ from resolveurl.resolver import ResolveUrl, ResolverError
 
 class PowVideoResolver(ResolveUrl):
     name = "powvideo"
-    domains = ["powvideo.net"]
-    pattern = '(?://|\.)(powvideo.net)/(?:embed-|iframe-|preview-)?([0-9a-zA-Z]+)'
+    domains = ["powvideo.net", "powvideo.cc"]
+    pattern = '(?://|\.)(powvideo\.(?:net|cc))/(?:embed-|iframe-|preview-)?([0-9a-zA-Z]+)'
 
     def __init__(self):
         self.net = common.Net()
