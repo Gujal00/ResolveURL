ae7ecb64737e0396f7bc0638da4bb5ca2c6cc0ce
jsergio123 <12888802+jsergio123@users.noreply.github.com>
2018-08-26 14:39:04 -0400

    Update estream.py

--- lib/resolveurl/plugins/estream.py
+++ lib/resolveurl/plugins/estream.py
@@ -18,12 +18,17 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
-from __resolve_generic__ import ResolveGeneric
+from lib import helpers
+from resolveurl.resolver import ResolveUrl, ResolverError
 
-class EstreamResolver(ResolveGeneric):
+
+class EstreamResolver(ResolveUrl):
     name = "estream"
     domains = ['estream.to', 'estream.nu', 'estream.xyz']
     pattern = '(?://|\.)(estream\.(?:to|nu|xyz))/(?:embed-)?([a-zA-Z0-9]+)'
     
+    def get_media_url(self, host, media_id):
+        return helpers.get_media_url(self.get_url(host, media_id), result_blacklist=['.m3u8']).replace(' ', '%20')
+        
     def get_url(self, host, media_id):
         return self._default_get_url(host, media_id, template='https://estream.to/embed-{media_id}.html')
