40952ffd78a3b203db40c8e86ea311bd036589b2
jsergio123 <12888802+jsergio123@users.noreply.github.com>
2018-09-15 18:28:52 -0400

    update nxload.py

--- lib/resolveurl/plugins/nxload.py
+++ lib/resolveurl/plugins/nxload.py
@@ -18,9 +18,33 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
-from __resolve_generic__ import ResolveGeneric
+import re
+from lib import helpers
+from resolveurl import common
+from resolveurl.resolver import ResolveUrl, ResolverError
 
-class NxloadResolver(ResolveGeneric):
+
+class NxloadResolver(ResolveUrl):
     name = "nxload"
     domains = ["nxload.com"]
     pattern = '(?://|\.)(nxload\.com)/(?:embed-)?([0-9a-zA-Z]+)'
+
+    def __init__(self):
+        self.net = common.Net()
+
+    def get_media_url(self, host, media_id):
+        web_url = self.get_url(host, media_id)
+        headers = {'User-Agent': common.RAND_UA}
+        html = self.net.http_GET(web_url, headers=headers).content
+
+        if html:
+            match = re.search('''['"]?sources['"]?\s*:\s*\[(.*?)\]''', html, re.DOTALL)
+            if match:
+                sources = [(source.rsplit('/', 1).pop(1), source) for source in
+                           re.findall('''['"](.*?)["']''', match.group(1), re.DOTALL)]
+                return helpers.pick_source(sources) + helpers.append_headers(headers)
+
+        raise ResolverError("Video not found")
+
+    def get_url(self, host, media_id):
+        return self._default_get_url(host, media_id)
