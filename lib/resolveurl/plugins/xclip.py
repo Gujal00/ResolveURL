dac9b6691deb5600850500655eaa68c174ef1346
Gujal <gujal@protonmail.com>
2018-10-04 14:22:40 +1300

    simplified using helper function

--- lib/resolveurl/plugins/xclip.py
+++ lib/resolveurl/plugins/xclip.py
@@ -30,18 +30,7 @@ class xClipResolver(ResolveUrl):
         self.net = common.Net()
 
     def get_media_url(self, host, media_id):
-        web_url = self.get_url(host, media_id)
-        headers = {'User-Agent': common.RAND_UA,
-                   'Referer': web_url}
-        html = self.net.http_GET(web_url, headers=headers).content
-        try:
-            js = jsunpack.unpack(html)
-            sources = helpers.scrape_sources(js, patterns=['''file:\s*"(?P<url>[^"]+)",\n?\s*label:\s*"(?P<label>[^"]+)'''])
-            if sources:
-                return helpers.pick_source(sources) + helpers.append_headers(headers)
-        except ValueError:
-            raise ResolverError('Video not found')
-
+        return helpers.get_media_url(self.get_url(host, media_id), patterns=['''file:\s*"(?P<url>[^"]+)",\n?\s*label:\s*"(?P<label>[^"]+)'''])
 
     def get_url(self, host, media_id):
         return self._default_get_url(host, media_id, template='https://{host}/{media_id}.html')
