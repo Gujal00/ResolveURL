41b9b3d7579dce73b22b70b25e5b45644834c2a6
jsergio123 <12888802+jsergio123@users.noreply.github.com>
2018-10-17 18:46:52 -0400

    Create vkprime.py

---
+++ lib/resolveurl/plugins/vkprime.py
@@ -0,0 +1,23 @@
+'''
+    Plugin for ResolveURL
+    Copyright (C) 2018
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+'''
+from __resolve_generic__ import ResolveGeneric
+
+class VkPrimeResolver(ResolveGeneric):
+    name = 'vkprime'
+    domains = ["vkprime.com"]
+    pattern = '(?://|\.)(vkprime\.com)/(?:embed-)?([a-zA-Z0-9]+)'
