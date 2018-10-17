ecf4c4316f69cb62c3133bf995992bed94e76f7c
jsergio123 <12888802+jsergio123@users.noreply.github.com>
2018-10-17 18:40:11 -0400

    Update gounlimited.py

--- lib/resolveurl/plugins/gounlimited.py
+++ lib/resolveurl/plugins/gounlimited.py
@@ -18,10 +18,33 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
-from __resolve_generic__ import ResolveGeneric
+
+import re
+from lib import helpers
+from resolveurl import common
+from resolveurl.resolver import ResolveUrl, ResolverError
 
 
-class GoUnlimitedResolver(ResolveGeneric):
+class GoUnlimitedResolver(ResolveUrl):
     name = "gounlimited.to"
     domains = ['gounlimited.to']
     pattern = '(?://|\.)(gounlimited\.to)/(?:embed-)?([0-9a-zA-Z]+)'
+    
+    def __init__(self):
+        self.net = common.Net()
+        
+    def get_media_url(self, host, media_id):
+        web_url = self.get_url(host, media_id)
+        headers = {'User-Agent': common.RAND_UA}
+        html = self.net.http_GET(web_url, headers=headers).content
+        
+        if html:
+            html += helpers.get_packed_data(html)
+            source = re.search('''sources\s*:\s*\["([^"]+)''', html)
+            if source:
+                return source.group(1) + helpers.append_headers(headers)
+                
+        raise ResolverError("Video not found")
+        
+    def get_url(self, host, media_id):
+        return self._default_get_url(host, media_id, template='https://{host}/embed-{media_id}.html')
