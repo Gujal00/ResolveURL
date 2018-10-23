4e9c8495fe298d9145f53ac3d59b2747636b6522
jsergio123 <12888802+jsergio123@users.noreply.github.com>
2018-10-22 20:44:43 -0400

    Update lib/resolveurl/plugins/tvlogy.py

--- lib/resolveurl/plugins/tvlogy.py
+++ lib/resolveurl/plugins/tvlogy.py
@@ -15,7 +15,7 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 '''
-
+import re
 from lib import helpers
 from resolveurl import common
 from resolveurl.resolver import ResolveUrl, ResolverError
@@ -24,7 +24,7 @@ from resolveurl.resolver import ResolveUrl, ResolverError
 class TVLogyResolver(ResolveUrl):
     name = "tvlogy.to"
     domains = ["tvlogy.to"]
-    pattern = '(?://|\.)(tvlogy\.to)/(?:watch\.php\?v=)?([0-9a-zA-Z]+)'
+    pattern = '(?://|\.)(tvlogy\.to)/(?:embed/|watch\.php\?v=)?([0-9a-zA-Z]+)'
 
     def __init__(self):
         self.net = common.Net()
@@ -32,19 +32,27 @@ class TVLogyResolver(ResolveUrl):
     def get_media_url(self, host, media_id):
         web_url = self.get_url(host, media_id)
         headers = {'User-Agent': common.FF_USER_AGENT}
-        response = self.net.http_GET(web_url, headers=headers)
-        html = response.content
+        html = self.net.http_GET(web_url, headers=headers).content
+
         if 'Not Found' in html:
             raise ResolverError('File Removed')
 
         if 'Video is processing' in html:
             raise ResolverError('File still being processed')
 
-        sources = helpers.scrape_sources(html, patterns=[',\s*src\s*:\s*"(?P<url>[^"]+)'])
-        strurl = helpers.pick_source(sources)
-        if 'dailymotion.com' in strurl:
-            strurl = 'plugin://plugin.video.f4mTester/?streamtype=HLS&url=' + strurl
-        return strurl
+        packed = re.search("JuicyCodes\.Run\((.+?)\)", html, re.I)
+        if packed:
+            from base64 import b64decode
+            packed = packed.group(1).replace('"', '').replace('+', '')
+            packed = b64decode(packed)
+            html += '%s</script>' % packed.strip()
+
+        sources = helpers.scrape_sources(html)
+        if sources:
+            headers.update({'Referer': web_url, 'Range': 'bytes=0-'})
+            return helpers.pick_source(sources) + helpers.append_headers(headers)
+
+        raise ResolverError('Video not found')
 
     def get_url(self, host, media_id):
-        return self._default_get_url(host, media_id, template='http://{host}/watch.php?v={media_id}')
+        return self._default_get_url(host, media_id, template='https://{host}/embed/{media_id}')
