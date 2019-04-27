7c3807b22f3642ac2cfda19f38a0e3c8b8e13c9e
jsergio123 <12888802+jsergio123@users.noreply.github.com>
2019-04-27 18:23:43 -0400

    Update vidlox.py

--- lib/resolveurl/plugins/vidlox.py
+++ lib/resolveurl/plugins/vidlox.py
@@ -34,18 +34,18 @@ class VidloxResolver(ResolveUrl):
 
     def get_media_url(self, host, media_id):
         web_url = self.get_url(host, media_id)
-        headers = {'User-Agent': common.FF_USER_AGENT, 'Referer': web_url}
+        headers = {'User-Agent': common.FF_USER_AGENT}
         html = self.net.http_GET(web_url, headers=headers).content
 
         if html:
             _srcs = re.search(r'sources\s*:\s*\[(.+?)\]', html)
             if _srcs:
-                srcs = helpers.scrape_sources(_srcs.group(1), patterns=['''["'](?P<url>http[^"']+)'''],
-                                              result_blacklist=['.m3u8'])
+                srcs = helpers.scrape_sources(_srcs.group(1), patterns=['''["'](?P<url>http[^"']+)'''], result_blacklist=['.m3u8'])
                 if srcs:
+                    headers.update({'Referer': web_url})
                     return helpers.pick_source(srcs) + helpers.append_headers(headers)
 
         raise ResolverError('Unable to locate link')
 
     def get_url(self, host, media_id):
-        return self._default_get_url(host, media_id)
+        return self._default_get_url(host, media_id, template='https://{host}/embed-{media_id}.html')
