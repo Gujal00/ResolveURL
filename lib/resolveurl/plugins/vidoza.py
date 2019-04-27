f826824e36907211cb7addf67ea0dabf7044ab2e
jsergio123 <12888802+jsergio123@users.noreply.github.com>
2019-04-27 18:33:15 -0400

    Update vidoza.py

--- lib/resolveurl/plugins/vidoza.py
+++ lib/resolveurl/plugins/vidoza.py
@@ -15,9 +15,17 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
-from __resolve_generic__ import ResolveGeneric
+from lib import helpers
+from resolveurl.resolver import ResolveUrl, ResolverError
 
-class VidozaResolver(ResolveGeneric):
+
+class VidozaResolver(ResolveUrl):
     name = 'vidoza'
     domains = ['vidoza.net']
     pattern = '(?://|\.)(vidoza\.net)/(?:embed-)?([0-9a-zA-Z]+)'
+
+    def get_media_url(self, host, media_id):
+        return helpers.get_media_url(self.get_url(host, media_id), patterns=['''["']?\s*(?:file|src)\s*["']?\s*[:=,]?\s*["'](?P<url>[^"']+)(?:[^}>\]]+)["']?\s*res\s*["']?\s*[:=]\s*["']?(?P<label>[^"',]+)'''], generic_patterns=False).replace(' ', '%20')
+
+    def get_url(self, host, media_id):
+        return self._default_get_url(host, media_id, template='https://{host}/embed-{media_id}.html')
