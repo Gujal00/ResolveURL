4f43e5136e942228df6e9716548a7168be8e5dd9
jsergio123 <12888802+jsergio123@users.noreply.github.com>
2019-05-16 18:47:53 -0400

    Update vidtodo.py

--- lib/resolveurl/plugins/vidtodo.py
+++ lib/resolveurl/plugins/vidtodo.py
@@ -17,14 +17,14 @@
 # Old  vidtodo.me  vidotodo.com
 
 from urlparse import urlparse
- 
 from lib import helpers
 from resolveurl.common import Net, RAND_UA
 from resolveurl.resolver import ResolveUrl, ResolverError
- 
+
+
 class VidToDoResolver(ResolveUrl):
     name = 'Vidtodo'
-    domains = ['vidtodoo.com','vidtodo.com']
+    domains = ['vidtodoo.com', 'vidtodo.com']
     pattern = '(?://|\.)((?:vidtodoo|vidtodo)\.(?:com))/(?:embed-)?([0-9a-zA-Z]+)'
  
     def __init__(self):
@@ -37,34 +37,32 @@ class VidToDoResolver(ResolveUrl):
             'Upgrade-Insecure-Requests': '1',
             'DNT': '1'
         }
-        
- 
+
     def get_media_url(self, host, media_id):
         web_url = self.get_url(host, media_id)
         r = self.net.http_GET(web_url, headers=self.desktopHeaders)
-        
-        if r._response.code == 200:
-            sources = helpers.scrape_sources(
-                r.content,
-                generic_patterns = False,
-                patterns = [
-                    '''sources.*?\[['"](?P<url>.*?)['"]''',
-                    '''sources.*?file:.*?['"](?P<url>.*?)['"](?:.*?label.*?['"](?P<label>.*?)['"])?''',
-                    '''updateSrc.*?src:.*?['"](?P<url>.*?)['"](?:.*?label.*?['"](?P<label>.*?)['"])?'''
-                ]
-            )
-            if sources:
-                # Headers for requesting media (copied from Firefox).
-                parsedUrl = urlparse(r.get_url())
-                kodiHeaders = {
-                    'User-Agent': self.userAgent,
-                    'Accept': 'video/webm,video/ogg,video/*;q=0.9,application/ogg;q=0.7,audio/*;q=0.6,*/*;q=0.5',
-                    'Referer': '%s://%s/' % (parsedUrl.scheme, parsedUrl.netloc),
-                    'Cookie': '; '.join(cookie.name + '=' + cookie.value for cookie in self.net._cj)
-                }
-                return helpers.pick_source(sources) + helpers.append_headers(kodiHeaders)
+
+        sources = helpers.scrape_sources(
+            r.content,
+            generic_patterns=False,
+            patterns=[
+                '''sources.*?\[['"](?P<url>.*?)['"]''',
+                '''sources.*?file:.*?['"](?P<url>.*?)['"](?:.*?label.*?['"](?P<label>.*?)['"])?''',
+                '''updateSrc.*?src:.*?['"](?P<url>.*?)['"](?:.*?label.*?['"](?P<label>.*?)['"])?'''
+            ]
+        )
+        if sources:
+            # Headers for requesting media (copied from Firefox).
+            parsedUrl = urlparse(r.get_url())
+            kodiHeaders = {
+                'User-Agent': self.userAgent,
+                'Accept': 'video/webm,video/ogg,video/*;q=0.9,application/ogg;q=0.7,audio/*;q=0.6,*/*;q=0.5',
+                'Referer': '%s://%s/' % (parsedUrl.scheme, parsedUrl.netloc),
+                'Cookie': '; '.join(cookie.name + '=' + cookie.value for cookie in self.net._cj)
+            }
+            return helpers.pick_source(sources) + helpers.append_headers(kodiHeaders)
+
         raise ResolverError('Unable to locate video')
-        
- 
+
     def get_url(self, host, media_id):
         return self._default_get_url(host, media_id, template='https://{host}/{media_id}')
