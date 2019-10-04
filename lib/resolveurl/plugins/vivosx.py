f4b057f0e96978996752e3c465102f05b4c2d51a
anis3 <anis3.n@web.de>
2019-10-04 11:35:04 +0000

    fix vivo.sx

--- lib/resolveurl/plugins/vivosx.py
+++ lib/resolveurl/plugins/vivosx.py
@@ -1,25 +1,32 @@
 '''
 Plugin for ResolveURL
 Copyright (C) 2018 gujal
-
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
-
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 GNU General Public License for more details.
-
 You should have received a copy of the GNU General Public License
 along with this program. If not, see <http://www.gnu.org/licenses/>.
 '''
 
-import re
+import re, urllib
 from lib import helpers
 from resolveurl import common
 from resolveurl.resolver import ResolveUrl, ResolverError
+
+def rot47(s):
+    x = []
+    for i in xrange(len(s)):
+        j = ord(s[i])
+        if j >= 33 and j <= 126:
+            x.append(chr(33 + ((j + 14) % 94)))
+        else:
+            x.append(s[i])
+    return ''.join(x)
 
 class VivosxResolver(ResolveUrl):
     name = "vivosx"
@@ -35,10 +42,10 @@ class VivosxResolver(ResolveUrl):
                    'Referer': web_url}
         html = self.net.http_GET(web_url, headers=headers).content
 
-        r = re.search(r'<video\s*id="player"[^>]+data-stream="([^"]+)', html)
-                
+        r = re.findall('InitializeStream.*?source:\s{0,1}[\'|\"](.*?)[\'|\"],', html, re.S)
+
         if r:
-            return r.group(1).decode('base64') + helpers.append_headers(headers)
+            return rot47(urllib.unquote(r[0])) + helpers.append_headers(headers)
 
         raise ResolverError('Video cannot be located.')
 
