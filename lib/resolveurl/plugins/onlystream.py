773006f6ff93121a01c386a0a44cb453400a08bd
gujal <gujal@protonmail.com>
2019-10-18 18:45:00 +1300

    Update onlystream.py
    Fix resolver

--- lib/resolveurl/plugins/onlystream.py
+++ lib/resolveurl/plugins/onlystream.py
@@ -15,13 +15,35 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
-from __resolve_generic__ import ResolveGeneric
+import re
+from lib import helpers
+from lib import jsunpack
+from resolveurl import common
+from resolveurl.resolver import ResolveUrl, ResolverError
 
 
-class OnlyStreamResolver(ResolveGeneric):
+class OnlyStreamResolver(ResolveUrl):
     name = 'onlystream'
-    domains = ['onlystream.tv', 'onlystream.xyz']
-    pattern = r'(?://|\.)(onlystream\.(?:tv|xyz))/(?:e/)?([0-9a-zA-Z-_/]+)'
+    domains = ['onlystream.tv']
+    pattern = r'(?://|\.)(onlystream\.tv)/(?:e/)?([0-9a-zA-Z-_/]+)'
+
+    def __init__(self):
+        self.net = common.Net()
+
+    def get_media_url(self, host, media_id):
+        web_url = self.get_url(host, media_id)
+        headers = {'User-Agent': common.RAND_UA,
+                   'Referer': web_url}
+        html = self.net.http_GET(web_url, headers=headers).content
+        r = re.search("script'>(eval.*?)</script", html, re.DOTALL)
+        
+        if r:
+            html = jsunpack.unpack(r.group(1))
+            sources = helpers.scrape_sources(html, result_blacklist=['.m3u8'])
+            if sources:
+                return helpers.pick_source(sources) + helpers.append_headers(headers)
+
+        raise ResolverError('Video cannot be located.')
 
     def get_url(self, host, media_id):
-        return self._default_get_url(host, media_id, 'https://{host}/{media_id}')
+        return self._default_get_url(host, media_id, template='https://{host}/e/{media_id}')
