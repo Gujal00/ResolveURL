3740b330b7fe3a39883052e0b85a344984d1eed9
gujal <gujal@protonmail.com>
2019-10-28 16:08:25 +1300

    Update speedwatch.py
    Additional embed path,
    fix scrape pattern

--- lib/resolveurl/plugins/speedwatch.py
+++ lib/resolveurl/plugins/speedwatch.py
@@ -16,12 +16,21 @@ You should have received a copy of the GNU General Public License
 along with this program. If not, see <http://www.gnu.org/licenses/>.
 '''
 
-from __resolve_generic__ import ResolveGeneric
+import re
+from lib import helpers
+from resolveurl import common
+from resolveurl.resolver import ResolveUrl, ResolverError
 
-class SpeedWatchResolver(ResolveGeneric):
+class SpeedWatchResolver(ResolveUrl):
     name = "speedwatch"
     domains = ["speedwatch.io"]
-    pattern = r'(?://|\.)(speedwatch\.io)/plyr/([0-9a-zA-Z]+)'
+    pattern = r'(?://|\.)(speedwatch\.io)/(?:plyr|e)/([0-9a-zA-Z]+)'
+
+    def __init__(self):
+        self.net = common.Net()
+
+    def get_media_url(self, host, media_id):
+        return helpers.get_media_url(self.get_url(host, media_id), patterns=[r'''sources\s*:\s*\["(?P<url>[^"]+)'''], generic_patterns=False)
 
     def get_url(self, host, media_id):
-        return self._default_get_url(host, media_id, template='https://www.{host}/plyr/{media_id}.html')
+        return self._default_get_url(host, media_id, template='https://www.{host}/e/{media_id}.html')
