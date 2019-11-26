e8d5adabae5e8ae4cf2c4ec0519ecaa2eb51fd79
Ajal Gujal <gujal@protonmail.com>
2019-11-26 09:49:47 +0400

    New Resolver StreamZ

---
+++ lib/resolveurl/plugins/streamz.py
@@ -0,0 +1,43 @@
+"""
+    Plugin for ResolveUrl
+    Copyright (C) 2019 gujal
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+from lib import helpers
+from resolveurl import common
+from resolveurl.resolver import ResolveUrl, ResolverError
+
+
+class StreamzResolver(ResolveUrl):
+    name = "streamz"
+    domains = ['streamz.cc']
+    pattern = r'(?://|\.)(streamz\.cc)/([0-9a-zA-Z]+)'
+
+    def __init__(self):
+        self.net = common.Net()
+
+    def get_media_url(self, host, media_id):
+        web_url = self.get_url(host, media_id)
+        headers = {'User-Agent': common.RAND_UA}
+        html = self.net.http_GET(web_url, headers=headers).content
+        if 'File not found, sorry!' not in html:
+            sources = helpers.scrape_sources(html)
+            if sources:
+                return helpers.pick_source(sources) + helpers.append_headers(headers)
+
+        raise ResolverError("Video not found")
+
+    def get_url(self, host, media_id):
+        return self._default_get_url(host, media_id, template='https://{host}/{media_id}')
