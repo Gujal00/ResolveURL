96c2ad882439fc21ef9b145dc7eefb05350fbb35
gujal <gujal@protonmail.com>
2020-01-02 12:47:18 +1300

    fix mixdrop (#250)

--- lib/resolveurl/plugins/mixdrop.py
+++ lib/resolveurl/plugins/mixdrop.py
@@ -15,7 +15,8 @@
 import re
 from resolveurl import common
 from resolveurl.resolver import ResolveUrl, ResolverError
-from lib import jsunpack
+from lib import helpers
+
 
 class MixdropResolver(ResolveUrl):
     name = "mixdrop"
@@ -27,16 +28,19 @@ class MixdropResolver(ResolveUrl):
 
     def get_media_url(self, host, media_id):
         web_url = self.get_url(host, media_id)
-        headers = {'Origin':'https://mixdrop.co','Referer':'https://mixdrop.co/e/' + media_id,'User-Agent': common.RAND_UA}
+        headers = {'Origin': 'https://mixdrop.co',
+                   'Referer': 'https://mixdrop.co/',
+                   'User-Agent': common.RAND_UA}
         html = self.net.http_GET(web_url, headers=headers).content
 
-        try:
-            r = re.search('\s+?(eval\(function\(p,a,c,k,e,d\).+)\s+?', html)
-            r = jsunpack.unpack(r.group(1))
-            r = re.search('vsrc?="([^"]+)', r.replace('\\', ''))
-            return "https:" + r.group(1)
-        except:
-            raise ResolverError("Video not found")
+        if '(p,a,c,k,e,d)' in html:
+            html = helpers.get_packed_data(html)
+        r = re.search(r'vsr[^=]+=\s*"([^"]+)', html)
+        if r:
+            headers = {'User-Agent': common.RAND_UA}
+            return "https:" + r.group(1) + helpers.append_headers(headers)
+
+        raise ResolverError("Video not found")
 
     def get_url(self, host, media_id):
         return self._default_get_url(host, media_id, template='https://{host}/e/{media_id}')
