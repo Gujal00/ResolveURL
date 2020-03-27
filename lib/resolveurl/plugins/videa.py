80069648c175ecdc308a7ad1e76d2040defd6ce8
gujal <gujal@protonmail.com>
2020-03-27 14:48:40 +1300

    Fix Videa resolver

--- lib/resolveurl/plugins/videa.py
+++ lib/resolveurl/plugins/videa.py
@@ -2,7 +2,7 @@
 """
     Kodi resolveurl plugin
     Copyright (C) 2016  alifrezser
-    
+
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
@@ -20,28 +20,31 @@ from lib import helpers
 from resolveurl import common
 from resolveurl.resolver import ResolveUrl, ResolverError
 
+
 class VideaResolver(ResolveUrl):
     name = "videa"
     domains = ["videa.hu", "videakid.hu"]
-    pattern = '(?://|\.)((?:videa|videakid)\.hu)/(?:player/?\?v=|videok/)(?:.*-|)([0-9a-zA-Z]+)'
+    pattern = r'(?://|\.)((?:videa|videakid)\.hu)/(?:player/?\?v=|player/v/|videok/)(?:.*-|)([0-9a-zA-Z]+)'
 
     def __init__(self):
         self.net = common.Net()
 
     def get_media_url(self, host, media_id):
         web_url = self.get_url(host, media_id)
-        headers = {'User-Agent': common.FF_USER_AGENT}
+        headers = {'User-Agent': common.FF_USER_AGENT,
+                   'Referer': 'https://{0}/player?v={1}'.format(host, media_id),
+                   'X-Requested-With': 'XMLHttpRequest'}
         html = self.net.http_GET(web_url, headers=headers).content
+        sources = re.findall(r'video_source\s*name="(?P<label>[^"]+)[^>]+>(?P<url>[^<]+)', html)
 
-        sources = helpers.get_dom(html, 'video_sources')
         if sources:
-            sources = re.findall('name\s*=\s*[\'|"]([^\'"]+).+?streamable.+?>([^<]+)', sources[0])
-            if sources[-1][0].lower() == 'lq': sources = sources[::-1]
-            source = helpers.pick_source(sources)
-            if source.startswith('//'): source = 'http:' + source
-            return source
+            source = helpers.pick_source(helpers.sort_sources_list(sources))
+            source = 'https:' + source if source.startswith('//') else source
+            headers.pop('X-Requested-With')
+            headers.update({'Origin': 'https://{}'.format(host)})
+            return source.replace('&amp;', '&') + helpers.append_headers(headers)
 
         raise ResolverError('Stream not found')
 
     def get_url(self, host, media_id):
-        return self._default_get_url(host, media_id, 'http://{host}/videaplayer_get_xml.php?v={media_id}&start=0&referrer=http://{host}')
+        return self._default_get_url(host, media_id, 'https://{host}/videaplayer_get_xml.php?v={media_id}&start=0&referrer=http://{host}')
