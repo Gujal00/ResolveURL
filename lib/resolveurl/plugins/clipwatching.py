4e2de14057e87fd0403ecca2c36b668c15b32f85
gujal <gujal@protonmail.com>
2020-03-30 15:04:12 +1300

    update clipwatching resolver

--- lib/resolveurl/plugins/clipwatching.py
+++ lib/resolveurl/plugins/clipwatching.py
@@ -15,30 +15,13 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
-import re
-from lib import helpers
-from resolveurl import common
-from resolveurl.resolver import ResolveUrl, ResolverError
+from __resolve_generic__ import ResolveGeneric
 
-class ClipWatchingResolver(ResolveUrl):
+
+class ClipWatchingResolver(ResolveGeneric):
     name = "clipwatching"
     domains = ['clipwatching.com']
     pattern = r'(?://|\.)(clipwatching\.com)/(?:embed-)?(\w+)'
 
-    def __init__(self):
-        self.net = common.Net()
-        
-    def get_media_url(self, host, media_id):
-        web_url = self.get_url(host, media_id)
-        headers = {'User-Agent': common.RAND_UA}
-        html = self.net.http_GET(web_url, headers=headers).content
-        
-        html += helpers.get_packed_data(html)
-        sources = helpers.scrape_sources(html)
-        if sources:
-            return helpers.pick_source(sources) + helpers.append_headers(headers)
-                
-        raise ResolverError("Video not found")
-        
     def get_url(self, host, media_id):
         return self._default_get_url(host, media_id, template='https://{host}/embed-{media_id}.html')
