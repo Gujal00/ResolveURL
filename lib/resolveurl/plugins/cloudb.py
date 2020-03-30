1f3474a3860942b5431e2f8fdf0efcf322763818
gujal <gujal@protonmail.com>
2020-03-30 16:05:55 +1300

    Update CloudB resolver

--- lib/resolveurl/plugins/cloudb.py
+++ lib/resolveurl/plugins/cloudb.py
@@ -15,31 +15,19 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
-import re
-from lib import helpers, jsunpack
-from resolveurl import common
-from resolveurl.resolver import ResolveUrl, ResolverError
+from __resolve_generic__ import ResolveGeneric
+from lib import helpers
 
-class CloudbResolver(ResolveUrl):
+
+class CloudbResolver(ResolveGeneric):
     name = "cloudb"
     domains = ['cloudb.me']
     pattern = r'(?://|\.)(cloudb\.me)/(?:embed-)?(\w+)'
 
-    def __init__(self):
-        self.net = common.Net()
-
     def get_media_url(self, host, media_id):
-
-        web_url = self.get_url(host, media_id)
-        headers = {'User-Agent': common.RAND_UA, 'Referer': web_url}
-        html = self.net.http_GET(web_url, headers=headers).content
-
-        html = jsunpack.unpack(re.search(r"(eval\(function.+\))", html, re.DOTALL).group(1))
-
-        try:
-            return re.search(r'sources:\["(.+?)"\]', html).group(1) + helpers.append_headers(headers)
-        except Exception:
-            raise ResolverError("Video not found")
+        return helpers.get_media_url(self.get_url(host, media_id),
+                                     patterns=[r'''sources:\s*\["(?P<url>[^"]+)'''],
+                                     generic_patterns=False)
 
     def get_url(self, host, media_id):
         return self._default_get_url(host, media_id, template='https://{host}/embed-{media_id}.html')
