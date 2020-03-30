7838e2c00370b638951cb505f7afe6143eacc424
gujal <gujal@protonmail.com>
2020-03-30 17:57:46 +1300

    Cleanup datemule resolver

--- lib/resolveurl/plugins/datemule.py
+++ lib/resolveurl/plugins/datemule.py
@@ -1,6 +1,6 @@
 '''
-    resolveurl Kodi plugin
-    Copyright (C) 2016
+    Plugin for ResolveUrl
+    Copyright (C) 2016 gujal
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -16,16 +16,19 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 '''
 
+from __resolve_generic__ import ResolveGeneric
 from lib import helpers
-from resolveurl.resolver import ResolveUrl, ResolverError
 
-class DateMuleResolver(ResolveUrl):
+
+class DateMuleResolver(ResolveGeneric):
     name = "datemule"
     domains = ["datemule.co", "datemule.com"]
-    pattern = '(?://|\.)(datemule\.(?:co|com))/watch/(?:featured/)?([\w-]+)'
+    pattern = r'(?://|\.)(datemule\.(?:co|com))/watch/(?:featured/)?([\w-]+)'
 
     def get_media_url(self, host, media_id):
-        return helpers.get_media_url(self.get_url(host, media_id), patterns=['''(?:hls|mp4)\s*:\s*\[?['"](?P<url>[^'"]+)''']).replace(' ', '%20')
+        return helpers.get_media_url(self.get_url(host, media_id),
+                                     patterns=[r'''(?:hls|mp4)\s*:\s*\[?['"](?P<url>[^'"]+)'''],
+                                     generic_patterns=False).replace(' ', '%20')
 
     def get_url(self, host, media_id):
         return self._default_get_url(host, media_id, 'https://www.datemule.com/watch/{media_id}')
