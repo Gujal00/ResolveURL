1052a99060611f369b3de0209776c68643f4ae5a
gujal <gujal@protonmail.com>
2020-04-03 01:49:27 +1300

    Fix upstream

--- lib/resolveurl/plugins/upstream.py
+++ lib/resolveurl/plugins/upstream.py
@@ -16,10 +16,27 @@ You should have received a copy of the GNU General Public License
 along with this program. If not, see <http://www.gnu.org/licenses/>.
 """
 
-from __resolve_generic__ import ResolveGeneric
+from lib import helpers
+from resolveurl import common
+from resolveurl.resolver import ResolveUrl, ResolverError
 
 
-class UpStreamResolver(ResolveGeneric):
+class UpStreamResolver(ResolveUrl):
     name = "upstream.to"
     domains = ['upstream.to']
     pattern = r'(?://|\.)(upstream\.to)/(?:embed-)?([0-9a-zA-Z]+)'
+
+    def __init__(self):
+        self.net = common.Net()
+
+    def get_media_url(self, host, media_id):
+        web_url = self.get_url(host, media_id)
+        headers = {'User-Agent': common.FF_USER_AGENT}
+        html = self.net.http_GET(web_url, headers=headers).content
+        sources = helpers.scrape_sources(html)
+        if sources:
+            return helpers.pick_source(helpers.sort_sources_list(sources)) + helpers.append_headers(headers)
+        raise ResolverError('Video not found')
+
+    def get_url(self, host, media_id):
+        return self._default_get_url(host, media_id, template='https://{host}/embed-{media_id}.html')
