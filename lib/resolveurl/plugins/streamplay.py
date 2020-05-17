178a348e1d68b9ea30bab07afb29b44df7369e52
gujal <gujal@protonmail.com>
2020-05-17 12:28:34 +1200

    Add viduplayer resolver

--- lib/resolveurl/plugins/streamplay.py
+++ lib/resolveurl/plugins/streamplay.py
@@ -23,8 +23,8 @@ from resolveurl.resolver import ResolveUrl, ResolverError
 
 class StreamplayResolver(ResolveUrl):
     name = "streamplay"
-    domains = ["streamplay.to", "streamplay.club", "streamplay.top", "streamplay.me"]
-    pattern = '(?://|\.)(streamplay\.(?:to|club|top|me))/(?:embed-|player-)?([0-9a-zA-Z]+)'
+    domains = ["streamplay.to", "streamplay.club", "streamplay.top", "streamplay.me", "streamp1ay.cc"]
+    pattern = r'(?://|\.)(streamp[1l]ay\.(?:to|club|top|me|fun|cc))/(?:embed-|player-)?([0-9a-zA-Z]+)'
 
     def __init__(self):
         self.net = common.Net()
@@ -35,8 +35,8 @@ class StreamplayResolver(ResolveUrl):
         html = self.net.http_GET(web_url, headers=headers).content
 
         if html:
-            var = re.search('var _0x[0-f]+=\[([^;]+)\];', html)
-            sources = helpers.scrape_sources(html, patterns=['''(?P<url>http[^,]+\.(?:mp4|m3u8))'''])
+            var = re.search(r'var _0x[0-f]+=\[([^;]+)\];', html)
+            sources = helpers.scrape_sources(html, patterns=[r'''(?P<url>http[^,]+\.(?:mp4|m3u8))'''])
             if sources and var:
                 headers.update({'Referer': web_url})
                 sources = [(source[0], self.decode_video_url(source[1])) for source in sources]
@@ -46,11 +46,10 @@ class StreamplayResolver(ResolveUrl):
         raise ResolverError('File not found')
 
     def get_url(self, host, media_id):
-        return self._default_get_url(host, media_id, template='https://streamplay.me/player-{media_id}.html')
-        
+        return self._default_get_url(host, media_id, template='https://streamplay.fun/{media_id}')
+
     def decode_video_url(self, url):
         tria = re.compile('[0-9a-z]{40,}', re.IGNORECASE).findall(url)[0]
         gira = tria[::-1]
         x = gira[:2] + gira[3:]
-        
         return re.sub(tria, x, url)
