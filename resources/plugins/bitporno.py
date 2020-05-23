91d9b86286fddbacbe9be9ec86c574b497ca27fc
gujal <gujal@protonmail.com>
2020-05-23 12:16:42 +1200

    bitporno additional embed pattern

--- resources/plugins/bitporno.py
+++ resources/plugins/bitporno.py
@@ -19,7 +19,7 @@ from resolveurl.plugins.__resolve_generic__ import ResolveGeneric
 class BitPornoResolver(ResolveGeneric):
     name = "bitporno"
     domains = ['bitporno.com']
-    pattern = r'(?://|\.)(bitporno\.com)/(?:embed/|\?v=)?([0-9a-zA-Z]+)'
+    pattern = r'(?://|\.)(bitporno\.com)/(?:embed/|v/|\?v=)?([0-9a-zA-Z]+)'
 
     def get_url(self, host, media_id):
         return self._default_get_url(host, media_id, template='https://www.bitporno.com/?v={media_id}')
