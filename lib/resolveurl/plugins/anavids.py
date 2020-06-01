ca3154bc596d3b8743980322bc7c523ea4907f7f
gujal <gujal@protonmail.com>
2020-06-02 10:58:04 +1200

    Fix Anavid Resolver

--- lib/resolveurl/plugins/anavids.py
+++ lib/resolveurl/plugins/anavids.py
@@ -16,20 +16,28 @@ You should have received a copy of the GNU General Public License
 along with this program. If not, see <http://www.gnu.org/licenses/>.
 """
 
-from __resolve_generic__ import ResolveGeneric
-from lib import helpers
+from resolveurl.plugins.lib import helpers
+from resolveurl import common
+from resolveurl.resolver import ResolveUrl, ResolverError
 
 
-class AnaVidsResolver(ResolveGeneric):
+class AnaVidsResolver(ResolveUrl):
     name = "anavids.com"
     domains = ['anavids.com']
     pattern = r'(?://|\.)(anavids\.com)/(?:embed-)?([0-9a-zA-Z]+)'
 
+    def __init__(self):
+        self.net = common.Net()
+
     def get_media_url(self, host, media_id):
-        return helpers.get_media_url(self.get_url(host, media_id),
-                                     patterns=[r'''file:"(?P<url>[^"]+)'''],
-                                     generic_patterns=False,
-                                     result_blacklist=['.mpd'])
+        web_url = self.get_url(host, media_id)
+        headers = {'User-Agent': common.FF_USER_AGENT}
+        html = self.net.http_GET(web_url, headers=headers).content
+        sources = helpers.scrape_sources(html)
+        if sources:
+            headers.update({'verifypeer': 'false'})
+            return helpers.pick_source(sources) + helpers.append_headers(headers)
+        raise ResolverError('Video cannot be located.')
 
     def get_url(self, host, media_id):
         return self._default_get_url(host, media_id, template='https://{host}/embed-{media_id}.html')
