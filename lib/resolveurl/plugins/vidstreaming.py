c4198dcc63e06183d7d57645e7e0f4f933546e0f
gujal <gujal@protonmail.com>
2020-08-16 11:42:34 +1200

    Fix Vidstreaming.IO

--- lib/resolveurl/plugins/vidstreaming.py
+++ lib/resolveurl/plugins/vidstreaming.py
@@ -25,4 +25,4 @@ class VidStreamingResolver(ResolveGeneric):
     pattern = r'(?://|\.)(vidstreaming\.io)/(?:streaming|embed|load)\.php\?id=([a-zA-Z0-9]+)'
 
     def get_url(self, host, media_id):
-        return self._default_get_url(host, media_id, template='https://{host}/streaming.php?id={media_id}')
+        return self._default_get_url(host, media_id, template='https://{host}/ajax.php?id={media_id}')
