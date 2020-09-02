eb41f4d6eda0498d7396b96893dd988b84ae8622
gujal <gujal@protonmail.com>
2020-09-02 18:50:40 +1200

    GoUnlimited, remove unnecessary code

--- lib/resolveurl/plugins/gounlimited.py
+++ lib/resolveurl/plugins/gounlimited.py
@@ -16,18 +16,12 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 from resolveurl.plugins.__resolve_generic__ import ResolveGeneric
-from resolveurl.plugins.lib import helpers
 
 
 class GoUnlimitedResolver(ResolveGeneric):
     name = "gounlimited.to"
     domains = ['gounlimited.to']
     pattern = r'(?://|\.)(gounlimited\.to)/(?:embed-)?([0-9a-zA-Z]+)'
-
-    def get_media_url(self, host, media_id):
-        return helpers.get_media_url(self.get_url(host, media_id),
-                                     patterns=[r'''src:\s*"(?P<url>[^"]+)'''],
-                                     generic_patterns=False)
 
     def get_url(self, host, media_id):
         return self._default_get_url(host, media_id, template='https://{host}/embed-{media_id}.html')
