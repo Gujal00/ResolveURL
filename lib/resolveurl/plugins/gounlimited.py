adec3b92905b21b8e24bde133b455767a1414b60
gujal <gujal@protonmail.com>
2020-09-25 15:54:48 +1200

    Fix Gounlimited

--- lib/resolveurl/plugins/gounlimited.py
+++ lib/resolveurl/plugins/gounlimited.py
@@ -15,6 +15,8 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
+
+from resolveurl.plugins.lib import helpers
 from resolveurl.plugins.__resolve_generic__ import ResolveGeneric
 
 
@@ -22,6 +24,10 @@ class GoUnlimitedResolver(ResolveGeneric):
     name = "gounlimited.to"
     domains = ['gounlimited.to']
     pattern = r'(?://|\.)(gounlimited\.to)/(?:embed-)?([0-9a-zA-Z]+)'
+
+    def get_media_url(self, host, media_id):
+        return helpers.get_media_url(self.get_url(host, media_id),
+                                     referer=False)
 
     def get_url(self, host, media_id):
         return self._default_get_url(host, media_id, template='https://{host}/embed-{media_id}.html')
