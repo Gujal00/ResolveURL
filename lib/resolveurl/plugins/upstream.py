26348a188d82d65b10cb1f024a1aedc90a877bc9
anis3 <anis3.n@web.de>
2020-09-29 22:31:42 +0000

    fix upstream (#378)
    * fix upstream

--- lib/resolveurl/plugins/upstream.py
+++ lib/resolveurl/plugins/upstream.py
@@ -1,6 +1,6 @@
 """
     Plugin for ResolveUrl
-    Copyright (C) 2020 gujal
+    Copyright (C) 2020 gujal, Anis3
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -16,24 +16,20 @@
     along with this program. If not, see <http://www.gnu.org/licenses/>.
 """
 
+from resolveurl.plugins.__resolve_generic__ import ResolveGeneric
 from resolveurl.plugins.lib import helpers
-from resolveurl import common
-from resolveurl.resolver import ResolveUrl, ResolverError
 
 
-class UpStreamResolver(ResolveUrl):
+class UpStreamResolver(ResolveGeneric):
     name = "upstream.to"
     domains = ['upstream.to']
     pattern = r'(?://|\.)(upstream\.to)/(?:embed-)?([0-9a-zA-Z]+)'
 
     def get_media_url(self, host, media_id):
-        web_url = self.get_url(host, media_id)
-        headers = {'User-Agent': common.FF_USER_AGENT}
-        html = self.net.http_GET(web_url, headers=headers).content
-        sources = helpers.scrape_sources(html)
-        if sources:
-            return helpers.pick_source(helpers.sort_sources_list(sources)) + helpers.append_headers(headers)
-        raise ResolverError('Video not found')
+        return helpers.get_media_url(self.get_url(host, media_id),
+                                     patterns=[r'''sources:\s*\[(?:{file:)?\s*"(?P<url>[^"]+)'''],
+                                     generic_patterns=False,
+                                     referer=False)
 
     def get_url(self, host, media_id):
         return self._default_get_url(host, media_id, template='https://{host}/embed-{media_id}.html')
