01953a433344d1ac8b1971713eff01ed05e58f95
anis3 <anis3.n@web.de>
2020-11-22 20:49:48 +0000

    fix mystream (#400)

--- lib/resolveurl/plugins/mystream.py
+++ lib/resolveurl/plugins/mystream.py
@@ -2,17 +2,14 @@
     Plugin for ResolveURL
     Copyright (C) 2019 gujal
     Copyright (C) 2020 eco-plus
-
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
-
     This program is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
-
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
@@ -26,7 +23,7 @@ from resolveurl.resolver import ResolveUrl, ResolverError
 class MystreamResolver(ResolveUrl):
     name = "mystream"
     domains = ['mystream.la', 'mystream.to', 'mstream.xyz', 'mstream.cloud', 'mstream.fun', 'mstream.press']
-    pattern = r'(?://|\.)(my?stream\.(?:la|to|cloud|xyz|fun|press))/(?:external|watch/)?([0-9a-zA-Z_]+)'
+    pattern = r'(?://|\.)(my?stream\.(?:la|to|cloud|xyz|fun|press))/(?:external|embed-|watch/)?([0-9a-zA-Z_]+)'
 
     def get_media_url(self, host, media_id):
         web_url = self.get_url(host, media_id)
@@ -47,7 +44,7 @@ class MystreamResolver(ResolveUrl):
         raise ResolverError('Video Link Not Found')
 
     def get_url(self, host, media_id):
-        return self._default_get_url(host, media_id, 'https://mstream.press/{media_id}')
+        return self._default_get_url(host, media_id, 'https://embed.mystream.to/{media_id}')
 
     def decode(self, data):
         startpos = data.find('"\\""+') + 5
