55829765b7a3a5facdd8ec6367f62a08af95f439
gujal <17394059+Gujal00@users.noreply.github.com>
2020-12-17 12:34:05 +1300

    youdbox fix

--- lib/resolveurl/plugins/youdbox.py
+++ lib/resolveurl/plugins/youdbox.py
@@ -15,13 +15,33 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
-from resolveurl.plugins.__resolve_generic__ import ResolveGeneric
+
+import re
+from resolveurl.plugins.lib import helpers
+from resolveurl import common
+from resolveurl.resolver import ResolveUrl, ResolverError
 
 
-class YouDBoxResolver(ResolveGeneric):
+class YouDBoxResolver(ResolveUrl):
     name = "youdbox"
     domains = ['youdbox.com']
     pattern = r'(?://|\.)(youdbox\.com)/(?:embed-)?(\w+)'
+
+    def get_media_url(self, host, media_id):
+        web_url = self.get_url(host, media_id)
+        headers = {'User-Agent': common.FF_USER_AGENT, 'Referer': web_url}
+        html = self.net.http_GET(web_url).content
+        r = re.search(r"{var\s*[^\s]+\s*=\s*(\[[^\]]+])", html)
+
+        if r:
+            data = eval(r.group(1))
+            data = ''.join(data[::-1])
+            s = re.search(r'source\s*src="([^"]+)', data)
+            if s:
+                url = s.group(1) + helpers.append_headers(headers)
+                return url
+
+        raise ResolverError('No video found')
 
     def get_url(self, host, media_id):
         return self._default_get_url(host, media_id, template='https://{host}/embed-{media_id}.html')
