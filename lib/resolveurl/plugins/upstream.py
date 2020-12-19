031c2c95bf4e06c85218aee81f7a703576527eec
gujal <17394059+Gujal00@users.noreply.github.com>
2020-12-19 14:17:36 +1300

    Fix Upstream Resolver

--- lib/resolveurl/plugins/upstream.py
+++ lib/resolveurl/plugins/upstream.py
@@ -16,20 +16,44 @@
     along with this program. If not, see <http://www.gnu.org/licenses/>.
 """
 
-from resolveurl.plugins.__resolve_generic__ import ResolveGeneric
+import re
 from resolveurl.plugins.lib import helpers
+from resolveurl import common
+from resolveurl.resolver import ResolveUrl, ResolverError
 
 
-class UpStreamResolver(ResolveGeneric):
+class UpStreamResolver(ResolveUrl):
     name = "upstream.to"
     domains = ['upstream.to']
     pattern = r'(?://|\.)(upstream\.to)/(?:embed-)?([0-9a-zA-Z]+)'
 
     def get_media_url(self, host, media_id):
-        return helpers.get_media_url(self.get_url(host, media_id),
-                                     patterns=[r'''sources:\s*\[(?:{file:)?\s*"(?P<url>[^"]+)'''],
-                                     generic_patterns=False,
-                                     referer=False)
+        web_url = self.get_url(host, media_id)
+        headers = {'User-Agent': common.CHROME_USER_AGENT}
+        html = self.net.http_GET(web_url, headers=headers).content
+        headers.update({'Referer': web_url})
+
+        if 'sorry' in html:
+            raise ResolverError("Video Deleted")
+
+        r = re.search(r"redirect_vid\('([^']+)','([^']+)','([^']+)'", html)
+        if r:
+            surl = 'https://{0}/dl?op=download_orig&id={1}&mode={2}&hash={3}'.format(
+                host, r.group(1), r.group(2), r.group(3)
+            )
+            dhtml = self.net.http_GET(surl, headers=headers).content
+            s = re.search('href="([^"]+)">Direct', dhtml)
+            if s:
+                return s.group(1) + helpers.append_headers(headers)
+
+        html += helpers.get_packed_data(html)
+        sources = helpers.scrape_sources(html,
+                                         patterns=[r'''sources:\s*\[(?:{file:)?\s*"(?P<url>[^"]+)'''],
+                                         generic_patterns=False)
+        if sources:
+            return helpers.pick_source(sources) + helpers.append_headers(headers)
+
+        raise ResolverError("Video not found")
 
     def get_url(self, host, media_id):
-        return self._default_get_url(host, media_id, template='https://{host}/embed-{media_id}.html')
+        return self._default_get_url(host, media_id, template='https://{host}/{media_id}')
