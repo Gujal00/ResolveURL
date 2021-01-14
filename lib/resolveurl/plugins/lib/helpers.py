967e734ced98ec735495b87ce3eedd5eecf0c586
gujal <17394059+Gujal00@users.noreply.github.com>
2021-01-15 10:59:56 +1300

    restore jetload

--- lib/resolveurl/plugins/lib/helpers.py
+++ lib/resolveurl/plugins/lib/helpers.py
@@ -306,42 +306,45 @@ def get_redirect_url(url, headers={}):
 
 def girc(page_data, url, co):
     """
-    Code adapted from https://github.com/addon-lab/addon-lab_resolver_Project
+    Code adapted from https://github.com/vb6rocod/utils/
+    Copyright (C) 2019 vb6rocod
+    and https://github.com/addon-lab/addon-lab_resolver_Project
     Copyright (C) 2021 ADDON-LAB, KAR10S
     """
     net = common.Net()
     hdrs = {'User-Agent': common.FF_USER_AGENT,
             'Referer': url}
     rurl = 'https://www.google.com/recaptcha/api.js'
-    aurl = 'https://www.google.com/recaptcha/api2/anchor'
-    purl = 'https://www.google.com/recaptcha/api2/reload'
-    key = re.findall(r'src="{0}\?.*?render=([^"]+)'.format(rurl), page_data)[0]
-    rurl = '{0}?render={1}'.format(rurl, key)
-    page_data1 = net.http_GET(rurl, headers=hdrs).content
-    v = re.findall('releases/([^/]+)', page_data1)[0]
-    rdata = {'ar': 1,
-             'k': key,
-             'co': co,
-             'hl': 'en',
-             'v': v,
-             'size': 'invisible',
-             'cb': '123456789'}
-    page_data2 = net.http_GET('{0}?{1}'.format(aurl, urllib_parse.urlencode(rdata)), headers=hdrs).content
-    rtoken = re.search('recaptcha-token.+?="([^"]+)', page_data2)
-    if rtoken:
-        rtoken = rtoken.group(1)
-    else:
-        return ''
-    pdata = {'v': v,
-             'reason': 'q',
-             'k': key,
-             'c': rtoken,
-             'sa': '',
-             'co': co}
-    hdrs.update({'Referer': aurl})
-    page_data3 = net.http_POST('{0}?k={1}'.format(purl, key), form_data=pdata, headers=hdrs).content
-    gtoken = re.search('rresp","([^"]+)', page_data3)
-    if gtoken:
-        return gtoken.group(1)
-    else:
-        return ''
+    aurl = 'https://www.google.com/recaptcha/api2'
+    key = re.search(r'src="{0}\?.*?render=([^"]+)'.format(rurl), page_data)
+    if key:
+        key = key.group(1)
+        rurl = '{0}?render={1}'.format(rurl, key)
+        page_data1 = net.http_GET(rurl, headers=hdrs).content
+        v = re.findall('releases/([^/]+)', page_data1)[0]
+        rdata = {'ar': 1,
+                 'k': key,
+                 'co': co,
+                 'hl': 'en',
+                 'v': v,
+                 'size': 'invisible',
+                 'cb': '123456789'}
+        page_data2 = net.http_GET('{0}/anchor?{1}'.format(aurl, urllib_parse.urlencode(rdata)), headers=hdrs).content
+        rtoken = re.search('recaptcha-token.+?="([^"]+)', page_data2)
+        if rtoken:
+            rtoken = rtoken.group(1)
+        else:
+            return ''
+        pdata = {'v': v,
+                 'reason': 'q',
+                 'k': key,
+                 'c': rtoken,
+                 'sa': '',
+                 'co': co}
+        hdrs.update({'Referer': aurl})
+        page_data3 = net.http_POST('{0}/reload?k={1}'.format(aurl, key), form_data=pdata, headers=hdrs).content
+        gtoken = re.search('rresp","([^"]+)', page_data3)
+        if gtoken:
+            return gtoken.group(1)
+
+    return ''
