2a631283b4f09e14828fe51c3db4e77804861ffc
gujal <17394059+Gujal00@users.noreply.github.com>
2021-01-19 21:17:19 +1300

    fix voyueurhit

--- resources/plugins/voyeurhit.py
+++ resources/plugins/voyeurhit.py
@@ -1,6 +1,6 @@
 """
     Plugin for ResolveURL
-    Copyright (C) 2016 gujal
+    Copyright (C) 2021 gujal
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -16,7 +16,7 @@
     along with this program. If not, see <http://www.gnu.org/licenses/>.
 """
 
-import re
+import json
 from resolveurl import common
 from resolveurl.plugins.lib import helpers
 from resolveurl.resolver import ResolveUrl, ResolverError
@@ -28,24 +28,40 @@ class VoyeurhitResolver(ResolveUrl):
     pattern = r'(?://|\.)(voyeurhit\.com)/(?:videos|embed)/([\w\-]+)'
 
     def get_media_url(self, host, media_id):
-        if not media_id.isdigit():
-            web_url = self.get_url(host, media_id)
-            headers = {'User-Agent': common.RAND_UA}
-            html = self.net.http_GET(web_url, headers=headers).content
-            embed = re.search(r"""<iframe.+?src=['"](http://voyeurhit.com/embed/\d+)""", html, re.I)
-            if embed:
-                return helpers.get_media_url(embed.group(1), patterns=[r"""video_url:\s*['"](?P<url>.+?)(?:/\?[^"']+)?["']"""]).replace(' ', '%20')
+        web_url = self.get_url(host, media_id)
+        headers = {'User-Agent': common.RAND_UA,
+                   'Referer': 'https://{0}/'.format(host)}
+        resp = self.net.http_GET(web_url, headers=headers).content
+        resp = json.loads(resp)[0]
+        if resp.get('video_url'):
+            return 'https://{0}{1}'.format(host, self._decode(resp.get('video_url'))) + helpers.append_headers(headers)
 
-            raise ResolverError('File not found')
-
-        else:
-            return helpers.get_media_url(self.get_url(host, media_id), patterns=[r"""video_url:\s*['"](?P<url>.+?)(?:/\?[^"']+)?["']"""]).replace(' ', '%20')
+        raise ResolverError('File not found')
 
     def get_url(self, host, media_id):
-        if not media_id.isdigit():
-            return self._default_get_url(host, media_id, template='http://{host}/videos/{media_id}/')
-        else:
-            return self._default_get_url(host, media_id, template='http://{host}/embed/{media_id}')
+        return self._default_get_url(host, media_id, template='https://{host}/api/videofile.php?video_id={media_id}&lifetime=8640000')
+
+    def _decode(self, e):
+        LUT = {u'\u0410': 'A', u'\u0412': 'B', u'\u0421': 'C', u'\u0415': 'E', u'\u041c': 'M'}
+        e = ''.join([x if ord(x) < 128 else LUT[x] for x in e])
+        t = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789.,~'
+        i = ''
+        a = 0
+        while a < len(e):
+            n = t.index(e[a])
+            o = t.index(e[a + 1])
+            s = t.index(e[a + 2])
+            r = t.index(e[a + 3])
+            a += 4
+            n = n << 2 | o >> 4
+            o = (15 & o) << 4 | s >> 2
+            l = (3 & s) << 6 | r
+            i += chr(n)
+            if s != 64:
+                i += chr(o)
+            if r != 64:
+                i += chr(l)
+        return i
 
     @classmethod
     def _is_enabled(cls):
