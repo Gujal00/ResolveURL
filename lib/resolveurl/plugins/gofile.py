e0df2d19cf6af2d7371271b8411ce16df3b45347
gujal <17394059+Gujal00@users.noreply.github.com>
2021-02-08 10:54:19 +1300

    fix GoFile

--- lib/resolveurl/plugins/gofile.py
+++ lib/resolveurl/plugins/gofile.py
@@ -17,7 +17,6 @@
 """
 
 import json
-from six.moves import urllib_parse
 from resolveurl.plugins.lib import helpers
 from resolveurl import common
 from resolveurl.resolver import ResolveUrl, ResolverError
@@ -37,7 +36,7 @@ class GofileResolver(ResolveUrl):
             sources = []
             if(download_url['data']['files']):
                 for file_index in download_url['data']['files']:
-                    url = urllib_parse.quote(download_url['data']['files'][file_index]['link'], ':/')
+                    url = download_url['data']['files'][file_index]['link']
                     size = download_url['data']['files'][file_index]['size']
                     sources += [(size, url)]
             return helpers.pick_source(sources, False)
