5792f264c07f9345888fc366e713010076dd83b9
gujal <17394059+Gujal00@users.noreply.github.com>
2021-03-03 15:41:32 +1300

    update youtube

--- lib/resolveurl/plugins/youtube.py
+++ lib/resolveurl/plugins/youtube.py
@@ -1,6 +1,6 @@
 """
     Plugin for ResolveUrl
-    Copyright (C) 2011 t0mm0
+    Copyright (C) 2018 gujal
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -15,17 +15,8 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
-from resolveurl.resolver import ResolveUrl, ResolverError
+from resolveurl.resolver import ResolveUrl
 from resolveurl.lib import kodi
-from resolveurl.plugins.lib import helpers
-import re
-from six.moves import urllib_parse
-from resolveurl import common
-
-try:
-    import youtube_resolver
-except ImportError:
-    youtube_resolver = None
 
 
 class YoutubeResolver(ResolveUrl):
@@ -33,38 +24,8 @@ class YoutubeResolver(ResolveUrl):
     domains = ['youtube.com', 'youtu.be', 'youtube-nocookie.com']
     pattern = r'''https?://(?:[0-9A-Z-]+\.)?(?:(youtu\.be|youtube(?:-nocookie)?\.com)/?\S*?[^\w\s-])([\w-]{11})(?=[^\w-]|$)(?![?=&+%\w.-]*(?:['"][^<>]*>|</a>))[?=&+%\w.-]*'''
 
-    def __init__(self):
-        self.headers = {'User-Agent': common.RAND_UA}
-
     def get_media_url(self, host, media_id):
-        try:
-            web_url = self.get_url(host, media_id)
-            html = self.net.http_GET(web_url, headers=self.headers).content
-            stream_map = urllib_parse.unquote(re.findall('url_encoded_fmt_stream_map=([^&]+)', html)[0])
-            streams = stream_map.split(',')
-            sources = []
-            streams_mp4 = [item for item in streams if 'video%2Fmp4' in item]
-            for stream in streams_mp4:
-                quality = re.findall('quality=([^&]+)', stream)[0]
-                url = re.findall('url=([^&]+)', stream)[0]
-                sources.append((quality, urllib_parse.unquote(url)))
-            if sources:
-                return helpers.pick_source(sources)
-
-        except:
-            if youtube_resolver is None:
-                return 'plugin://plugin.video.youtube/play/?video_id=' + media_id
-            else:
-                streams = youtube_resolver.resolve(media_id)
-                streams_no_dash = [item for item in streams if item['container'] != 'mpd']
-                stream_tuples = [(item['title'], item['url']) for item in streams_no_dash]
-                if stream_tuples:
-                    return helpers.pick_source(stream_tuples)
-
-        raise ResolverError('Video not found')
-
-    def get_url(self, host, media_id):
-        return 'https://www.youtube.com/get_video_info?html5=1&video_id=%s' % media_id
+        return 'plugin://plugin.video.youtube/play/?video_id={0}'.format(media_id)
 
     @classmethod
     def _is_enabled(cls):
@@ -73,6 +34,5 @@ class YoutubeResolver(ResolveUrl):
     @classmethod
     def get_settings_xml(cls):
         xml = super(cls, cls).get_settings_xml()
-        if youtube_resolver is None:
-            xml.append('<setting label="This plugin calls the youtube addon -change settings there." type="lsep" />')
+        xml.append('<setting label="This plugin calls the youtube addon -change settings there." type="lsep" />')
         return xml
