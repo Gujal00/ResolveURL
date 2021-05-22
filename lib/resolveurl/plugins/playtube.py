7494f858602bc92be674f57a1741416cfdcc22be
gujal <17394059+Gujal00@users.noreply.github.com>
2021-05-22 23:35:28 +1200

    Fix playtube

--- lib/resolveurl/plugins/playtube.py
+++ lib/resolveurl/plugins/playtube.py
@@ -16,22 +16,37 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 '''
 
+from resolveurl import common
+from resolveurl.resolver import ResolveUrl, ResolverError
 from resolveurl.plugins.lib import helpers
-from resolveurl.plugins.__resolve_generic__ import ResolveGeneric
+import re
+import json
 
 
-class PlayTubeResolver(ResolveGeneric):
+class PlayTubeResolver(ResolveUrl):
     name = "playtube"
     domains = ["playtube.ws"]
     pattern = r'(?://|\.)(playtube\.ws)/(?:embed-)?([0-9a-zA-Z]+)'
 
     def get_media_url(self, host, media_id):
-        return helpers.get_media_url(self.get_url(host, media_id),
-                                     patterns=[r'''sources\s*:\s*\["(?P<url>[^"]+)''',
-                                               r'''(?:file|src):\s*"(?P<url>[^"]+)'''],
-                                     generic_patterns=False,
-                                     result_blacklist=['dl', '.srt', '.vtt'],
-                                     referer=False)
+        web_url = self.get_url(host, media_id)
+        headers = {'User-Agent': common.RAND_UA}
+        html = self.net.http_GET(web_url, headers=headers).content
+        html = helpers.get_packed_data(html)
+        r = re.search(r"op:\s*'([^']+)',\s*file_code:\s*'([^']+)',\s*hash:\s*'([^']+)'", html)
+        if r:
+            url = 'https://playtube.ws/dl'
+            data = {'op': r.group(1),
+                    'file_code': r.group(2),
+                    'hash': r.group(3)}
+            headers.update({'Referer': url[:-2],
+                            'Origin': url[:-3]})
+            resp = self.net.http_POST(url, form_data=data, headers=headers).content
+            resp = json.loads(resp)[0]
+            source = helpers.tear_decode(resp.get('file'), resp.get('seed'))
+            if source:
+                return source + helpers.append_headers(headers)
+        raise ResolverError('File not found')
 
     def get_url(self, host, media_id):
         return self._default_get_url(host, media_id, template='https://{host}/embed-{media_id}.html')
