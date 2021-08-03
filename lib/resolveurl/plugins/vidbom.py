9f0fa5c9e9db55d213b61befd5de4ee90a7c8369
gujal <17394059+Gujal00@users.noreply.github.com>
2021-08-03 14:31:19 +1200

    update vidbom

--- lib/resolveurl/plugins/vidbom.py
+++ lib/resolveurl/plugins/vidbom.py
@@ -24,13 +24,15 @@ from resolveurl.resolver import ResolveUrl, ResolverError
 
 class VidBomResolver(ResolveUrl):
     name = "vidbom"
-    domains = ['vidbom.com', 'vidbem.com', 'vidbm.com']
-    pattern = r'(?://|\.)(vidb[oe]?m\.com)/(?:embed[/-])?([A-Za-z0-9]+)'
+    domains = ['vidbom.com', 'vidbem.com', 'vidbm.com', 'vedpom.com', 'vedbom.com',
+               'myviid.com', 'myviid.net', 'myvid.com',
+               'vidshare.com', 'vedsharr.com', 'vedshar.com']
+    pattern = r'(?://|\.)((?:v[ie]d[bp][oe]?m|myvii?d|v[ei]dshar[er]?)\.(?:com|net))(?::\d+)?/(?:embed[/-])?([A-Za-z0-9]+)'
 
     def get_media_url(self, host, media_id):
         web_url = self.get_url(host, media_id)
         headers = {'User-Agent': common.RAND_UA,
-                   'Referer': 'https://vidbem.com/'}
+                   'Referer': web_url}
         html = self.net.http_GET(web_url, headers=headers).content
         html = html.encode('utf-8') if helpers.PY2 else html
         aa_text = re.search(r"""(ﾟωﾟﾉ\s*=\s*/｀ｍ´\s*）\s*ﾉ.+?;)\s*</script""", html, re.I)
@@ -38,11 +40,14 @@ class VidBomResolver(ResolveUrl):
         if aa_text:
             aa_decoded = aadecode.decode(aa_text.group(1))
             sources = helpers.scrape_sources(aa_decoded)
-            if sources:
-                headers.update({'Referer': web_url})
-                return helpers.pick_source(sources) + helpers.append_headers(headers)
+        else:
+            sources = helpers.scrape_sources(html, patterns=[r'''sources:\s*\[{src:\s*"(?P<url>[^"]+)'''])
+
+        if sources:
+            headers.update({'Referer': web_url})
+            return helpers.pick_source(sources) + helpers.append_headers(headers)
 
         raise ResolverError('Video not found')
 
     def get_url(self, host, media_id):
-        return self._default_get_url(host, media_id, template='https://vidbem.com/embed-{media_id}.html')
+        return self._default_get_url(host, media_id, template='https://{host}/embed-{media_id}.html')
