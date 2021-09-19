b0a83e7eaa4515a7c8119b63e3607c9b596c03dd
gujal <17394059+Gujal00@users.noreply.github.com>
2021-09-20 11:15:34 +1200

    Fix Streamtape

--- lib/resolveurl/plugins/streamtape.py
+++ lib/resolveurl/plugins/streamtape.py
@@ -40,18 +40,17 @@ class StreamTapeResolver(ResolveUrl):
         except urllib_error.HTTPError:
             raise ResolverError('Video deleted or removed.')
             return
-        src = re.search(r'''ById\('.+?=\s*(["']//[^;<]+)''', r)
+        src = re.findall(r'''ById\('.+?=\s*(["']//[^;<]+)''', r)
         if src:
             src_url = ''
-            parts = src.group(1).split('+')
+            parts = src[-1].replace("'", '"').split('+')
             for part in parts:
-                p1 = re.findall(r'''['"]([^'"]*)''', part)[0]
+                p1 = re.findall(r'"([^"]*)', part)[0]
                 p2 = 0
                 if 'substring' in part:
-                    subs = part.split(".substring(")
+                    subs = re.findall(r'substring\((\d+)', part)
                     for sub in subs:
-                        if '&' not in sub:
-                            p2 += int(sub[:-1])
+                        p2 += int(sub)
                 src_url += p1[p2:]
             src_url += '&stream=1'
             src_url = 'https:' + src_url if src_url.startswith('//') else src_url
