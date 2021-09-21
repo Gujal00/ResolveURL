5e5c92f31e51d0846100bf2b9e3e92253fe6685a
gujal <17394059+Gujal00@users.noreply.github.com>
2021-09-21 17:37:57 +1200

    update vidcloud9

--- lib/resolveurl/plugins/vidcloud9.py
+++ lib/resolveurl/plugins/vidcloud9.py
@@ -16,6 +16,12 @@ You should have received a copy of the GNU General Public License
 along with this program. If not, see <http://www.gnu.org/licenses/>.
 """
 
+import random
+import base64
+import re
+import json
+from Cryptodome.Cipher import AES
+from Cryptodome.Util import Padding
 from resolveurl.plugins.lib import helpers
 from resolveurl import common
 from resolveurl.resolver import ResolveUrl, ResolverError
@@ -30,12 +36,37 @@ class VidCloud9Resolver(ResolveUrl):
         web_url = self.get_url(host, media_id)
         headers = {'User-Agent': common.FF_USER_AGENT,
                    'Referer': 'https://vidembed.cc/'}
+
+        key = '25746538592938496764662879833288'.encode('utf8')
+        iv = self.f_random(16)
+        encryptor = AES.new(key, AES.MODE_CBC, iv.encode('utf8'))
+        eid = encryptor.encrypt(Padding.pad(media_id.encode('utf8'), AES.block_size))
+        url = 'https://vidembed.cc' + '/encrypt-ajax.php?id=' + base64.b64encode(eid).decode('utf8') \
+            + '&refer=none&time=' + self.f_random(2) + iv + self.f_random(2)
+        headers.update({"X-Requested-With": "XMLHttpRequest"})
+        js_data = json.loads(self.net.http_GET(url, headers=headers).content)
+        sources = js_data.get('source', None)
+        if sources:
+            sources = [(source.get('label'), source.get('file')) for source in sources]
+            headers.pop('X-Requested-With')
+            source = helpers.pick_source(helpers.sort_sources_list(sources))
+            return source + helpers.append_headers(headers)
+
+        # Try Beta Server if no sources found with earlier method
+        headers.pop("X-Requested-With")
         html = self.net.http_GET(web_url, headers=headers).content
         sources = helpers.scrape_sources(html)
         if sources:
             headers.update({'Origin': 'https://vidembed.cc'})
             return helpers.pick_source(sources) + helpers.append_headers(headers)
+
         raise ResolverError('Video not found')
 
     def get_url(self, host, media_id):
         return self._default_get_url(host, media_id, template='https://vidembed.cc/loadserver.php?id={media_id}')
+
+    def f_random(self, x):
+        stime = ''
+        for _ in range(x):
+            stime += str(random.randint(0, 9))
+        return stime
