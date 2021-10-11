35478a49bda4f4f6c95acac55ff9a60730ae56bd
gujal <17394059+Gujal00@users.noreply.github.com>
2021-10-12 08:38:09 +1300

    voe.sx prefer mp4

--- lib/resolveurl/plugins/voesx.py
+++ lib/resolveurl/plugins/voesx.py
@@ -16,18 +16,31 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
+import re
+import base64
+from resolveurl import common
 from resolveurl.plugins.lib import helpers
-from resolveurl.plugins.__resolve_generic__ import ResolveGeneric
+from resolveurl.resolver import ResolveUrl
 
 
-class VoeResolver(ResolveGeneric):
+class VoeResolver(ResolveUrl):
     name = "voe"
     domains = ["voe.sx"]
     pattern = r'(?://|\.)(voe\.sx)/(?:e/)?([0-9A-Za-z]+)'
 
     def get_media_url(self, host, media_id):
-        return helpers.get_media_url(self.get_url(host, media_id),
-                                     patterns=[r'''(?:mp4|hls)":\s*"(?P<url>[^"]+)",\s*"video_height":\s*(?P<label>[^,]+)'''],
+        web_url = self.get_url(host, media_id)
+        headers = {'User-Agent': common.RAND_UA,
+                   'Referer': 'https://{0}/'.format(host)}
+        html = self.net.http_GET(web_url, headers=headers).content
+        r = re.search(r'uttf0\((\[[^)]+)', html)
+        if r:
+            r = eval(r.group(1))
+            r = base64.b64decode(''.join(r)[::-1].encode('utf8')).decode('utf8')
+            return r + helpers.append_headers(headers)
+
+        return helpers.get_media_url(web_url,
+                                     patterns=[r'''hls":\s*"(?P<url>[^"]+)",\s*"video_height":\s*(?P<label>[^,]+)'''],
                                      generic_patterns=False)
 
     def get_url(self, host, media_id):
