4dd0df1111ec0c3feb14dc0315290d2933b61cb4
gujal <17394059+Gujal00@users.noreply.github.com>
2021-10-13 11:50:35 +1300

    Fix streamingcommunity

--- lib/resolveurl/plugins/streamcommunity.py
+++ lib/resolveurl/plugins/streamcommunity.py
@@ -16,6 +16,7 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 import re
+import json
 from resolveurl.plugins.lib import helpers
 from resolveurl import common
 from resolveurl.resolver import ResolveUrl, ResolverError
@@ -23,24 +24,26 @@ from resolveurl.resolver import ResolveUrl, ResolverError
 
 class StreamCommunityResolver(ResolveUrl):
     name = "streamcommunity"
-    domains = ['streamingcommunity.xyz', 'streamingcommunity.one']
-    pattern = r'(?://|\.)(streamingcommunity\.(?:one|xyz))/watch/(\d+(?:\?e=)?\d+)'
+    domains = ['streamingcommunity.xyz', 'streamingcommunity.one', 'streamingcommunity.video']
+    pattern = r'(?://|\.)(streamingcommunity\.(?:one|xyz|video))/watch/(\d+(?:\?e=)?\d+)'
 
     def get_media_url(self, host, media_id):
         web_url = self.get_url(host, media_id)
         headers = {'User-Agent': common.RAND_UA}
         html = self.net.http_GET(web_url, headers=headers).content
-        match = re.search(r'''<video-player.+?video_url.+?(http[^&]+)''', html, re.DOTALL)
+        match = re.search(r'''<video-player.+?scws_id[^\d]+(\d+)''', html, re.DOTALL)
         if match:
-            url = match.group(1).replace('\\', '')
-            headers.update({'Referer': web_url})
-            a = self.net.http_GET('https://streamingcommunity.xyz/client-address', headers=headers).content
-            return url + self.get_token(a) + helpers.append_headers(headers)
+            scws_id = match.group(1)
+            headers.update({'Referer': 'https://streamingcommunity.video/'})
+            html = self.net.http_GET('https://scws.xyz/videos/' + scws_id, headers=headers).content
+            a = json.loads(html).get('client_ip')
+            url = 'https://scws.xyz/master/{0}?{1}'.format(scws_id, self.get_token(a))
+            return url + helpers.append_headers(headers)
 
         raise ResolverError('Video Link Not Found')
 
     def get_url(self, host, media_id):
-        return self._default_get_url(host, media_id, template='https://streamingcommunity.xyz/watch/{media_id}')
+        return self._default_get_url(host, media_id, template='https://streamingcommunity.video/watch/{media_id}')
 
     def get_token(self, a):
         import time
@@ -50,4 +53,4 @@ class StreamCommunityResolver(ResolveUrl):
         s = '{0}{1} Yc8U6r8KjAKAepEA'.format(t, a)
         c = base64.b64encode(md5(s.encode('utf-8')).digest()).decode('utf-8')
         c = c.replace('=', '').replace('+', '-').replace('/', '_')
-        return '?token={0}&expires={1}'.format(c, t)
+        return 'token={0}&expires={1}&n=1'.format(c, t)
