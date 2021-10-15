e9a0a7837a1ecd5363996854706cd2d1c14ab6d7
gujal <17394059+Gujal00@users.noreply.github.com>
2021-10-16 10:06:15 +1300

    streamsb fix

--- lib/resolveurl/plugins/streamsb.py
+++ lib/resolveurl/plugins/streamsb.py
@@ -17,22 +17,34 @@
     along with this program. If not, see <http://www.gnu.org/licenses/>.
 """
 
-from resolveurl.plugins.__resolve_generic__ import ResolveGeneric
+import re
 from resolveurl.plugins.lib import helpers
+from resolveurl import common
+from resolveurl.resolver import ResolveUrl, ResolverError
 
 
-class StreamSBResolver(ResolveGeneric):
+class StreamSBResolver(ResolveUrl):
     name = "streamsb"
     domains = ["sbembed.com", "sbembed1.com", "sbplay.org", "sbvideo.net", "streamsb.net", "sbplay.one", "cloudemb.com", "playersb.com", "tubesb.com"]
-    pattern = r'(?://|\.)((?:tube|player|cloudemb|stream)?s?b?(?:embed\d?|play|video)?\.(?:com|net|org|one))/(?:embed-|e|play)?/?([0-9a-zA-Z]+)'
+    pattern = r'(?://|\.)((?:tube|player|cloudemb|stream)?s?b?(?:embed\d?|play|video)?\.(?:com|net|org|one))/(?:embed-|e|play|d)?/?([0-9a-zA-Z]+)'
 
     def get_media_url(self, host, media_id):
-        return helpers.get_media_url(self.get_url(host, media_id),
-                                     patterns=[r'''sources\s*:\s*\["(?P<url>[^"]+)''',
-                                               r'''(?:file|src):\s*"(?P<url>[^"]+)'''],
-                                     generic_patterns=False,
-                                     result_blacklist=['dl', '.srt', '.vtt'],
-                                     referer=False)
+        web_url = self.get_url(host, media_id)
+        headers = {'User-Agent': common.RAND_UA,
+                   'Referer': 'https://{0}/'.format(host)}
+        html = self.net.http_GET(web_url, headers=headers).content
+        sources = re.findall(r'download_video([^"]+)[^\d]+\d+x(\d+)', html)
+        if sources:
+            sources.sort(key=lambda x: int(x[1]), reverse=True)
+            sources = [(x[1] + 'p', x[0]) for x in sources]
+            code, mode, hash = eval(helpers.pick_source(sources))
+            dl_url = 'https://{0}/dl?op=download_orig&id={1}&mode={2}&hash={3}'.format(host, code, mode, hash)
+            html = self.net.http_GET(dl_url, headers=headers).content
+            r = re.search('href="([^"]+)">Direct', html)
+            if r:
+                return r.group(1) + helpers.append_headers(headers)
+
+        raise ResolverError('Video not found')
 
     def get_url(self, host, media_id):
-        return self._default_get_url(host, media_id, template='https://{host}/play/{media_id}')
+        return self._default_get_url(host, media_id, template='https://{host}/d/{media_id}.html')
