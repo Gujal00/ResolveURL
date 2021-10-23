05934f62fe06781761e67291f8f03da1e7783bce
shellc0de <50564043+shellc0de27@users.noreply.github.com>
2021-10-22 18:34:12 -0700

    Fembed additional domain (#529)

--- lib/resolveurl/plugins/fembed.py
+++ lib/resolveurl/plugins/fembed.py
@@ -29,12 +29,13 @@ class FembedResolver(ResolveUrl):
                "femoload.xyz", "feurl.com", "dailyplanet.pw", "jplayer.net", "xstreamcdn.com", "gcloud.live",
                "vcdnplay.com", "vidohd.com", "vidsource.me", "votrefile.xyz", "zidiplay.com", "fcdn.stream",
                "mediashore.org", "there.to", "femax20.com", "sexhd.co", "viplayer.cc", "mrdhan.com", "votrefilms.xyz",
-               "embedsito.com", "dutrag.com", "youvideos.ru", "streamm4u.club", "moviepl.xyz", "asianclub.tv", "vidcloud.fun"]
+               "embedsito.com", "dutrag.com", "youvideos.ru", "streamm4u.club", "moviepl.xyz", "asianclub.tv",
+               "vidcloud.fun", "fplayer.info"]
     pattern = r'(?://|\.)(' \
               r'(?:fembed|feurl|femax20|24hd|anime789|[fv]cdn|sharinglink|streamm4u|votrefil[em]s?|' \
-              r'femoload|asianclub|dailyplanet|jplayer|mrdhan|there|sexhd|gcloud|mediashore|xstreamcdn|' \
+              r'femoload|asianclub|dailyplanet|[jf]player|mrdhan|there|sexhd|gcloud|mediashore|xstreamcdn|' \
               r'vcdnplay|vidohd|vidsource|viplayer|zidiplay|embedsito|dutrag|youvideos|moviepl|vidcloud)\.' \
-              r'(?:com|club|io|xyz|pw|net|to|live|me|stream|co|cc|org|ru|tv|fun))/(?:v|f)/([a-zA-Z0-9-]+)'
+              r'(?:com|club|io|xyz|pw|net|to|live|me|stream|co|cc|org|ru|tv|fun|info))/(?:v|f)/([a-zA-Z0-9-]+)'
 
     def get_media_url(self, host, media_id):
         web_url = self.get_url(host, media_id)
