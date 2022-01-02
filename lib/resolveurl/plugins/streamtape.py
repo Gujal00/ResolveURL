f9a47fe6899483238c0f628543dbe58db6ba3061
shellc0de <50564043+shellc0de27@users.noreply.github.com>
2022-01-02 00:40:05 -0800

    Streamtape additional domain (#598)

--- lib/resolveurl/plugins/streamtape.py
+++ lib/resolveurl/plugins/streamtape.py
@@ -26,8 +26,8 @@ from six.moves import urllib_error
 class StreamTapeResolver(ResolveUrl):
     name = "streamtape"
     domains = ['streamtape.com', 'strtape.cloud', 'streamtape.net', 'streamta.pe', 'streamtape.site',
-               'strcloud.link', 'strtpe.link']
-    pattern = r'(?://|\.)(str(?:eam)?(?:ta?p?e?|cloud)\.(?:com|cloud|net|pe|site|link))/(?:e|v)/([0-9a-zA-Z]+)'
+               'strcloud.link', 'strtpe.link', 'streamtape.cc']
+    pattern = r'(?://|\.)(str(?:eam)?(?:ta?p?e?|cloud)\.(?:com|cloud|net|pe|site|link|cc))/(?:e|v)/([0-9a-zA-Z]+)'
 
     def get_media_url(self, host, media_id):
         web_url = self.get_url(host, media_id)
