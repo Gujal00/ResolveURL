12b9190952ba8760bf75530b6b122ce6c077ad6f
Paradigm Shifter <1566256+Twilight0@users.noreply.github.com>
2022-01-03 12:18:27 +0200

    Additional domain for videovard (#603)

--- lib/resolveurl/plugins/videovard.py
+++ lib/resolveurl/plugins/videovard.py
@@ -24,8 +24,8 @@ import json
 
 class VideoVardResolver(ResolveUrl):
     name = "videovard"
-    domains = ["videovard.sx"]
-    pattern = r'(?://|\.)(videovard\.sx)/[ved]/([0-9a-zA-Z]+)'
+    domains = ["videovard.sx", "videovard.to"]
+    pattern = r'(?://|\.)(videovard\.(?:sx|to))/[ved]/([0-9a-zA-Z]+)'
 
     def get_media_url(self, host, media_id):
         web_url = self.get_url(host, media_id)
