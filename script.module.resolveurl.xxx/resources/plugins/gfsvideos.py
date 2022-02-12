bf472f81824e2947dceaa6d19b6f1a9af5bdfa67
shellc0de <50564043+shellc0de27@users.noreply.github.com>
2022-02-11 16:20:18 -0800

    Fix gfsvideos

--- script.module.resolveurl.xxx/resources/plugins/gfsvideos.py
+++ script.module.resolveurl.xxx/resources/plugins/gfsvideos.py
@@ -16,13 +16,27 @@ You should have received a copy of the GNU General Public License
 along with this program. If not, see <http://www.gnu.org/licenses/>.
 """
 
-from resolveurl.plugins.__resolve_generic__ import ResolveGeneric
+import re
+from resolveurl import common
+from resolveurl.plugins.lib import helpers
+from resolveurl.resolver import ResolveUrl, ResolverError
 
 
-class GFSVideosResolver(ResolveGeneric):
+class GFSVideosResolver(ResolveUrl):
     name = 'gfsvideos'
     domains = ['gfsvideos.com']
     pattern = r'(?://|\.)(gfsvideos\.com)/video/([\w\-]+)\.html'
+
+    def get_media_url(self, host, media_id):
+        web_url = self.get_url(host, media_id)
+        headers = {'User-Agent': common.RAND_UA}
+        html = self.net.http_GET(web_url, headers=headers).content
+        source = re.search(r'''<source\s*src=["']([^"']+)''', html)
+        if source:
+            headers.update({'Referer': web_url, 'verifypeer': 'false'})
+            return source.group(1) + helpers.append_headers(headers)
+
+        raise ResolverError('File Not Found or Removed')
 
     def get_url(self, host, media_id):
         return self._default_get_url(host, media_id, template='http://www.{host}/video/{media_id}.html')
