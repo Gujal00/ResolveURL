75e1dd6f5b9113b1a0e639d47c402428323c7522
gujal <17394059+Gujal00@users.noreply.github.com>
2022-02-18 21:45:05 +1300

    Fix streamsb

--- script.module.resolveurl/lib/resolveurl/plugins/streamsb.py
+++ script.module.resolveurl/lib/resolveurl/plugins/streamsb.py
@@ -86,4 +86,4 @@ class StreamSBResolver(ResolveUrl):
         c2 = binascii.hexlify(x.encode('utf8')).decode('utf8')
         x = '{0}||{1}||{2}||streamsb'.format(makeid(12), c2, makeid(12))
         c3 = binascii.hexlify(x.encode('utf8')).decode('utf8')
-        return 'https://{0}/sourcesx38/{1}/{2}'.format(host, c1, c3)
+        return 'https://{0}/sources40/{1}/{2}'.format(host, c1, c3)
