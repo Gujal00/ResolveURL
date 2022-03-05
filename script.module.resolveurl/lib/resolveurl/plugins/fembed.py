8c284d92b109c7f1ef3712bae2155978ba02e2b7
shellc0de <50564043+shellc0de27@users.noreply.github.com>
2022-03-04 23:47:40 -0800

    Fembed domains (#33)

--- script.module.resolveurl/lib/resolveurl/plugins/fembed.py
+++ script.module.resolveurl/lib/resolveurl/plugins/fembed.py
@@ -32,14 +32,14 @@ class FembedResolver(ResolveUrl):
                "embedsito.com", "dutrag.com", "youvideos.ru", "streamm4u.club", "moviepl.xyz", "asianclub.tv",
                "vidcloud.fun", "fplayer.info", "diasfem.com", "fembad.org", "moviemaniac.org", "albavido.xyz",
                "ncdnstm.com", "fembed-hd.com", "superplayxyz.club", "cinegrabber.com", "ndrama.xyz",
-               "javstream.top", "javpoll.com", "suzihaza.com", "fembed.net"]
+               "javstream.top", "javpoll.com", "suzihaza.com", "fembed.net", "ezsubz.com", "reeoov.tube"]
     pattern = r'(?://|\.)(' \
               r'(?:femb[ae]d(?:-hd)?|feurl|femax20|24hd|anime789|[fv]cdn|sharinglink|streamm4u|votrefil[em]s?|' \
               r'femoload|asianclub|dailyplanet|[jf]player|mrdhan|there|sexhd|gcloud|mediashore|xstreamcdn|' \
               r'vcdnplay|vidohd|vidsource|viplayer|zidiplay|embedsito|dutrag|youvideos|moviepl|vidcloud|' \
               r'diasfem|moviemaniac|albavido|ncdnstm|superplayxyz|cinegrabber|ndrama|jav(?:stream|poll)|' \
-              r'suzihaza)\.' \
-              r'(?:com|club|io|xyz|pw|net|to|live|me|stream|co|cc|org|ru|tv|fun|info|top))' \
+              r'suzihaza|ezsubz|reeoov)\.' \
+              r'(?:com|club|io|xyz|pw|net|to|live|me|stream|co|cc|org|ru|tv|fun|info|top|tube))' \
               r'/(?:v|f)/([a-zA-Z0-9-]+)'
 
     def __init__(self):
