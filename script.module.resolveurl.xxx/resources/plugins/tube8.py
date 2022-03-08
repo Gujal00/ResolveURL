693bd752c92f74cb2a4e8c94779e8f0f638406b8
shellc0de <50564043+shellc0de27@users.noreply.github.com>
2022-03-07 19:49:06 -0800

    Fix Tube8 (#38)

--- script.module.resolveurl.xxx/resources/plugins/tube8.py
+++ script.module.resolveurl.xxx/resources/plugins/tube8.py
@@ -16,31 +16,19 @@
     along with this program. If not, see <http://www.gnu.org/licenses/>.
 """
 
-import re
-import json
-from resolveurl import common
+from resolveurl.plugins.__resolve_generic__ import ResolveGeneric
 from resolveurl.plugins.lib import helpers
-from resolveurl.resolver import ResolveUrl, ResolverError
 
 
-class Tube8Resolver(ResolveUrl):
+class Tube8Resolver(ResolveGeneric):
     name = 'tube8'
     domains = ['tube8.com', 'tube8.net', 'tube8.info', 'tube8.org']
     pattern = r'(?://|\.)(tube8\.(?:com|net|info|org))/(?:embed/)?(.+?/\d+)'
 
     def get_media_url(self, host, media_id):
-        web_url = self.get_url(host, media_id)
-        headers = {'User-Agent': common.RAND_UA}
-        html = self.net.http_GET(web_url, headers=headers).content
-        flashvars = re.search(r'''var flashvars\s*=\s*({.+?});''', html)
-        if flashvars:
-            r = json.loads(flashvars.group(1)).get("mediaDefinition", {})
-            sources = [(i.get("quality"), i.get("videoUrl")) for i in r]
-            if sources:
-                headers.update({'Referer': web_url})
-                return helpers.pick_source(sources) + helpers.append_headers(headers)
-
-        raise ResolverError('File not found')
+        return helpers.get_media_url(self.get_url(host, media_id),
+                                     patterns=[r'''quality_(?P<label>[^"]+)":"(?P<url>[^"]+)'''],
+                                     generic_patterns=False)
 
     def get_url(self, host, media_id):
         return self._default_get_url(host, media_id, template='https://www.tube8.com/embed/{media_id}/')
