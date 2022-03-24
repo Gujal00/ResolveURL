d9d586ad69c60b5ff02ba9106344e50faa3e60bf
soter80 <42871821+soter80@users.noreply.github.com>
2022-03-24 21:51:10 +0100

    Add new streamcommunity domain (#56)
    Add streamcommunity.icu

--- script.module.resolveurl/lib/resolveurl/plugins/streamcommunity.py
+++ script.module.resolveurl/lib/resolveurl/plugins/streamcommunity.py
@@ -28,8 +28,8 @@ class StreamCommunityResolver(ResolveUrl):
                'streamingcommunity.work', 'streamingcommunity.name', 'streamingcommunity.video',
                'streamingcommunity.live', 'streamingcommunity.tv', 'streamingcommunity.space',
                'streamingcommunity.art', 'streamingcommunity.fun', 'streamingcommunity.website',
-               'streamingcommunity.host', 'streamingcommunity.site', 'streamingcommunity.bond']
-    pattern = r'(?://|\.)(streamingcommunity\.(?:one|xyz|video|vip|work|name|live|tv|space|art|fun|website|host|site|bond))/watch/(\d+(?:\?e=)?\d+)'
+               'streamingcommunity.host', 'streamingcommunity.site', 'streamingcommunity.bond', 'streamingCommunity.icu'] 
+    pattern = r'(?://|\.)(streamingcommunity\.(?:one|xyz|video|vip|work|name|live|tv|space|art|fun|website|host|site|bond|icu))/watch/(\d+(?:\?e=)?\d+)'
 
     def get_media_url(self, host, media_id):
         web_url = self.get_url(host, media_id)
@@ -38,7 +38,7 @@ class StreamCommunityResolver(ResolveUrl):
         match = re.search(r'''<video-player.+?scws_id[^\d]+(\d+)''', html, re.DOTALL)
         if match:
             scws_id = match.group(1)
-            headers.update({'Referer': 'https://streamingcommunity.bond/'})
+            headers.update({'Referer': 'https://streamingcommunity.icu/'})
             html = self.net.http_GET('https://scws.xyz/videos/' + scws_id, headers=headers).content
             a = json.loads(html).get('client_ip')
             url = 'https://scws.xyz/master/{0}?{1}'.format(scws_id, self.get_token(a))
@@ -47,7 +47,7 @@ class StreamCommunityResolver(ResolveUrl):
         raise ResolverError('Video Link Not Found')
 
     def get_url(self, host, media_id):
-        return self._default_get_url(host, media_id, template='https://streamingcommunity.bond/watch/{media_id}')
+        return self._default_get_url(host, media_id, template='https://streamingcommunity.icu/watch/{media_id}')
 
     def get_token(self, a):
         import time
