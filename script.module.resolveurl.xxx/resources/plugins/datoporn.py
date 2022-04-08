7044fab54eae98c34b59f59b1ab454a791dea1b7
shellc0de <50564043+shellc0de27@users.noreply.github.com>
2022-04-07 18:04:10 -0700

    Fix Datoporn (#66)

--- script.module.resolveurl.xxx/resources/plugins/datoporn.py
+++ script.module.resolveurl.xxx/resources/plugins/datoporn.py
@@ -49,7 +49,7 @@ class DatoPornResolver(ResolveUrl):
         raise ResolverError('File not found')
 
     def get_url(self, host, media_id):
-        return self._default_get_url(host, media_id, template='https://dato.porn/embed/{media_id}/')
+        return self._default_get_url(host, media_id, template='https://www.datoporn.com/embed/{media_id}/')
 
     @classmethod
     def _is_enabled(cls):
