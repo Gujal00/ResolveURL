e6257018227233533f9836404e186d139e0de32d
shellc0de <50564043+shellc0de27@users.noreply.github.com>
2022-04-23 22:31:08 -0700

    Doodstream additional domain (#79)

--- script.module.resolveurl/lib/resolveurl/plugins/doodstream.py
+++ script.module.resolveurl/lib/resolveurl/plugins/doodstream.py
@@ -27,8 +27,8 @@ from resolveurl.resolver import ResolveUrl, ResolverError
 class DoodStreamResolver(ResolveUrl):
     name = "doodstream"
     domains = ['dood.watch', 'doodstream.com', 'dood.to', 'dood.so', 'dood.cx', 'dood.la', 'dood.ws',
-               'dood.sh', 'doodstream.co']
-    pattern = r'(?://|\.)(dood(?:stream)?\.(?:com?|watch|to|s[ho]|cx|la|ws))/(?:d|e)/([0-9a-zA-Z]+)'
+               'dood.sh', 'doodstream.co', 'dood.pm']
+    pattern = r'(?://|\.)(dood(?:stream)?\.(?:com?|watch|to|s[ho]|cx|la|ws|pm))/(?:d|e)/([0-9a-zA-Z]+)'
 
     def get_media_url(self, host, media_id):
         if host.endswith('.cx'):
