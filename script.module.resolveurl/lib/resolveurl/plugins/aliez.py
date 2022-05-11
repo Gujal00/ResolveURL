015d7ab1638b20dcd2a50aa646fbb13afac23315
shellc0de <shellc0de69@protonmail.com>
2022-05-10 23:32:50 -0700

    Fix Aliez

--- script.module.resolveurl/lib/resolveurl/plugins/aliez.py
+++ script.module.resolveurl/lib/resolveurl/plugins/aliez.py
@@ -12,11 +12,11 @@
 
     This program is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
     GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    along with this program. If not, see <http://www.gnu.org/licenses/>.
 """
 import re
 from resolveurl.lib import helpers
@@ -24,7 +24,7 @@ from resolveurl.plugins.__resolve_generic__ import ResolveGeneric
 
 
 class AliezResolver(ResolveGeneric):
-    name = "Aliez"
+    name = 'Aliez'
     domains = ['aliez.me']
     pattern = r'(?://|\.)(aliez\.me)/(?:(?:player/video\.php\?id=([0-9]+)&s=([A-Za-z0-9]+))|(?:video/([0-9]+)/([A-Za-z0-9]+)))'
 
@@ -36,12 +36,12 @@ class AliezResolver(ResolveGeneric):
     def get_host_and_id(self, url):
         r = re.search(self.pattern, url, re.I)
         if r:
-            r = filter(None, r.groups())
+            r = list(filter(None, r.groups()))
             r = [r[0], '%s|%s' % (r[1], r[2])]
             return r
         else:
             return False
 
     def get_url(self, host, media_id):
-        media_id = media_id.split("|")
-        return self._default_get_url(host, media_id, 'http://emb.apl5.me/player/video.php?id=%s&s=%s&w=590&h=332' % (media_id[0], media_id[1]))
+        media_id = media_id.split('|')
+        return self._default_get_url(host, media_id, 'http://emb.apl133.me/player/video.php?id=%s&s=%s&w=590&h=332' % (media_id[0], media_id[1]))
