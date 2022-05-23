c4361386a32a1024d6a5e88aecd4ab86c61f07c4
gujal <17394059+Gujal00@users.noreply.github.com>
2022-05-23 13:18:48 +1200

    xhamster additional domains

--- script.module.resolveurl.xxx/resources/plugins/xhamster.py
+++ script.module.resolveurl.xxx/resources/plugins/xhamster.py
@@ -24,7 +24,7 @@ class xHamsterResolver(ResolveGeneric):
     name = 'xhamster'
     domains = ['xhamster.com', 'xhamster.desi']
     domains += ['xhamster{0}.com'.format(x) for x in range(1, 50)]
-    domains += ['xhamster{0}.desi'.format(x) for x in range(1, 16)]
+    domains += ['xhamster{0}.desi'.format(x) for x in range(1, 25)]
     pattern = r'(?://|\.)(xhamster\d?\d?\.(?:com|desi))/.+[/\-=](\w+)'
 
     def get_media_url(self, host, media_id):
@@ -34,7 +34,7 @@ class xHamsterResolver(ResolveGeneric):
                                      result_blacklist=["auto"])
 
     def get_url(self, host, media_id):
-        return self._default_get_url(host, media_id, template='https://xhamster2.com/embed/{media_id}')
+        return self._default_get_url(host, media_id, template='https://{host}/embed/{media_id}')
 
     @classmethod
     def _is_enabled(cls):
