455890f5bcda9d902944ba17488951c14f3a355f
soter80 <42871821+soter80@users.noreply.github.com>
2022-05-24 21:58:13 +0200

    New domain added
    Slmaxed.com added

--- script.module.resolveurl/lib/resolveurl/plugins/streamlare.py
+++ script.module.resolveurl/lib/resolveurl/plugins/streamlare.py
@@ -24,8 +24,8 @@ from resolveurl.resolver import ResolveUrl, ResolverError
 
 class StreamLareResolver(ResolveUrl):
     name = "StreamLare"
-    domains = ["streamlare.com"]
-    pattern = r'(?://|\.)(streamlare\.com)/(?:e|v)/([0-9A-Za-z]+)'
+    domains = ["streamlare.com", "slmaxed.com"] 
+    pattern = r'(?://|\.)(streamlare\slmaxed\.com)/(?:e|v)/([0-9A-Za-z]+)'
 
     def get_media_url(self, host, media_id):
         web_url = self.get_url(host, media_id)
