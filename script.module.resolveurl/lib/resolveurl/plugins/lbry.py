634ddf173b7a0306a2dea70590363fe7df90aca7
azzy9 <19675382+azzy9@users.noreply.github.com>
2022-05-24 19:26:45 +0100

    lbry - add support for lbry.science

--- script.module.resolveurl/lib/resolveurl/plugins/lbry.py
+++ script.module.resolveurl/lib/resolveurl/plugins/lbry.py
@@ -1,6 +1,6 @@
 """
-    Plugin for ResolveURL
-    Copyright (C) 2021  script.module.resolveurl
+    Plugin for ResolveUrl
+    Copyright (C) 2022 script.module.resolveurl
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -13,7 +13,7 @@
     GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    along with this program. If not, see <http://www.gnu.org/licenses/>.
 """
 
 import json
@@ -24,14 +24,14 @@ from resolveurl.resolver import ResolveUrl, ResolverError
 
 class LbryResolver(ResolveUrl):
     name = 'Lbry'
-    domains = ['lbry.tv', 'odysee.com', 'madiator.com']
-    pattern = r'(?://|\.)(lbry\.tv|odysee\.com|madiator\.com)/(\@[^:\/]+\:[^:\/]+\/[^:\/]+:[0-9a-zA-Z]+)'
+    domains = ['lbry.tv', 'lbry.science', 'odysee.com', 'madiator.com']
+    pattern = r'(?://|\.)(lbry\.(?:tv|science)|odysee\.com|madiator\.com)/(\@[^:\/]+(?:\:|\%23)[^:\/]+\/[^:\/]+(?:\:|\%23)[0-9a-zA-Z]+)'
 
     def get_media_url(self, host, media_id):
         web_url = self.get_url(host, media_id)
         form_data = {'jsonrpc': '2.0',
                      'method': 'get',
-                     'params': {'uri': 'lbry://' + (media_id).replace(':', '#'),
+                     'params': {'uri': 'lbry://' + media_id.replace('%23', '#').replace(':', '#'),
                                 'save_file': False}}
         headers = {'User-Agent': common.FF_USER_AGENT, 'Origin': 'https://lbry.tv', 'Referer': web_url}
         response = json.loads(self.net.http_POST('https://api.lbry.tv/api/v1/proxy?m=get', form_data=form_data, headers=headers, jdata=True).content)
