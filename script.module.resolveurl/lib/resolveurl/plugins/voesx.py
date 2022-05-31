a0952c7c64f08146f8102aed88036d4ff0e98d37
shellc0de <50564043+shellc0de27@users.noreply.github.com>
2022-05-30 23:14:26 -0700

    Voesx addtional domains (#119)

--- script.module.resolveurl/lib/resolveurl/plugins/voesx.py
+++ script.module.resolveurl/lib/resolveurl/plugins/voesx.py
@@ -25,8 +25,9 @@ from resolveurl.resolver import ResolveUrl, ResolverError
 
 class VoeResolver(ResolveUrl):
     name = 'Voe'
-    domains = ['voe.sx', 'voe-unblock.com', 'voe-unblock.net', 'voeunblock.com', 'voeunblock1.com']
-    pattern = r'(?://|\.)(voe(?:-?unblock\d?)?\.(?:sx|com|net))/(?:e/)?([0-9A-Za-z]+)'
+    domains = ['voe.sx', 'voe-unblock.com', 'voe-unblock.net', 'voeunblock.com']
+    domains += ['voeunblock{}.com'.format(x) for x in range(1, 20)]
+    pattern = r'(?://|\.)(voe(?:-?unblock\d{0,2})?\.(?:sx|com|net))/(?:e/)?([0-9A-Za-z]+)'
 
     def get_media_url(self, host, media_id):
         web_url = self.get_url(host, media_id)
