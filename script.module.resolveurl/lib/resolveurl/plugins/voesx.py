a65950392b70e77e839c8a3d97c19e6a9ef4a7ac
shellc0de <shellc0de69@protonmail.com>
2022-06-08 17:02:31 -0700

    fix

--- script.module.resolveurl/lib/resolveurl/plugins/voesx.py
+++ script.module.resolveurl/lib/resolveurl/plugins/voesx.py
@@ -25,7 +25,7 @@ from resolveurl.resolver import ResolveUrl, ResolverError
 
 class VoeResolver(ResolveUrl):
     name = 'Voe'
-    domains = ['voe.sx', 'voe-unblock.com', 'voe-unblock.net', 'voeunblock.com', 'voeunbl0ck.com'
+    domains = ['voe.sx', 'voe-unblock.com', 'voe-unblock.net', 'voeunblock.com', 'voeunbl0ck.com',
                'voeunblck.com']
     domains += ['voeunblock{}.com'.format(x) for x in range(1, 11)]
     pattern = r'(?://|\.)(voe(?:-?unbl[o0]?ck\d{0,2})?\.(?:sx|com|net))/(?:e/)?([0-9A-Za-z]+)'
