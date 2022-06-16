edba867a37dd6857bb9fede6d66299b745cc5cd9
gujal <17394059+Gujal00@users.noreply.github.com>
2022-06-16 19:35:18 +1200

    Fembed additional domain

--- script.module.resolveurl/lib/resolveurl/plugins/fembed.py
+++ script.module.resolveurl/lib/resolveurl/plugins/fembed.py
@@ -33,13 +33,13 @@ class FEmbedResolver(ResolveUrl):
                'vidcloud.fun', 'fplayer.info', 'diasfem.com', 'fembad.org', 'moviemaniac.org', 'albavido.xyz',
                'ncdnstm.com', 'fembed-hd.com', 'superplayxyz.club', 'cinegrabber.com', 'ndrama.xyz',
                'javstream.top', 'javpoll.com', 'suzihaza.com', 'fembed.net', 'ezsubz.com', 'reeoov.tube',
-               'diampokusy.com', 'filmvi.xyz']
+               'diampokusy.com', 'filmvi.xyz', 'vidsrc.xyz']
     pattern = r'(?://|\.)(' \
               r'(?:femb[ae]d(?:-hd)?|feurl|femax20|24hd|anime789|[fv]cdn|sharinglink|streamm4u|votrefil[em]s?|' \
               r'femoload|asianclub|dailyplanet|[jf]player|mrdhan|there|sexhd|gcloud|mediashore|xstreamcdn|' \
               r'vcdnplay|vidohd|vidsource|viplayer|zidiplay|embedsito|dutrag|youvideos|moviepl|vidcloud|' \
               r'diasfem|moviemaniac|albavido|ncdnstm|superplayxyz|cinegrabber|ndrama|jav(?:stream|poll)|' \
-              r'suzihaza|ezsubz|reeoov|diampokusy|filmvi)\.' \
+              r'suzihaza|ezsubz|reeoov|diampokusy|filmvi|vidsrc)\.' \
               r'(?:com|club|io|xyz|pw|net|to|live|me|stream|co|cc|org|ru|tv|fun|info|top|tube))' \
               r'/(?:v|f)/([a-zA-Z0-9-]+)'
 
