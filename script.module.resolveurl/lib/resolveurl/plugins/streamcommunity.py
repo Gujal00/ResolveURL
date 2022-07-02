cd071855b13812fc258fb30ca78f05f73946440d
shellc0de <shellc0de69@protonmail.com>
2022-07-01 19:11:38 -0700

    Streamcommunity domain

--- script.module.resolveurl/lib/resolveurl/plugins/streamcommunity.py
+++ script.module.resolveurl/lib/resolveurl/plugins/streamcommunity.py
@@ -31,9 +31,11 @@ class StreamCommunityResolver(ResolveUrl):
                'streamingcommunity.art', 'streamingcommunity.fun', 'streamingcommunity.website',
                'streamingcommunity.host', 'streamingcommunity.site', 'streamingcommunity.bond',
                'streamingCommunity.icu', 'streamingcommunity.bar', 'streamingcommunity.top',
-               'streamingcommunity.cc', 'streamingcommunity.monster', 'streamingcommunity.press']
+               'streamingcommunity.cc', 'streamingcommunity.monster', 'streamingcommunity.press',
+               'streamingcommunity.business']
     pattern = r'(?://|\.)(streamingcommunity\.' \
-        r'(?:one|xyz|video|vip|work|name|live|tv|space|art|fun|website|host|site|bond|icu|bar|top|cc|monster|press))' \
+        r'(?:one|xyz|video|vip|work|name|live|tv|space|art|fun|website|host|site|bond|icu|bar|top|' \
+        r'cc|monster|press|business))' \
         r'/watch/(\d+(?:\?e=)?\d+)'
 
     def get_media_url(self, host, media_id):
@@ -52,7 +54,7 @@ class StreamCommunityResolver(ResolveUrl):
         raise ResolverError('Video Link Not Found')
 
     def get_url(self, host, media_id):
-        return self._default_get_url(host, media_id, template='https://streamingcommunity.press/watch/{media_id}')
+        return self._default_get_url(host, media_id, template='https://streamingcommunity.business/watch/{media_id}')
 
     def get_token(self, a):
         import time
