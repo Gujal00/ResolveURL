8230b9324ce772a4c9f4fb0c5b99e4632bad3dc6
shellc0de <50564043+shellc0de27@users.noreply.github.com>
2022-07-02 00:41:13 -0700

    Voesx Domain (#146)

--- script.module.resolveurl/lib/resolveurl/plugins/voesx.py
+++ script.module.resolveurl/lib/resolveurl/plugins/voesx.py
@@ -25,10 +25,13 @@ from resolveurl.resolver import ResolveUrl, ResolverError
 
 class VoeResolver(ResolveUrl):
     name = 'Voe'
-    domains = ['voe.sx', 'voe-unblock.com', 'voe-unblock.net', 'voeunblock.com', 'voeunbl0ck.com',
-               'voeunblck.com', 'voeunblk.com', 'voe-un-block.com', 'voeun-block.net']
+    domains = ['voe.sx', 'voe-unblock.com', 'voe-unblock.net', 'voeunblock.com',
+               'voeunbl0ck.com', 'voeunblck.com', 'voeunblk.com', 'voe-un-block.com',
+               'voeun-block.net', 'un-block-voe.net']
     domains += ['voeunblock{}.com'.format(x) for x in range(1, 11)]
-    pattern = r'(?://|\.)(voe(?:-?un-?bl[o0]?c?k\d{0,2})?\.(?:sx|com|net))/(?:e/)?([0-9A-Za-z]+)'
+    pattern = r'(?://|\.)(' \
+              r'(?:voe)?(?:-?un-?bl[o0]?c?k\d{0,2})?(?:-?voe)?\.(?:sx|com|net))/' \
+              r'(?:e/)?([0-9A-Za-z]+)'
 
     def get_media_url(self, host, media_id):
         web_url = self.get_url(host, media_id)
