7377aceed8573127a1304e656726704041e8394d
shellc0de <50564043+shellc0de27@users.noreply.github.com>
2022-08-07 22:35:39 -0700

    Voesx additional domain (#168)

--- script.module.resolveurl/lib/resolveurl/plugins/voesx.py
+++ script.module.resolveurl/lib/resolveurl/plugins/voesx.py
@@ -28,9 +28,11 @@ class VoeResolver(ResolveUrl):
     domains = ['voe.sx', 'voe-unblock.com', 'voe-unblock.net', 'voeunblock.com',
                'voeunbl0ck.com', 'voeunblck.com', 'voeunblk.com', 'voe-un-block.com',
                'voeun-block.net', 'un-block-voe.net', 'v-o-e-unblock.com',
-               'audaciousdefaulthouse.com', 'launchreliantcleaverriver.com', 'reputationsheriffkennethsand.com']
+               'audaciousdefaulthouse.com', 'launchreliantcleaverriver.com',
+               'reputationsheriffkennethsand.com', 'fittingcentermondaysunday.com']
     domains += ['voeunblock{}.com'.format(x) for x in range(1, 11)]
-    pattern = r'(?://|\.)((?:audaciousdefaulthouse|launchreliantcleaverriver|reputationsheriffkennethsand|' \
+    pattern = r'(?://|\.)((?:audaciousdefaulthouse|launchreliantcleaverriver|' \
+              r'reputationsheriffkennethsand|fittingcentermondaysunday|' \
               r'(?:v-?o-?e)?(?:-?un-?bl[o0]?c?k\d{0,2})?(?:-?voe)?)\.(?:sx|com|net))/' \
               r'(?:e/)?([0-9A-Za-z]+)'
 
