72f70f7ac5acdff3f21a1d31289f44ff18bc4609
shellc0de <50564043+shellc0de27@users.noreply.github.com>
2022-08-09 21:25:54 -0700

    Pornone adjustment (#173)

--- script.module.resolveurl.xxx/resources/plugins/pornone.py
+++ script.module.resolveurl.xxx/resources/plugins/pornone.py
@@ -25,7 +25,7 @@ class PornOneResolver(ResolveGeneric):
     pattern = r'(?://|\.)(pornone\.com)/[\w\-/]+/(\d+)'
 
     def get_url(self, host, media_id):
-        return self._default_get_url(host, media_id, template='https://{host}/embed/{media_id}/')
+        return self._default_get_url(host, media_id, template='https://{host}/{media_id}/')
 
     @classmethod
     def _is_enabled(cls):
