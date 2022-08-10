4a85ef7df10be29e7474b99d9076840a94c2fd40
shellc0de <50564043+shellc0de27@users.noreply.github.com>
2022-08-09 21:27:08 -0700

    Fix Filemoon (#172)
    * Fix Filemoon
    
    * they changed again

--- script.module.resolveurl/lib/resolveurl/plugins/filemoon.py
+++ script.module.resolveurl/lib/resolveurl/plugins/filemoon.py
@@ -16,37 +16,21 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
-import re
-import json
-from resolveurl import common
+from resolveurl.plugins.__resolve_generic__ import ResolveGeneric
 from resolveurl.lib import helpers
-from resolveurl.resolver import ResolveUrl, ResolverError
 
 
-class FileMoonResolver(ResolveUrl):
+class FileMoonResolver(ResolveGeneric):
     name = 'FileMoon'
     domains = ['filemoon.sx', 'filemoon.to']
     pattern = r'(?://|\.)(filemoon\.(?:sx|to))/(?:e|d)/([0-9a-zA-Z]+)'
 
     def get_media_url(self, host, media_id):
-        web_url = self.get_url(host, media_id)
-        headers = {'User-Agent': common.RAND_UA}
-        html = self.net.http_GET(web_url, headers=headers).content
-        html = helpers.get_packed_data(html)
-        r = re.search(r'''b:\s*'([^']+)',\s*file_code:\s*'([^']+)',\s*hash:\s*'([^']+)''', html)
-        if r:
-            url = 'https://{}/dl'.format(host)
-            payload = {'b': r.group(1), 'file_code': r.group(2), 'hash': r.group(3)}
-            headers.update({'Origin': url[:-3], 'Referer': url[:-2]})
-            req = self.net.http_POST(url, form_data=payload, headers=headers)
-            data = json.loads(req.content)[0]
-            vfile = data.get('file')
-            seed = data.get('seed')
-            source = helpers.tear_decode(vfile, seed)
-            if source:
-                return source + helpers.append_headers(headers)
-
-        raise ResolverError('File Not Found or Removed')
+        return helpers.get_media_url(
+            self.get_url(host, media_id),
+            patterns=[r'''sources:\s*\[{file:\s*["'](?P<url>[^"']+)'''],
+            generic_patterns=False
+        )
 
     def get_url(self, host, media_id):
         return self._default_get_url(host, media_id, template='https://{host}/e/{media_id}')
