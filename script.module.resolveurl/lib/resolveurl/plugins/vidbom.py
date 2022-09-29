b969fc8da1f562ac881415f674a9c764fad4c603
shellc0de <shellc0de69@protonmail.com>
2022-09-28 19:00:07 -0700

    Vidbom domain

--- script.module.resolveurl/lib/resolveurl/plugins/vidbom.py
+++ script.module.resolveurl/lib/resolveurl/plugins/vidbom.py
@@ -26,8 +26,8 @@ from resolveurl.resolver import ResolveUrl, ResolverError
 class VidBomResolver(ResolveUrl):
     name = 'VidBom'
     domains = ['vidbom.com', 'vidbem.com', 'vidbm.com', 'vedpom.com', 'vedbom.com', 'vedbom.org',
-               'vadbom.com', 'vidbam.org', 'myviid.com', 'myviid.net', 'myvid.com', 'vidshare.com',
-               'vedsharr.com', 'vedshar.com', 'vedshare.com', 'vadshar.com', 'vidshar.org']
+               'vadbom.com', 'vidbam.org', 'vadbam.com', 'myviid.com', 'myviid.net', 'myvid.com',
+               'vidshare.com', 'vedsharr.com', 'vedshar.com', 'vedshare.com', 'vadshar.com', 'vidshar.org']
     pattern = r'(?://|\.)((?:v[aie]d[bp][aoe]?m|myvii?d|v[aei]dshar[er]?)\.(?:com|net|org))(?::\d+)?/(?:embed[/-])?([A-Za-z0-9]+)'
 
     def get_media_url(self, host, media_id):
