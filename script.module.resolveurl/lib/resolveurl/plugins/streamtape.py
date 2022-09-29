244d5320fc9c1d6384fe0f11b239470deaf39e44
shellc0de <50564043+shellc0de27@users.noreply.github.com>
2022-09-29 13:12:38 -0700

    Streamtape domain (#222)

--- script.module.resolveurl/lib/resolveurl/plugins/streamtape.py
+++ script.module.resolveurl/lib/resolveurl/plugins/streamtape.py
@@ -27,9 +27,9 @@ class StreamTapeResolver(ResolveUrl):
     name = 'StreamTape'
     domains = ['streamtape.com', 'strtape.cloud', 'streamtape.net', 'streamta.pe', 'streamtape.site',
                'strcloud.link', 'strtpe.link', 'streamtape.cc', 'scloud.online', 'stape.fun',
-               'streamadblockplus.com', 'shavetape.cash']
+               'streamadblockplus.com', 'shavetape.cash', 'streamtape.to']
     pattern = r'(?://|\.)(s(?:tr)?(?:eam|have)?(?:ta?p?e?|cloud|adblockplus)\.' \
-              r'(?:com|cloud|net|pe|site|link|cc|online|fun|cash))/(?:e|v)/([0-9a-zA-Z]+)'
+              r'(?:com|cloud|net|pe|site|link|cc|online|fun|cash|to))/(?:e|v)/([0-9a-zA-Z]+)'
 
     def get_media_url(self, host, media_id):
         web_url = self.get_url(host, media_id)
