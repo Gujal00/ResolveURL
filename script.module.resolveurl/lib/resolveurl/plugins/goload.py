f18f77f6da3a980d195201afc026a28c1c873a72
shellc0de <50564043+shellc0de27@users.noreply.github.com>
2022-09-30 20:52:36 -0700

    Merge Vidstreaming into Goload (#223)

--- script.module.resolveurl/lib/resolveurl/plugins/goload.py
+++ script.module.resolveurl/lib/resolveurl/plugins/goload.py
@@ -28,8 +28,9 @@ from resolveurl.lib.pyaes import AESModeOfOperationCBC, Encrypter, Decrypter
 
 class GoloadResolver(ResolveUrl):
     name = 'GoLoad'
-    domains = ['goload.io', 'goload.pro', 'gogohd.net', 'streamani.net', 'gogo-play.net']
-    pattern = r'(?://|\.)((?:gogo-play|streamani|goload|gogohd)\.(?:io|pro|net))/' \
+    domains = ['goload.io', 'goload.pro', 'gogohd.net', 'streamani.net', 'gogo-play.net',
+               'vidstreaming.io']
+    pattern = r'(?://|\.)((?:gogo-play|streamani|goload|gogohd|vidstreaming)\.(?:io|pro|net))/' \
               r'(?:streaming|embed(?:plus)?|ajax|load)(?:\.php)?\?id=([a-zA-Z0-9-]+)'
     keys = ['37911490979715163134003223491201', '54674138327930866480207815084989']
     iv = six.ensure_binary('3134003223491201')
