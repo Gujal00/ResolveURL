5203921793151032295dedaf7f9f5e36471145ec
shellc0de <50564043+shellc0de27@users.noreply.github.com>
2022-09-30 20:51:40 -0700

    Add Goostream resolver (#224)

---
+++ script.module.resolveurl/lib/resolveurl/plugins/goostream.py
@@ -0,0 +1,25 @@
+"""
+    Plugin for ResolveURL
+    Copyright (C) 2022 shellc0de
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+from resolveurl.plugins.__resolve_generic__ import ResolveGeneric
+
+
+class GooStreamResolver(ResolveGeneric):
+    name = 'GooStream'
+    domains = ['goostream.net']
+    pattern = r'(?://|\.)(goostream\.net)/(?:embed-|e/|d/)?([0-9a-zA-Z]+)'
