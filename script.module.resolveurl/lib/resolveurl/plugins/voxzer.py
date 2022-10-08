b5f9bc1f11c6f2ffc28ce524a68753aaf65a78e6
gujal <17394059+Gujal00@users.noreply.github.com>
2022-10-09 10:51:44 +1300

    add voxzer resolver

---
+++ script.module.resolveurl/lib/resolveurl/plugins/voxzer.py
@@ -0,0 +1,36 @@
+"""
+    Plugin for ResolveURL
+    Copyright (C) 2022 gujal
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+from resolveurl.lib import helpers
+from resolveurl.plugins.__resolve_generic__ import ResolveGeneric
+
+
+class VoxzerResolver(ResolveGeneric):
+    name = 'Voxzer'
+    domains = ['voxzer.org']
+    pattern = r'(?://|\.)(voxzer.org)/view/([0-9a-zA-Z]+)'
+
+    def get_media_url(self, host, media_id):
+        return helpers.get_media_url(
+            self.get_url(host, media_id),
+            patterns=[r'''link":"(?P<url>[^"]+)'''],
+            generic_patterns=False
+        )
+
+    def get_url(self, host, media_id):
+        return self._default_get_url(host, media_id, 'https://player.{host}/list/{media_id}')
