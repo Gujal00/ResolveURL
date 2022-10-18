9dc9924abb5683fc1706b9f9a909cf3d7c65a218
shellc0de <50564043+shellc0de27@users.noreply.github.com>
2022-10-18 16:58:25 -0700

    Vidbom additional domain (#239)

--- script.module.resolveurl/lib/resolveurl/plugins/vidbom.py
+++ script.module.resolveurl/lib/resolveurl/plugins/vidbom.py
@@ -25,10 +25,10 @@ from resolveurl.resolver import ResolveUrl, ResolverError
 
 class VidBomResolver(ResolveUrl):
     name = 'VidBom'
-    domains = ['vidbom.com', 'vidbem.com', 'vidbm.com', 'vedpom.com', 'vedbom.com', 'vedbom.org',
+    domains = ['vidbom.com', 'vidbem.com', 'vidbm.com', 'vedpom.com', 'vedbom.com', 'vedbom.org', 'vedbam.xyz',
                'vadbom.com', 'vidbam.org', 'vadbam.com', 'myviid.com', 'myviid.net', 'myvid.com',
                'vidshare.com', 'vedsharr.com', 'vedshar.com', 'vedshare.com', 'vadshar.com', 'vidshar.org']
-    pattern = r'(?://|\.)((?:v[aie]d[bp][aoe]?m|myvii?d|v[aei]dshar[er]?)\.(?:com|net|org))(?::\d+)?/(?:embed[/-])?([A-Za-z0-9]+)'
+    pattern = r'(?://|\.)((?:v[aie]d[bp][aoe]?m|myvii?d|v[aei]dshar[er]?)\.(?:com|net|org|xyz))(?::\d+)?/(?:embed[/-])?([A-Za-z0-9]+)'
 
     def get_media_url(self, host, media_id):
         web_url = self.get_url(host, media_id)
