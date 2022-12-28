200bf95da2407a611a42a069742012cc54e444d5
gujal <17394059+Gujal00@users.noreply.github.com>
2022-12-28 15:09:13 +1300

    fix neohd resolver

--- script.module.resolveurl/lib/resolveurl/plugins/neohd.py
+++ script.module.resolveurl/lib/resolveurl/plugins/neohd.py
@@ -16,21 +16,44 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
-from resolveurl.plugins.__resolve_generic__ import ResolveGeneric
+import re
+import json
+import codecs
+import time
+from resolveurl import common
 from resolveurl.lib import helpers
+from resolveurl.lib.jscrypto import jscrypto
+from resolveurl.resolver import ResolveUrl, ResolverError
 
 
-class NeoHDResolver(ResolveGeneric):
+class NeoHDResolver(ResolveUrl):
     name = 'NeoHD'
-    domains = ['neohd.xyz']
-    pattern = r'(?://|\.)(neohd\.xyz)/embed/([0-9a-f-]+)'
+    domains = ['neohd.xyz', 'ninjahd.one']
+    pattern = r'(?://|\.)((?:neo|ninja)hd\.(?:xyz|one))/embed/([0-9a-zA-Z-]+)'
 
     def get_media_url(self, host, media_id):
-        return helpers.get_media_url(
-            self.get_url(host, media_id),
-            patterns=[r'''"file":"(?P<url>[^"]+).+?"label":"(?P<label>[^"]+)'''],
-            generic_patterns=False
-        ).replace(' ', '%20')
+        web_url = self.get_url(host, media_id)
+        headers = {'User-Agent': common.RAND_UA}
+        html = self.net.http_GET(web_url, headers=headers).content
+        r = re.search(r"var\s*playerConfig\s*=\s*([^;]+)", html)
+        if r:
+            data = json.loads(r.group(1))
+            ct = data.get('ct', False)
+            salt = codecs.decode(data.get('s'), 'hex')
+            html2 = jscrypto.decode(ct, 'F1r3b4Ll_GDP~5H', salt)
+            html2 = html2[1:-1].replace('\\"', '"')
+            s = re.search(r'apiQuery":"([^"]+)', html2)
+            if s:
+                headers.update({'Referer': web_url, 'X-Requested-With': 'XMLHttpRequest'})
+                aurl = 'https://{0}/api/?{1}&_={2}'.format(host, s.group(1), int(time.time() * 1000))
+                jd = json.loads(self.net.http_GET(aurl, headers=headers).content)
+                url = jd.get('sources')[0].get('file').replace(' ', '%20')
+                if url.startswith('//'):
+                    url = 'https:' + url
+                headers.pop('X-Requested-With')
+                return url + helpers.append_headers(headers)
+
+        raise ResolverError('File Not Found or Removed')
 
     def get_url(self, host, media_id):
         return self._default_get_url(host, media_id, template='https://{host}/embed/{media_id}')
