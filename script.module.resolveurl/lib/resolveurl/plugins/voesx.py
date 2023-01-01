c90857ce8bc0ca51f5f7bb391b30931834ad6688
keypusher <40206257+k3ypusher@users.noreply.github.com>
2023-01-01 13:31:57 +0100

    Update voesx.py (#293)
    VOE additional domain.

--- script.module.resolveurl/lib/resolveurl/plugins/voesx.py
+++ script.module.resolveurl/lib/resolveurl/plugins/voesx.py
@@ -33,14 +33,15 @@ class VoeResolver(ResolveUrl):
                'housecardsummerbutton.com', 'fraudclatterflyingcar.com',
                'bigclatterhomesguideservice.com', 'uptodatefinishconferenceroom.com',
                'realfinanceblogcenter.com', 'tinycat-voe-fashion.com',
-               '20demidistance9elongations.com', 'telyn610zoanthropy.com', 'toxitabellaeatrebates306.com']
+               '20demidistance9elongations.com', 'telyn610zoanthropy.com', 'toxitabellaeatrebates306.com',
+               'greaseball6eventual20.com']
     domains += ['voeunblock{}.com'.format(x) for x in range(1, 11)]
     pattern = r'(?://|\.)((?:audaciousdefaulthouse|launchreliantcleaverriver|' \
               r'reputationsheriffkennethsand|fittingcentermondaysunday|' \
               r'housecardsummerbutton|fraudclatterflyingcar|' \
               r'bigclatterhomesguideservice|uptodatefinishconferenceroom|' \
               r'realfinanceblogcenter|tinycat-voe-fashion|20demidistance9elongations|' \
-              r'telyn610zoanthropy|toxitabellaeatrebates306|' \
+              r'telyn610zoanthropy|toxitabellaeatrebates306|greaseball6eventual20|' \
               r'(?:v-?o-?e)?(?:-?un-?bl[o0]?c?k\d{0,2})?(?:-?voe)?)\.(?:sx|com|net))/' \
               r'(?:e/)?([0-9A-Za-z]+)'
 
