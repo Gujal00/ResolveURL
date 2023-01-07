f3a586e2647fa43251b18b8adbbf38e93bc085fa
shellc0de <shellc0de69@protonmail.com>
2023-01-06 17:13:33 -0800

    Add Embedgram resolver

---
+++ script.module.resolveurl/lib/resolveurl/plugins/embedgram.py
@@ -0,0 +1,36 @@
+"""
+    Plugin for ResolveURL
+    Copyright (C) 2023 shellc0de
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+from resolveurl.lib import helpers
+from resolveurl.plugins.__resolve_generic__ import ResolveGeneric
+
+
+class EmbedGramResolver(ResolveGeneric):
+    name = 'EmbedGram'
+    domains = ['embedgram.com']
+    pattern = r'(?://|\.)(embedgram\.com)/(?:v|f)/([0-9a-zA-Z]+)'
+
+    def get_media_url(self, host, media_id):
+        return helpers.get_media_url(
+            self.get_url(host, media_id),
+            patterns=[r'''source\s*src=(?P<url>[^\s]+)\s*title=["'](?P<label>[^"']+)'''],
+            generic_patterns=False
+        )
+
+    def get_url(self, host, media_id):
+        return self._default_get_url(host, media_id, template='https://{host}/v/{media_id}')
