53833605988dd28c0d2cf832fc3fc85844cdb235
keypusher <40206257+k3ypusher@users.noreply.github.com>
2023-01-08 04:36:22 +0100

    Update voesx.py (#297)
    Adds new domain 745mingiestblissfully.com

--- script.module.resolveurl/lib/resolveurl/plugins/voesx.py
+++ script.module.resolveurl/lib/resolveurl/plugins/voesx.py
@@ -34,7 +34,7 @@ class VoeResolver(ResolveUrl):
                'bigclatterhomesguideservice.com', 'uptodatefinishconferenceroom.com',
                'realfinanceblogcenter.com', 'tinycat-voe-fashion.com',
                '20demidistance9elongations.com', 'telyn610zoanthropy.com', 'toxitabellaeatrebates306.com',
-               'greaseball6eventual20.com']
+               'greaseball6eventual20.com', '745mingiestblissfully.com']
     domains += ['voeunblock{}.com'.format(x) for x in range(1, 11)]
     pattern = r'(?://|\.)((?:audaciousdefaulthouse|launchreliantcleaverriver|' \
               r'reputationsheriffkennethsand|fittingcentermondaysunday|' \
@@ -42,6 +42,7 @@ class VoeResolver(ResolveUrl):
               r'bigclatterhomesguideservice|uptodatefinishconferenceroom|' \
               r'realfinanceblogcenter|tinycat-voe-fashion|20demidistance9elongations|' \
               r'telyn610zoanthropy|toxitabellaeatrebates306|greaseball6eventual20|' \
+              r'745mingiestblissfully|' \
               r'(?:v-?o-?e)?(?:-?un-?bl[o0]?c?k\d{0,2})?(?:-?voe)?)\.(?:sx|com|net))/' \
               r'(?:e/)?([0-9A-Za-z]+)'
 
