f04fda20a997ae44e1d4acfe7450ffbc9271189a
keypusher <40206257+k3ypusher@users.noreply.github.com>
2023-01-14 19:52:07 +0100

    Update voesx.py
    oopsie

--- script.module.resolveurl/lib/resolveurl/plugins/voesx.py
+++ script.module.resolveurl/lib/resolveurl/plugins/voesx.py
@@ -42,7 +42,7 @@ class VoeResolver(ResolveUrl):
               r'bigclatterhomesguideservice|uptodatefinishconferenceroom|' \
               r'realfinanceblogcenter|tinycat-voe-fashion|20demidistance9elongations|' \
               r'telyn610zoanthropy|toxitabellaeatrebates306|greaseball6eventual20|' \
-              r'745mingiestblissfully|19turanosephantasia' \
+              r'745mingiestblissfully|19turanosephantasia|' \
               r'(?:v-?o-?e)?(?:-?un-?bl[o0]?c?k\d{0,2})?(?:-?voe)?)\.(?:sx|com|net))/' \
               r'(?:e/)?([0-9A-Za-z]+)'
 
