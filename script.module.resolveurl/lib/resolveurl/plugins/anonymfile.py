8c37ebe1d7714a74b2308e9c5b9f9349a4da5358
gujal <17394059+Gujal00@users.noreply.github.com>
2023-01-23 09:27:09 +1300

    Add Anonymfile resolver

---
+++ script.module.resolveurl/lib/resolveurl/plugins/anonymfile.py
@@ -0,0 +1,38 @@
+"""
+    Plugin for ResolveURL
+    Copyright (c) 2022 gujal
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+from resolveurl.plugins.__resolve_generic__ import ResolveGeneric
+from resolveurl.lib import helpers
+
+
+class AnonymFileResolver(ResolveGeneric):
+    name = 'AnonymFile'
+    domains = ['anonymfile.com']
+    pattern = r'(?://|\.)(anonymfile\.com)/([0-9a-zA-Z]+)'
+
+    def get_media_url(self, host, media_id):
+        return helpers.get_media_url(
+            self.get_url(host, media_id),
+            patterns=[r'#download.+?href="(?P<url>[^"]+)'],
+            generic_patterns=False,
+            referer=False,
+            result_blacklist=['.zip', '.rar', '.7z']
+        ).replace(' ', '%20')
+
+    def get_url(self, host, media_id):
+        return self._default_get_url(host, media_id, template='https://{host}/{media_id}/')
