e9be992af70f41e8e538d3aa4556542ac8d9660e
gujal <17394059+Gujal00@users.noreply.github.com>
2023-02-03 19:22:07 +1300

    fix tvlogy

--- script.module.resolveurl/lib/resolveurl/plugins/tvlogy.py
+++ script.module.resolveurl/lib/resolveurl/plugins/tvlogy.py
@@ -19,6 +19,7 @@
 import re
 import random
 import base64
+import json
 from resolveurl.lib import helpers
 from resolveurl import common
 from resolveurl.resolver import ResolveUrl, ResolverError
@@ -32,49 +33,59 @@ class TVLogyResolver(ResolveUrl):
     def get_media_url(self, host, media_id):
         embeds = ['http://bestarticles.me/', 'http://tellygossips.net/', 'http://tvarticles.org/']
         web_url = self.get_url(host, media_id)
-        headers = {'User-Agent': common.FF_USER_AGENT,
-                   'Referer': random.choice(embeds)}
+        headers = {'User-Agent': common.FF_USER_AGENT}
 
-        html = self.net.http_GET(web_url, headers=headers).content
+        if 'hls.' in web_url:
+            headers.update({'X-Requested-With': 'XMLHttpRequest'})
+            pdata = {'hash': media_id,
+                     'r': 'https://{0}/'.format(host)}
+            resp = json.loads(self.net.http_POST(web_url, form_data=pdata, headers=headers).content)
+            str_url = resp.get('videoSource')
+            if str_url:
+                headers.pop('X-Requested-With')
+                return str_url + helpers.append_headers(headers)
+        else:
+            headers.update({'Referer': random.choice(embeds)})
+            html = self.net.http_GET(web_url, headers=headers).content
 
-        if 'Not Found' in html:
-            raise ResolverError('File Removed')
+            if 'Not Found' in html:
+                raise ResolverError('File Removed')
 
-        if 'Video is processing' in html:
-            raise ResolverError('File still being processed')
+            if 'Video is processing' in html:
+                raise ResolverError('File still being processed')
 
-        html += helpers.get_packed_data(html)
-        packed = re.search(r"JuicyCodes\.Run\((.+?)\)", html, re.I)
-        if packed:
-            from base64 import b64decode
-            packed = packed.group(1).replace('"', '').replace('+', '')
-            packed = b64decode(packed.encode('ascii'))
-            html += '%s</script>' % packed.decode('latin-1').strip()
+            html += helpers.get_packed_data(html)
+            packed = re.search(r"JuicyCodes\.Run\((.+?)\)", html, re.I)
+            if packed:
+                from base64 import b64decode
+                packed = packed.group(1).replace('"', '').replace('+', '')
+                packed = b64decode(packed.encode('ascii'))
+                html += '%s</script>' % packed.decode('latin-1').strip()
 
-        source = helpers.scrape_sources(html, patterns=[r'''"file":\s*"(?P<url>[^"]+\.(?:m3u8|mp4|txt))"'''])
-        if source:
-            headers.update({'Referer': web_url, 'Accept': '*/*'})
-            vsrv = re.search(r'//(\d+)/', source[0][1])
-            if vsrv:
-                source = re.sub(r"//\d+/", "//{0}/".format(host), source[0][1]) + '?s={0}&d='.format(vsrv.group(1))
-                disk = re.findall(r'videoDisk":\s*"([^"]+)', html)
-                if disk:
-                    disk = base64.b64encode(disk[0].encode('utf-8')).decode('utf-8')
-                    source += disk
-            else:
-                source = source[0][1]
-            html = self.net.http_GET(source, headers=headers).content
-            sources = re.findall(r'RESOLUTION=\d+x(\d+)\n([^\n]+)', html)
-            src = helpers.pick_source(helpers.sort_sources_list(sources))
-            if not src.startswith('http'):
-                src = re.sub(source.split('/')[-1], src, source)
-            return src + helpers.append_headers(headers)
+            source = helpers.scrape_sources(html, patterns=[r'''"file":\s*"(?P<url>[^"]+\.(?:m3u8|mp4|txt))"'''])
+            if source:
+                headers.update({'Referer': web_url, 'Accept': '*/*'})
+                vsrv = re.search(r'//(\d+)/', source[0][1])
+                if vsrv:
+                    source = re.sub(r"//\d+/", "//{0}/".format(host), source[0][1]) + '?s={0}&d='.format(vsrv.group(1))
+                    disk = re.findall(r'videoDisk":\s*"([^"]+)', html)
+                    if disk:
+                        disk = base64.b64encode(disk[0].encode('utf-8')).decode('utf-8')
+                        source += disk
+                else:
+                    source = source[0][1]
+                html = self.net.http_GET(source, headers=headers).content
+                sources = re.findall(r'RESOLUTION=\d+x(\d+)\n([^\n]+)', html)
+                src = helpers.pick_source(helpers.sort_sources_list(sources))
+                if not src.startswith('http'):
+                    src = re.sub(source.split('/')[-1], src, source)
+                return src + helpers.append_headers(headers)
 
         raise ResolverError('Video not found')
 
     def get_url(self, host, media_id):
         if 'hls.' in host:
-            template = 'https://{host}/player/index.php?data={media_id}'
+            template = 'https://{host}/player/index.php?data={media_id}&do=getVideo'
         else:
             template = 'https://{host}/{media_id}'
         return self._default_get_url(host, media_id, template=template)
