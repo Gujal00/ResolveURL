029299fedaab9320b9af6e8106a53a9a495ebb29
shellc0de <50564043+shellc0de27@users.noreply.github.com>
2023-02-08 20:17:52 -0800

    Filemoon domain (#344)

--- script.module.resolveurl/lib/resolveurl/plugins/filemoon.py
+++ script.module.resolveurl/lib/resolveurl/plugins/filemoon.py
@@ -23,8 +23,8 @@ from resolveurl.lib import helpers
 class FileMoonResolver(ResolveGeneric):
     name = 'FileMoon'
     domains = ['filemoon.sx', 'filemoon.to', 'filemoon.in', 'filemoon.link', 'filemoon.nl',
-               'filemoon.wf']
-    pattern = r'(?://|\.)(filemoon\.(?:sx|to|in|link|nl|wf))/(?:e|d)/([0-9a-zA-Z]+)'
+               'filemoon.wf', 'cinegrab.com']
+    pattern = r'(?://|\.)((?:filemoon|cinegrab)\.(?:sx|to|in|link|nl|wf|com))/(?:e|d)/([0-9a-zA-Z]+)'
 
     def get_media_url(self, host, media_id):
         return helpers.get_media_url(
