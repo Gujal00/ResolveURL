bddba4bef4ad1889f78929de936b5dec08ceb359
shellc0de <50564043+shellc0de27@users.noreply.github.com>
2023-02-17 10:54:13 -0800

    Pandafiles adjustment (#360)

--- script.module.resolveurl/lib/resolveurl/plugins/pandafiles.py
+++ script.module.resolveurl/lib/resolveurl/plugins/pandafiles.py
@@ -50,9 +50,9 @@ class PandaFilesResolver(ResolveUrl):
             headers.update({'verifypeer': 'false'})
             query = urllib_parse.parse_qsl(urllib_parse.urlparse(source.group(1)).query)
             if not query:
-                return source.group(1) + helpers.append_headers(headers)
+                return source.group(1).replace(' ', '%20') + helpers.append_headers(headers)
             src = base64.b64decode(query[0][1]).decode('utf-8')
-            return src + helpers.append_headers(headers)
+            return src.replace(' ', '%20') + helpers.append_headers(headers)
 
         raise ResolverError('File Not Found or removed')
 
