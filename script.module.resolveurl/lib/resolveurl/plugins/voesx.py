19b96e668823cc88bcd7b89cb83fd17bd2be063a
Keypusher <keypusher@icloud.com>
2023-02-25 02:15:28 +0100

    Update voesx.py

--- script.module.resolveurl/lib/resolveurl/plugins/voesx.py
+++ script.module.resolveurl/lib/resolveurl/plugins/voesx.py
@@ -36,7 +36,8 @@ class VoeResolver(ResolveUrl):
                '20demidistance9elongations.com', 'telyn610zoanthropy.com', 'toxitabellaeatrebates306.com',
                'greaseball6eventual20.com', '745mingiestblissfully.com', '19turanosephantasia.com',
                '30sensualizeexpression.com', '321naturelikefurfuroid.com', '449unceremoniousnasoseptal.com',
-               'guidon40hyporadius9.com', 'cyamidpulverulence530.com', 'boonlessbestselling244.com']
+               'guidon40hyporadius9.com', 'cyamidpulverulence530.com', 'boonlessbestselling244.com',
+               'antecoxalbobbing1010.com']
     domains += ['voeunblock{}.com'.format(x) for x in range(1, 11)]
     pattern = r'(?://|\.)((?:audaciousdefaulthouse|launchreliantcleaverriver|' \
               r'reputationsheriffkennethsand|fittingcentermondaysunday|' \
@@ -46,7 +47,7 @@ class VoeResolver(ResolveUrl):
               r'telyn610zoanthropy|toxitabellaeatrebates306|greaseball6eventual20|' \
               r'745mingiestblissfully|19turanosephantasia|30sensualizeexpression|' \
               r'321naturelikefurfuroid|449unceremoniousnasoseptal|guidon40hyporadius9|' \
-              r'cyamidpulverulence530|boonlessbestselling244|' \
+              r'cyamidpulverulence530|boonlessbestselling244|antecoxalbobbing1010|' \
               r'(?:v-?o-?e)?(?:-?un-?bl[o0]?c?k\d{0,2})?(?:-?voe)?)\.(?:sx|com|net))/' \
               r'(?:e/)?([0-9A-Za-z]+)'
 
