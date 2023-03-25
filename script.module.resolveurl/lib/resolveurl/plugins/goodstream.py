08117542ae9ae11ac3df3f2f20035baa2d0a173a
gujal <17394059+Gujal00@users.noreply.github.com>
2023-03-26 06:40:01 +1300

    fix goodstream

--- script.module.resolveurl/lib/resolveurl/plugins/goodstream.py
+++ script.module.resolveurl/lib/resolveurl/plugins/goodstream.py
@@ -17,12 +17,21 @@
 """
 
 from resolveurl.plugins.__resolve_generic__ import ResolveGeneric
+from resolveurl.lib import helpers
 
 
 class GoodStreamResolver(ResolveGeneric):
     name = 'GoodStream'
     domains = ['goodstream.uno']
     pattern = r'(?://|\.)(goodstream\.uno)/video/embed/([0-9a-zA-Z]+)'
+
+    def get_media_url(self, host, media_id):
+        return helpers.get_media_url(
+            self.get_url(host, media_id),
+            patterns=[r'''file:\s*["'](?P<url>[^"']+)'''],
+            generic_patterns=False,
+            referer=False,
+        )
 
     def get_url(self, host, media_id):
         return self._default_get_url(host, media_id, template='https://{host}/video/embed/{media_id}')
