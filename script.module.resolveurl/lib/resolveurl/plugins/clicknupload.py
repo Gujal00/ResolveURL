d1889f15b52ebebea60e4b57ab1a220916e7fa45
gujal <17394059+Gujal00@users.noreply.github.com>
2023-04-03 09:43:49 +1200

    update clicknupload, fixes #404

--- script.module.resolveurl/lib/resolveurl/plugins/clicknupload.py
+++ script.module.resolveurl/lib/resolveurl/plugins/clicknupload.py
@@ -58,7 +58,7 @@ class ClickNUploadResolver(ResolveUrl):
         return
 
     def get_url(self, host, media_id):
-        return self._default_get_url(host, media_id, template='https://{host}/{media_id}')
+        return self._default_get_url(host, media_id, template='https://clicknupload.red/{media_id}')
 
     @classmethod
     def isPopup(self):
