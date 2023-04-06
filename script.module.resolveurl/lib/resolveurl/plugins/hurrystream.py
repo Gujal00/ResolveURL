59479fe44d59ce8aab5283d3da469a8a7c8be96c
shellc0de <shellc0de69@protonmail.com>
2023-04-05 18:01:22 -0700

    Hurrystream Fix

--- script.module.resolveurl/lib/resolveurl/plugins/hurrystream.py
+++ script.module.resolveurl/lib/resolveurl/plugins/hurrystream.py
@@ -28,7 +28,8 @@ class HurryStreamResolver(ResolveGeneric):
     def get_media_url(self, host, media_id):
         return helpers.get_media_url(
             self.get_url(host, media_id),
-            patterns=[r'''sources:\s*\[{src:\s*["'](?P<url>[^"']+)''']
+            patterns=[r'''sources:\s*\[{src:\s*["'](?P<url>[^"']+)''',
+                      r'''file:\s*"(?P<url>[^"]+)''']
         )
 
     def get_url(self, host, media_id):
