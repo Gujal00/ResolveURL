0ee9a9eb2a32e5b4c320467916f074856df63389
gujal <17394059+Gujal00@users.noreply.github.com>
2023-04-14 13:03:18 +1200

    add vidspeed resolver fixes #419

---
+++ script.module.resolveurl/lib/resolveurl/plugins/vidspeed.py
@@ -0,0 +1,36 @@
+"""
+    Plugin for ResolveURL
+    Copyright (C) 2023 gujal
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+from resolveurl.plugins.__resolve_generic__ import ResolveGeneric
+from resolveurl.lib import helpers
+
+
+class VidSpeedResolver(ResolveGeneric):
+    name = 'VidSpeed'
+    domains = ['vidspeed.cc']
+    pattern = r'(?://|\.)(vidspeed\.cc)/(?:embed-)?([0-9a-zA-Z]+)'
+
+    def get_media_url(self, host, media_id):
+        return helpers.get_media_url(
+            self.get_url(host, media_id),
+            patterns=[r'''file:\s*"(?P<url>[^"]+)'''],
+            referer=False
+        )
+
+    def get_url(self, host, media_id):
+        return self._default_get_url(host, media_id, template='https://{host}/embed-{media_id}.html')
