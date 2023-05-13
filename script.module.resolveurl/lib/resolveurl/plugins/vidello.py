8843a0264a4cb01fe9a3496f62c5310e19847557
shellc0de <shellc0de69@protonmail.com>
2023-05-13 11:58:10 -0700

    Add Vidello resolver

---
+++ script.module.resolveurl/lib/resolveurl/plugins/vidello.py
@@ -0,0 +1,32 @@
+"""
+    Plugin for ResolveURL
+    Copyright (C) 2023 shellc0de
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+from resolveurl.lib import helpers
+from resolveurl.plugins.__resolve_generic__ import ResolveGeneric
+
+
+class VidelloResolver(ResolveGeneric):
+    name = 'Vidello'
+    domains = ['vidello.net']
+
+    def get_media_url(self, host, media_id):
+        return helpers.get_media_url(
+            self.get_url(host, media_id),
+            patterns=[r'''sources:\s*\[{src:\s*["'](?P<url>[^"']+)'''],
+            generic_patterns=False
+        )
