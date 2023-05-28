86a6e8261e5a7e9a708f659eca9c1b61539c4fb8
DWH-WFC <45105289+DWH-WFC@users.noreply.github.com>
2023-05-28 10:46:47 +0200

    Update streamhub.py
    https://streamhub.to/87htws5prcf6

--- script.module.resolveurl/lib/resolveurl/plugins/streamhub.py
+++ script.module.resolveurl/lib/resolveurl/plugins/streamhub.py
@@ -29,7 +29,8 @@ class StreamHubResolver(ResolveGeneric):
         return helpers.get_media_url(
             self.get_url(host, media_id),
             patterns=[r'''sources:\s*\[{src:\s*"(?P<url>[^"]+)",'''],
-            generic_patterns=False
+            generic_patterns=False,
+            referer=False
         )
 
     def get_url(self, host, media_id):
