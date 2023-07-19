681dc2e5c3db4bad78fefb00996afe8f2e03230b
bassemhelal18 <77381611+bassemhelal18@users.noreply.github.com>
2023-07-20 00:22:50 +0300

    Update streamwish.py

--- script.module.resolveurl/lib/resolveurl/plugins/streamwish.py
+++ script.module.resolveurl/lib/resolveurl/plugins/streamwish.py
@@ -23,9 +23,9 @@ from resolveurl.plugins.__resolve_generic__ import ResolveGeneric
 class StreamWishResolver(ResolveGeneric):
     name = 'StreamWish'
     domains = ['streamwish.com', 'streamwish.to', 'ajmidyad.sbs', 'khadhnayad.sbs', 'yadmalik.sbs',
-               'hayaatieadhab.sbs', 'kharabnahs.sbs', 'atabkhha.sbs', 'atabknha.sbs', 'atabknhk.sbs']
+               'hayaatieadhab.sbs', 'kharabnahs.sbs', 'atabkhha.sbs', 'atabknha.sbs', 'atabknhk.sbs', 'atabknhs.sbs']
     pattern = r'(?://|\.)((?:streamwish|ajmidyad|khadhnayad|yadmalik|hayaatieadhab|kharabnahs|' \
-              r'atabkhha|atabknha|atabknhk)' \
+              r'atabkhha|atabknha|atabknhk|atabknhs)' \
               r'\.(?:com|to|sbs))/(?:e/|f/)?([0-9a-zA-Z]+)'
 
     def get_media_url(self, host, media_id):
