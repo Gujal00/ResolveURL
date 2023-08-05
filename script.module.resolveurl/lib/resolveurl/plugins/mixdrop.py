6f7a859df56888f6e33dcd4393adbf68b119aefd
shellc0de <shellc0de69@protonmail.com>
2023-08-05 11:01:32 -0700

    Mixdrop domain

--- script.module.resolveurl/lib/resolveurl/plugins/mixdrop.py
+++ script.module.resolveurl/lib/resolveurl/plugins/mixdrop.py
@@ -25,7 +25,8 @@ from resolveurl.lib import helpers
 class MixDropResolver(ResolveUrl):
     name = 'MixDrop'
     domains = ['mixdrop.co', 'mixdrop.to', 'mixdrop.sx', 'mixdrop.bz', 'mixdrop.ch',
-               'mixdrp.co', 'mixdrp.to', 'mixdrop.gl', 'mixdrop.club', 'mixdroop.bz']
+               'mixdrp.co', 'mixdrp.to', 'mixdrop.gl', 'mixdrop.club', 'mixdroop.bz',
+               'mixdroop.co']
     pattern = r'(?://|\.)(mixdro*p\.(?:c[ho]|to|sx|bz|gl|club))/(?:f|e)/(\w+)'
 
     def get_media_url(self, host, media_id):
