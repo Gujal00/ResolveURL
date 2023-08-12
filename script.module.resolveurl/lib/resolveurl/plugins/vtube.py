3893fa09ebb735f11c1eed40bb1d664c614c713c
shellc0de <shellc0de69@protonmail.com>
2023-08-12 11:16:57 -0700

    Vtube adjustment

--- script.module.resolveurl/lib/resolveurl/plugins/vtube.py
+++ script.module.resolveurl/lib/resolveurl/plugins/vtube.py
@@ -33,4 +33,4 @@ class VTubeResolver(ResolveGeneric):
         )
 
     def get_url(self, host, media_id):
-        return self._default_get_url(host, media_id, template='https://vtbe.to/embed-{media_id}.html')
+        return self._default_get_url(host, media_id, template='https://vtbe.to/{media_id}.html')
