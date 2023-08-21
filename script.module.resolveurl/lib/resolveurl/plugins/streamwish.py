e6cefcad71799f6c9d1a51e6f22ee8842487e7b9
bassemhelal18 <77381611+bassemhelal18@users.noreply.github.com>
2023-08-21 14:24:20 +0300

    Update streamwish.py

--- script.module.resolveurl/lib/resolveurl/plugins/streamwish.py
+++ script.module.resolveurl/lib/resolveurl/plugins/streamwish.py
@@ -27,11 +27,11 @@ class StreamWishResolver(ResolveGeneric):
                'hayaatieadhab.sbs', 'kharabnahs.sbs', 'atabkhha.sbs', 'atabknha.sbs', 'atabknhk.sbs',
                'atabknhs.sbs', 'abkrzkr.sbs', 'abkrzkz.sbs', 'wishembed.pro', 'mwish.pro',
                'awish.pro', 'dwish.pro', 'vidmoviesb.xyz', 'embedwish.com', 'cilootv.store',
-               'tuktukcinema.store', 'doodporn.xyz', 'ankrzkz.sbs','volvovideo.top']
+               'tuktukcinema.store', 'doodporn.xyz', 'ankrzkz.sbs','volvovideo.top', 'streamwish.site']
     pattern = r'(?://|\.)((?:streamwish|ajmidyad|khadhnayad|yadmalik|hayaatieadhab|kharabnahs|' \
               r'atabkhha|atabknha|atabknhk|atabknhs|abkrzkr|abkrzkz|wishembed|[mad]wish|vidmoviesb|' \
               r'cilootv|tuktukcinema|embedwish|doodporn|ankrzkz|volvovideo)' \
-              r'\.(?:com|to|sbs|pro|xyz|store|top))/(?:e/|f/)?([0-9a-zA-Z$:/.]+)'
+              r'\.(?:com|to|sbs|pro|xyz|store|top|site))/(?:e/|f/)?([0-9a-zA-Z$:/.]+)'
 
     def get_media_url(self, host, media_id):
         if '$$' in media_id:
