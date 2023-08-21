f6030ae1d3bbc30823d20fde8938b6f17cfcc080
DenDyGH <29734746+DenDyGH@users.noreply.github.com>
2023-08-21 11:09:37 +0200

    Update voesx.py

--- script.module.resolveurl/lib/resolveurl/plugins/voesx.py
+++ script.module.resolveurl/lib/resolveurl/plugins/voesx.py
@@ -38,7 +38,7 @@ class VoeResolver(ResolveUrl):
                'guidon40hyporadius9.com', 'cyamidpulverulence530.com', 'boonlessbestselling244.com',
                'antecoxalbobbing1010.com', 'matriculant401merited.com', 'scatch176duplicities.com',
                'availedsmallest.com', 'counterclockwisejacky.com', 'simpulumlamerop.com',
-               'metagnathtuggers.com', 'gamoneinterrupted.com', 'chromotypic.com']
+               'metagnathtuggers.com', 'gamoneinterrupted.com', 'chromotypic.com', 'crownmakermacaronicism.com']
     domains += ['voeunblock{}.com'.format(x) for x in range(1, 11)]
     pattern = r'(?://|\.)((?:audaciousdefaulthouse|launchreliantcleaverriver|' \
               r'reputationsheriffkennethsand|fittingcentermondaysunday|' \
@@ -51,7 +51,7 @@ class VoeResolver(ResolveUrl):
               r'cyamidpulverulence530|boonlessbestselling244|antecoxalbobbing1010|' \
               r'matriculant401merited|scatch176duplicities|availedsmallest|' \
               r'counterclockwisejacky|simpulumlamerop|wolfdyslectic|' \
-              r'metagnathtuggers|gamoneinterrupted|chromotypic|' \
+              r'metagnathtuggers|gamoneinterrupted|chromotypic|crownmakermacaronicism|' \
               r'(?:v-?o-?e)?(?:-?un-?bl[o0]?c?k\d{0,2})?(?:-?voe)?)\.(?:sx|com|net))/' \
               r'(?:e/)?([0-9A-Za-z]+)'
 
