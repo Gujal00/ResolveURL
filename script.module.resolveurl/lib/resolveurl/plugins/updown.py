91b7972b25bc68e69b28b2d5d27495b6a6b21ffa
bassemhelal18 <77381611+bassemhelal18@users.noreply.github.com>
2023-09-04 20:22:12 +0300

    Update updown.py

--- script.module.resolveurl/lib/resolveurl/plugins/updown.py
+++ script.module.resolveurl/lib/resolveurl/plugins/updown.py
@@ -23,8 +23,8 @@ from resolveurl.lib import helpers
 
 class UpDownResolver(ResolveGeneric):
     name = 'UpDown'
-    domains = ['updown.sbs']
-    pattern = r'(?://|\.)(.*?updown\.sbs)/(?:embed-)?([0-9a-zA-Z]+)'
+    domains = ['updown.sbs','updown.icu']
+    pattern = r'(?://|\.)(.*?updown\.(?:sbs|icu))/(?:embed-)?([0-9a-zA-Z]+)'
 
     def get_media_url(self, host, media_id):
         return helpers.get_media_url(
