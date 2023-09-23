7a942f6c67934a31957c6e2e4287bceb9fe2c4a1
bassemhelal18 <77381611+bassemhelal18@users.noreply.github.com>
2023-09-23 14:15:13 +0300

    Update filelions.py

--- script.module.resolveurl/lib/resolveurl/plugins/filelions.py
+++ script.module.resolveurl/lib/resolveurl/plugins/filelions.py
@@ -24,10 +24,10 @@ class FileLionsResolver(ResolveGeneric):
     name = 'FileLions'
     domains = ['filelions.com', 'filelions.to', 'ajmidyadfihayh.sbs', 'alhayabambi.sbs', 'techradar.ink',
                'moflix-stream.click', 'azipcdn.com', 'mlions.pro', 'alions.pro', 'dlions.pro',
-               'filelions.live', 'motvy55.store', 'filelions.xyz', 'lumiawatch.top']
+               'filelions.live', 'motvy55.store', 'filelions.xyz', 'lumiawatch.top', 'filelions.online']
     pattern = r'(?://|\.)((?:filelions|ajmidyadfihayh|alhayabambi|techradar|moflix-stream|azipcdn|motvy55|' \
               r'[mad]lions|lumiawatch)\.' \
-              r'(?:com|to|sbs|ink|click|pro|live|store|xyz|top))/(?:v|f|d)/([0-9a-zA-Z]+)'
+              r'(?:com|to|sbs|ink|click|pro|live|store|xyz|top|online))/(?:v|f|d)/([0-9a-zA-Z]+)'
 
     def get_media_url(self, host, media_id):
         return helpers.get_media_url(
