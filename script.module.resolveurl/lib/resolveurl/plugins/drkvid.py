f0b42c963e20cf79d5cdcd4745ca09d3f354be3d
bassemhelal18 <77381611+bassemhelal18@users.noreply.github.com>
2023-11-06 20:33:19 +0200

    Update drkvid.py

--- script.module.resolveurl/lib/resolveurl/plugins/drkvid.py
+++ script.module.resolveurl/lib/resolveurl/plugins/drkvid.py
@@ -23,8 +23,8 @@ from resolveurl.lib import helpers
 
 class DrkVidResolver(ResolveGeneric):
     name = 'DrkVid'
-    domains = ['drkvid.site', 'allviid.online','telvod.site','allviid.info']
-    pattern = r'(?://|\.)(.*?\.(?:drkvid|allviid|telvod)\.(?:site|online|info))/(?:embed-)?([0-9a-zA-Z]+)'
+    domains = ['drkvid.site', 'allviid.online','telvod.site','allviid.info', 'allviidss.buzz']
+    pattern = r'(?://|\.)(.*?\.(?:drkvid|allviid|telvod|allviidss)\.(?:site|online|info|buzz))/(?:embed-)?([0-9a-zA-Z]+)'
 
     def get_media_url(self, host, media_id):
         return helpers.get_media_url(
