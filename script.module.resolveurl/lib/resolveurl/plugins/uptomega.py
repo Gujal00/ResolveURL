0908dddc8d64394ee94e78db19eec16236dd2788
gujal <17394059+Gujal00@users.noreply.github.com>
2024-01-04 12:00:41 +1300

    new resolver uptomega

---
+++ script.module.resolveurl/lib/resolveurl/plugins/uptomega.py
@@ -0,0 +1,25 @@
+"""
+    Plugin for ResolveURL
+    Copyright (C) 2024 gujal
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+from resolveurl.plugins.__resolve_generic__ import ResolveGeneric
+
+
+class UptoMegaResolver(ResolveGeneric):
+    name = 'UptoMega'
+    domains = ['uptomega.net']
+    pattern = r'(?://|\.)(uptomega\.net)/(?:embed-)?([0-9a-zA-Z]+)'
