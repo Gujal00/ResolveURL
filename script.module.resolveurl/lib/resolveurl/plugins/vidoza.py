feced5c4701a522602b37ead150c13e901ae78a5
MrDini123 <16760748+MrDini123@users.noreply.github.com>
2024-01-13 21:21:14 +0100

    Added subtitle support to vidoza resolver

--- script.module.resolveurl/lib/resolveurl/plugins/vidoza.py
+++ script.module.resolveurl/lib/resolveurl/plugins/vidoza.py
@@ -16,22 +16,36 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
+import re
 from resolveurl.lib import helpers
-from resolveurl.plugins.__resolve_generic__ import ResolveGeneric
+from resolveurl import common
+from resolveurl.resolver import ResolveUrl, ResolverError
+from six.moves import urllib_parse
 
 
-class VidozaResolver(ResolveGeneric):
+class VidozaResolver(ResolveUrl):
     name = 'Vidoza'
     domains = ['vidoza.net', 'vidoza.co']
+    headers = {'User-Agent': common.RAND_UA}
     pattern = r'(?://|\.)(vidoza\.(?:net|co))/(?:embed-)?([0-9a-zA-Z]+)'
 
-    def get_media_url(self, host, media_id):
-        return helpers.get_media_url(
-            self.get_url(host, media_id),
-            patterns=[r'''["']?\s*(?:file|src)\s*["']?\s*[:=,]?\s*["'](?P<url>[^"']+)(?:[^}>\]]+)["']?\s*res\s*["']?\s*[:=]\s*["']?(?P<label>[^"',]+)'''],
-            generic_patterns=False,
-            referer=False
-        ).replace(' ', '%20')
+    def get_media_url(self, host, media_id, subs=False):
+        web_url = self.get_url(host, media_id)
+        html = self.net.http_GET(web_url, headers=self.headers).content
+
+        if subs:
+            subtitles = {}
+            for src, srclang in re.findall(r'''<track\s*kind=['"]?subtitles['"]?\s*src=['"]([^'"]+)['"]\s*srclang=['"]([^'"]+)''', html):
+                subtitles[srclang] = src if src.startswith('http') else urllib_parse.urljoin(web_url, src)
+
+        result = helpers.scrape_sources(html, patterns=[r'''["']?\s*(?:file|src)\s*["']?\s*[:=,]?\s*["'](?P<url>[^"']+)(?:[^}>\]]+)["']?\s*res\s*["']?\s*[:=]\s*["']?(?P<label>[^"',]+)'''])
+
+        if result:
+            if subs:
+                return helpers.pick_source(result, self.headers) + helpers.append_headers(self.headers), subtitles
+            return helpers.pick_source(result, self.headers) + helpers.append_headers(self.headers)
+
+        raise ResolverError('Video Link Not Found')
 
     def get_url(self, host, media_id):
         return self._default_get_url(host, media_id, template='https://{host}/embed-{media_id}.html')
