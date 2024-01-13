bf4fcfd7d44b7e12864e26e63b937455a7c65ce5
MrDini123 <16760748+MrDini123@users.noreply.github.com>
2024-01-13 22:08:39 +0100

    Added subtitle support to vidmoly resolver

--- script.module.resolveurl/lib/resolveurl/plugins/vidmoly.py
+++ script.module.resolveurl/lib/resolveurl/plugins/vidmoly.py
@@ -16,22 +16,36 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
-from resolveurl.plugins.__resolve_generic__ import ResolveGeneric
+import re
 from resolveurl.lib import helpers
+from resolveurl import common
+from resolveurl.resolver import ResolveUrl, ResolverError
+from six.moves import urllib_parse
 
 
-class VidMolyResolver(ResolveGeneric):
+class VidMolyResolver(ResolveUrl):
     name = 'VidMoly'
     domains = ['vidmoly.me', 'vidmoly.to', 'vidmoly.net']
+    headers = {'User-Agent': common.RAND_UA}
     pattern = r'(?://|\.)(vidmoly\.(?:me|to|net))/(?:embed-|w/)?([0-9a-zA-Z]+)'
 
-    def get_media_url(self, host, media_id):
-        return helpers.get_media_url(
-            self.get_url(host, media_id),
-            patterns=[r'''sources:\s*\[{file:"(?P<url>[^"]+)'''],
-            result_blacklist=['.mpd'],
-            referer=True
-        )
+    def get_media_url(self, host, media_id, subs=False):
+        web_url = self.get_url(host, media_id)
+        self.headers.update({'Referer': web_url, 'Origin': urllib_parse.urljoin(web_url, '/')[:-1]})
+        html = self.net.http_GET(web_url, headers=self.headers).content
+
+        if subs:
+            subtitles = {}
+            for src, label in re.findall(r'''{file:\s*"([^"]+)",\s*label:\s*"([^"]+)",\s*kind:\s*"captions"''', html, re.DOTALL):
+                subtitles[label] = src if src.startswith('http') else urllib_parse.urljoin(web_url, src)
+
+        result = helpers.scrape_sources(html, patterns=[r'''sources:\s*\[{file:"(?P<url>[^"]+)'''])
+        if result:
+            if subs:
+                return helpers.pick_source(result, self.headers) + helpers.append_headers(self.headers), subtitles
+            return helpers.pick_source(result, self.headers) + helpers.append_headers(self.headers)
+        
+        raise ResolverError('Video Link Not Found')
 
     def get_url(self, host, media_id):
         return self._default_get_url(host, media_id, template='https://vidmoly.net/{media_id}.html')
