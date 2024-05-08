25c0e4ebd49ce27ae1840e3244d8a91eb1759452
Peter3344 <129428292+Peter3344@users.noreply.github.com>
2024-05-08 14:25:44 +0300

    Update streamwish.py

--- script.module.resolveurl/lib/resolveurl/plugins/streamwish.py
+++ script.module.resolveurl/lib/resolveurl/plugins/streamwish.py
@@ -31,13 +31,13 @@ class StreamWishResolver(ResolveGeneric):
                'wishfast.top', 'ankrznm.sbs', 'sfastwish.com', 'eghjrutf.sbs', 'eghzrutw.sbs',
                'playembed.online', 'egsyxurh.sbs', 'egtpgrvh.sbs', 'flaswish.com', 'obeywish.com',
                'cdnwish.com', 'javsw.me', 'cinemathek.online', 'trgsfjll.sbs', 'fsdcmo.sbs',
-               'anime4low.sbs', 'mohahhda.site', 'ma2d.store']
+               'anime4low.sbs', 'mohahhda.site', 'ma2d.store', 'dancima.shop']
     pattern = r'(?://|\.)((?:(?:stream|flas|obey|sfast|embed|[mad]|cdn)?wish(?:embed|fast)?|ajmidyad|' \
               r'atabkhha|atabknha|atabknhk|atabknhs|abkrzkr|abkrzkz|vidmoviesb|kharabnahs|hayaatieadhab|' \
               r'cilootv|tuktukcinema|doodporn|ankrzkz|volvovideo|strmwis|ankrznm|yadmalik|khadhnayad|' \
               r'eghjrutf|eghzrutw|playembed|egsyxurh|egtpgrvh|uqloads|javsw|cinemathek|trgsfjll|fsdcmo|' \
-              r'anime4low|mohahhda|ma2d)' \
-              r'\.(?:com|to|sbs|pro|xyz|store|top|site|online|me))/(?:e/|f/)?([0-9a-zA-Z$:/.]+)'
+              r'anime4low|mohahhda|ma2d|dancima)' \
+              r'\.(?:com|to|sbs|pro|xyz|store|top|site|online|me|shop))/(?:e/|f/)?([0-9a-zA-Z$:/.]+)'
 
     def get_media_url(self, host, media_id):
         if '$$' in media_id:
