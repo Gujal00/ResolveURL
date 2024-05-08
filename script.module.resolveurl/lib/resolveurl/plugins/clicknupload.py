d0c3f18a448a3a201eaa7bf3bc095d1ad55950aa
Peter3344 <129428292+Peter3344@users.noreply.github.com>
2024-05-08 15:25:06 +0300

    Update clicknupload.py

--- script.module.resolveurl/lib/resolveurl/plugins/clicknupload.py
+++ script.module.resolveurl/lib/resolveurl/plugins/clicknupload.py
@@ -31,7 +31,7 @@ class ClickNUploadResolver(ResolveUrl):
     domains = ['clicknupload.to', 'clicknupload.cc', 'clicknupload.co', 'clicknupload.com',
                'clicknupload.me', 'clicknupload.link', 'clicknupload.red', 'clicknupload.org',
                'clicknupload.club', 'clicknupload.click', 'clicknupload.download',
-               'clicknupload.vip', 'clickndownload.org', 'clicknupload.space']
+               'clicknupload.vip', 'clickndownload.org', 'clicknupload.space', 'clickndownload.click']
     pattern = r'(?://|\.)(clickn(?:up|down)load\.(?:com?|me|link|org|cc|club|to|red|click|download|vip|space))/(?:f/)?([0-9A-Za-z]+)'
 
     def get_media_url(self, host, media_id):
