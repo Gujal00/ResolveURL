5eaa83213ba617d29ac0486db8c00b7c96142fcf
gujal <17394059+Gujal00@users.noreply.github.com>
2024-05-10 10:12:32 +1200

    fix gofile.io

--- script.module.resolveurl/lib/resolveurl/plugins/gofile.py
+++ script.module.resolveurl/lib/resolveurl/plugins/gofile.py
@@ -1,6 +1,7 @@
 """
     Plugin for ResolveURL
     Copyright (C) 2022 shellc0de
+                  2024 peter3344
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -29,30 +30,29 @@ class GoFileResolver(ResolveUrl):
     pattern = r'(?://|\.)(gofile\.io)/(?:\?c=|d/)([0-9a-zA-Z]+)'
 
     def get_media_url(self, host, media_id):
-        web_url = self.get_url(host, media_id)
-        headers = {'User-Agent': common.FF_USER_AGENT, 'Referer': web_url}
+        headers = {'User-Agent': common.FF_USER_AGENT,
+                   'Origin': 'https://{0}'.format(host),
+                   'Referer': 'https://{0}/'.format(host)
+                   }
         base_api = 'https://api.gofile.io'
-        r = self.net.http_GET('{}/createAccount'.format(base_api), headers=headers)
+        r = self.net.http_POST('{}/accounts'.format(base_api), form_data="{}", headers=headers)
         token = json.loads(r.content).get('data').get('token')
         if not token:
             raise ResolverError('Unable to retrieve token!')
 
         r = self.net.http_GET('https://{}/dist/js/alljs.js'.format(host), headers=headers).content
-        wtoken = re.search(r'fetchData\.wt\s*=\s*"([^"]+)', r)
+        wtoken = re.search(r'fetchData\s*=\s*{\s*wt:\s*"([^"]+)', r)
         if not wtoken:
             raise ResolverError('Unable to retrieve websiteToken!')
 
-        content_url = '{}/getContent?contentId={}&token={}&wt={}'.format(
-            base_api, media_id, token, wtoken.group(1)
-        )
+        headers.update({'Authorization': 'Bearer ' + token})
+        content_url = '{}/contents/{}?wt={}&cache=true'.format(base_api, media_id, wtoken.group(1))
         r = self.net.http_GET(content_url, headers=headers).content
-        data = json.loads(r).get('data').get('contents')
-        if not data:
-            raise ResolverError('This file does not exist.')
+        data = json.loads(r).get('data').get('children')
+        if data:
+            headers.pop('Authorization')
+            headers.update({'Cookie': 'accountToken={}'.format(token)})
+            sources = [(data[x].get('size'), data[x].get('link')) for x in data]
+            return helpers.pick_source(sources, False) + helpers.append_headers(headers)
 
-        headers.update({'Cookie': 'accountToken={}'.format(token)})
-        sources = [(data[x].get('size'), data[x].get('link')) for x in data]
-        return helpers.pick_source(sources, False) + helpers.append_headers(headers)
-
-    def get_url(self, host, media_id):
-        return self._default_get_url(host, media_id, template='https://{host}/d/{media_id}')
+        raise ResolverError('This file does not exist.')
