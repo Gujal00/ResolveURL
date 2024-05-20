6e8b0025f1d41142cb69c6c5449802c9df3eb039
bassemhelal18 <77381611+bassemhelal18@users.noreply.github.com>
2024-05-20 16:19:29 +0300

    Update allviid.py (#773)
    * Update allviid.py

--- script.module.resolveurl/lib/resolveurl/plugins/allviid.py
+++ script.module.resolveurl/lib/resolveurl/plugins/allviid.py
@@ -17,12 +17,20 @@
 """
 
 from resolveurl.plugins.__resolve_generic__ import ResolveGeneric
-
+from resolveurl.lib import helpers
 
 class AllViidResolver(ResolveGeneric):
     name = 'AllViid'
     domains = ['allviid.xyz', 'all-vid.autos']
-    pattern = r'(?://|\.)(all-?vi*d\.(?:xyz|autos))/(?:embed-)?([0-9a-zA-Z]+)'
+    pattern = r'(?://|\.)(.*?all-?vi*d\.(?:xyz|autos))/(?:embed-)?([0-9a-zA-Z]+)'
+
+    def get_media_url(self, host, media_id):
+        return helpers.get_media_url(
+            self.get_url(host, media_id),
+            patterns=[r'''sources:\s*\[(?:{\s*file:)?\s*"(?P<url>[^"]+)'''],
+            generic_patterns=False,
+            referer=False
+        )
 
     def get_url(self, host, media_id):
         return self._default_get_url(host, media_id, template='https://{host}/embed-{media_id}.html')
