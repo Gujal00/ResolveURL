0c513d79f92dbaaf3ae66fce12ea047bfc6f4b67
MrDini123 <16760748+MrDini123@users.noreply.github.com>
2024-05-27 20:02:39 +0200

    Fixed vidmoly resolver

--- script.module.resolveurl/lib/resolveurl/plugins/vidmoly.py
+++ script.module.resolveurl/lib/resolveurl/plugins/vidmoly.py
@@ -16,23 +16,38 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
-from resolveurl.plugins.__resolve_generic__ import ResolveGeneric
+from resolveurl.resolver import ResolveUrl, ResolverError
+from resolveurl import common
 from resolveurl.lib import helpers
 
 
-class VidMolyResolver(ResolveGeneric):
+class VidMolyResolver(ResolveUrl):
     name = 'VidMoly'
     domains = ['vidmoly.me', 'vidmoly.to', 'vidmoly.net']
     pattern = r'(?://|\.)(vidmoly\.(?:me|to|net))/(?:embed-|w/)?([0-9a-zA-Z]+)'
 
     def get_media_url(self, host, media_id, subs=False):
-        return helpers.get_media_url(
-            self.get_url(host, media_id),
-            patterns=[r'''sources:\s*\[{file:"(?P<url>[^"]+)'''],
+        web_url = self.get_url(host, media_id)
+        headers = {"User-Agent": common.FF_USER_AGENT, "Referer": web_url, "Sec-Fetch-Dest": "iframe"}
+        html = self.net.http_GET(web_url, headers=headers).content
+        sources = helpers.scrape_sources(
+            html,
             result_blacklist=['.mpd'],
-            referer=True,
-            subs=subs
+            patterns=[r'''sources:\s*\[{file:"(?P<url>[^"]+)'''],
+            generic_patterns=False
         )
+
+        if subs:
+            subtitles = helpers.scrape_subtitles(html, web_url)
+        
+        if sources:
+            stream_url = helpers.pick_source(sources) + helpers.append_headers(headers)
+            if subs:
+                return stream_url, subtitles
+            return stream_url
+        
+        raise ResolverError('No video found')
+            
 
     def get_url(self, host, media_id):
         return self._default_get_url(host, media_id, template='https://vidmoly.net/embed-{media_id}.html')
