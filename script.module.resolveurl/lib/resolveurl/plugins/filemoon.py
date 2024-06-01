0523ec842209f7a49586aeb5c47db71167d922b5
MrDini123 <16760748+MrDini123@users.noreply.github.com>
2024-06-01 12:27:23 +0200

    Added filemoon domain

--- script.module.resolveurl/lib/resolveurl/plugins/filemoon.py
+++ script.module.resolveurl/lib/resolveurl/plugins/filemoon.py
@@ -28,8 +28,8 @@ class FileMoonResolver(ResolveUrl):
     name = 'FileMoon'
     domains = ['filemoon.sx', 'filemoon.to', 'filemoon.in', 'filemoon.link', 'filemoon.nl',
                'filemoon.wf', 'cinegrab.com', 'filemoon.eu', 'filemoon.art', 'moonmov.pro',
-               'kerapoxy.cc']
-    pattern = r'(?://|\.)((?:filemoon|cinegrab|moonmov|kerapoxy)\.(?:sx|to|in|link|nl|wf|com|eu|art|pro|cc))' \
+               'kerapoxy.cc', 'furher.in']
+    pattern = r'(?://|\.)((?:filemoon|cinegrab|moonmov|kerapoxy|furher)\.(?:sx|to|in|link|nl|wf|com|eu|art|pro|cc))' \
               r'/(?:e|d|download)/([0-9a-zA-Z$:/.]+)'
 
     def get_media_url(self, host, media_id):
