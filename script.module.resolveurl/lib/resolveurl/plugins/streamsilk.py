bdd6a54c0a620d897a91cee13b7decb6df2c6c64
gujal <17394059+Gujal00@users.noreply.github.com>
2024-09-09 23:30:35 +0530

    new resolver streamsilk. Fixes #884

---
+++ script.module.resolveurl/lib/resolveurl/plugins/streamsilk.py
@@ -0,0 +1,47 @@
+"""
+    Plugin for ResolveURL
+    Copyright (C) 2024 gujal
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+import re
+from resolveurl.lib import helpers, jsunhunt
+from resolveurl import common
+from resolveurl.resolver import ResolveUrl, ResolverError
+
+
+class StreamSilkResolver(ResolveUrl):
+    name = 'StreamSilk'
+    domains = ['streamsilk.com']
+    pattern = r'(?://|\.)(streamsilk\.com)/(?:d|p)/([0-9a-zA-Z]+)'
+
+    def get_media_url(self, host, media_id):
+        web_url = self.get_url(host, media_id)
+        headers = {'User-Agent': common.RAND_UA}
+        html = self.net.http_GET(web_url, headers=headers).content
+        if jsunhunt.detect(html):
+            html = jsunhunt.unhunt(html)
+        r = re.search(r'urlPlay\s*=\s*"([^"]+)', html)
+        if r:
+            headers.update({
+                'Referer': 'https://{0}/'.format(host),
+                'Origin': 'https://{0}'.format(host)
+            })
+            return r.group(1).strip() + helpers.append_headers(headers)
+
+        raise ResolverError('File not found')
+
+    def get_url(self, host, media_id):
+        return self._default_get_url(host, media_id, template='https://{host}/p/{media_id}')
