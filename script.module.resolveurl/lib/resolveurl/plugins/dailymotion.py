d62e863ada8c68c3dad6ff63ddef768aaeb16848
gujal <17394059+Gujal00@users.noreply.github.com>
2024-11-04 08:37:22 +1300

    add subtitle support to dailymotion resolver

--- script.module.resolveurl/lib/resolveurl/plugins/dailymotion.py
+++ script.module.resolveurl/lib/resolveurl/plugins/dailymotion.py
@@ -29,7 +29,7 @@ class DailymotionResolver(ResolveUrl):
     pattern = r'(?://|\.)(dailymotion\.com|dai\.ly)(?:/(?:video|embed|sequence|swf|player)' \
               r'(?:/video|/full)?)?/(?:[a-z0-9]+\.html\?video=)?([0-9a-zA-Z]+)'
 
-    def get_media_url(self, host, media_id):
+    def get_media_url(self, host, media_id, subs=False):
         web_url = self.get_url(host, media_id)
         headers = {'User-Agent': common.RAND_UA,
                    'Origin': 'https://www.dailymotion.com',
@@ -40,11 +40,20 @@ class DailymotionResolver(ResolveUrl):
             raise ResolverError(js_result.get('error').get('title'))
 
         quals = js_result.get('qualities')
+        if subs:
+            subtitles = {}
+            matches = js_result.get('subtitles', {}).get('data')
+            if matches:
+                for key in list(matches.keys()):
+                    subtitles[matches[key].get('label')] = matches[key].get('urls', [])[0]
 
         if quals:
             mbtext = self.net.http_GET(quals.get('auto')[0].get('url'), headers=headers).content
             sources = re.findall('NAME="(?P<label>[^"]+)".*(?:,PROGRESSIVE-URI="|\n)(?P<url>[^#]+)', mbtext)
-            return helpers.pick_source(helpers.sort_sources_list(sources)) + helpers.append_headers(headers)
+            vid_src = helpers.pick_source(helpers.sort_sources_list(sources)) + helpers.append_headers(headers)
+            if subs:
+                return vid_src, subtitles
+            return vid_src
         raise ResolverError('No playable video found.')
 
     def get_url(self, host, media_id):
