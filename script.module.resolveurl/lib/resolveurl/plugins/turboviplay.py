e43b005f159aedcb883eb059001197c0ec0d1bea
Peter3344 <129428292+Peter3344@users.noreply.github.com>
2024-11-09 18:09:57 +0200

    Update turboviplay.py

--- script.module.resolveurl/lib/resolveurl/plugins/turboviplay.py
+++ script.module.resolveurl/lib/resolveurl/plugins/turboviplay.py
@@ -23,7 +23,7 @@ from resolveurl.lib import helpers
 class TurboVIPlayResolver(ResolveGeneric):
     name = 'TurboVIPlay'
     domains = ['turboviplay.com', 'emturbovid.com', 'tuborstb.co', 'javggvideo.xyz']
-    pattern = r'(?://|\.)((?:turboviplay|emturbovid|tuborstb|javggvideo)\.(?:com?|xyz))/t/([0-9a-zA-Z]+)'
+    pattern = r'(?://|\.)((?:turboviplay|emturbovid|tuborstb|javggvideo)\.(?:com?|xyz))/(?:t/|d/)?([0-9a-zA-Z]+)'
 
     def get_media_url(self, host, media_id):
         return helpers.get_media_url(
