7db04d5e1953daf04b86e90bc3e5047d700002fc
pikdum <pikdum@kuudere.moe>
2024-11-22 00:03:05 -0600

    wip: improvements

--- script.module.resolveurl/lib/resolveurl/plugins/torbox.py
+++ script.module.resolveurl/lib/resolveurl/plugins/torbox.py
@@ -16,13 +16,13 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
-import re
-from six.moves import urllib_parse, urllib_error
 import json
-from resolveurl.lib import helpers
+import re
+
 from resolveurl import common
 from resolveurl.common import i18n
-from resolveurl.resolver import ResolveUrl, ResolverError
+from resolveurl.resolver import ResolverError, ResolveUrl
+from six.moves import urllib_error, urllib_parse
 
 logger = common.log_utils.Logger.get_logger(__name__)
 # logger.disable()
@@ -30,14 +30,12 @@ logger = common.log_utils.Logger.get_logger(__name__)
 AGENT = "ResolveURL for Kodi"
 VERSION = common.addon_version
 USER_AGENT = "{0}/{1}".format(AGENT, VERSION)
-FORMATS = common.VIDEO_FORMATS
-
-api_url = "https://api.torbox.app/v1/api"
 
 
 class TorBoxResolver(ResolveUrl):
     name = "TorBox"
     domains = ["*"]
+    api_url = "https://api.torbox.app/v1/api"
 
     def __init__(self):
         self.hosters = None
@@ -47,97 +45,116 @@ class TorBoxResolver(ResolveUrl):
             "Authorization": "Bearer %s" % self.__get_token(),
         }
 
-    def __create_torrent(self, magnet) -> dict | None:
-        logger.log_warning("Creating torrent for magnet: %s" % magnet)
-        url = "{0}/torrents/createtorrent".format(api_url)
-        data = {"magnet": magnet, "as_queued": True}
-        result = self.net.http_POST(url, form_data=data, headers=self.headers).content
-        # TODO: if this throws 400 error, probably free tier rate limit
-        # is free tier worth handling specially?
-        if not result:
-            return None
-        result = json.loads(result)
-        if result.get("success"):
-            return result.get("data")
-        return None
+    def __get(self, endpoint, query, empty=None):
+        try:
+            url = "{0}/{1}?{2}".format(
+                self.api_url, endpoint, urllib_parse.urlencode(query)
+            )
+            result = self.net.http_GET(url, headers=self.headers).content
+            if not result:
+                return empty
+            result = json.loads(result)
+            if result.get("success"):
+                return result.get("data")
+            return empty
+        except urllib_error.HTTPError as e:
+            if e.code == 429:
+                common.kodi.sleep(3000)
+                return self.__get(endpoint, query, empty)
+            return empty
 
-    def __check_cache(self, hash) -> bool:
-        logger.log_warning("Checking cache for hash: %s" % hash)
-        url = "{0}/torrents/checkcached?hash={1}&format=list&list_files=false".format(
-            api_url, hash
+    def __post(self, endpoint, data, empty=None):
+        try:
+            url = "{0}/{1}".format(self.api_url, endpoint)
+            result = self.net.http_POST(url, form_data=data, headers=self.headers).content
+            if not result:
+                return empty
+            result = json.loads(result)
+            if result.get("success"):
+                return result.get("data")
+            return empty
+        except urllib_error.HTTPError as e:
+            if e.code == 429:
+                common.kodi.sleep(3000)
+                return self.__post(endpoint, data, empty)
+            return empty
+
+    def __create_torrent(self, magnet) -> dict | None:
+        result = self.__post(
+            "torrents/createtorrent", {"magnet": magnet}, {}
         )
-        result = self.net.http_GET(url, headers=self.headers).content
-        if not result:
-            return False
-        result = json.loads(result)
-        return bool(result.get("data"))
+        logger.log_warning("Create torrent: %s" % result)
+        return result
+
+    def __check_cache(self, btih) -> bool:
+        result = self.__get(
+            "torrents/checkcached",
+            {"hash": btih, "format": "list", "list_files": False},
+        )
+        logger.log_warning("Check cache: %s" % result)
+        return bool(result)
 
     def __check_ready(self, torrent_id) -> bool:
-        # TODO: handle rate limiting (429 status)
-        return True
-        logger.log_warning("Checking if torrent is ready: %s" % torrent_id)
-        url = "{0}/torrents/mylist?id={1}&bypass_cache=true".format(api_url, torrent_id)
-        result = self.net.http_GET(url, headers=self.headers).content
-        if not result:
-            return False
-        result = json.loads(result)
-        if result.get("success"):
-            return result.get("data").get("download_present")
-        return False
+        result = self.__get(
+            "torrents/mylist", {"id": torrent_id, "bypass_cache": True}, {}
+        )
+        return result.get("download_present", False)
 
-    def __check_existing(self, hash) -> str | None:
-        logger.log_warning("Checking existing torrent for hash: %s" % hash)
-        url = "{0}/torrents/mylist?bypass_cache=true".format(api_url)
-        result = self.net.http_GET(url, headers=self.headers).content
-        if not result:
-            return None
-        result = json.loads(result)
-        if result.get("success"):
-            torrents = result.get("data")
-            for torrent in torrents:
-                if torrent.get("hash") == hash:
-                    return torrent.get("id")
+    def __check_existing(self, btih) -> str | None:
+        torrents = self.__get("torrents/mylist", {"bypass_cache": True}, [])
+        for torrent in torrents:
+            if torrent.get("hash") == btih:
+                return torrent.get("id")
         return None
 
     def __download_link(self, torrent_id) -> str | None:
-        logger.log_warning("Getting download link for torrent: %s" % torrent_id)
-        url = "{0}/torrents/requestdl?torrent_id={1}&token={2}".format(
-            api_url, torrent_id, self.__get_token()
+        return self.__get(
+            "torrents/requestdl",
+            {"torrent_id": torrent_id, "token": self.__get_token()},
         )
-        logger.log_warning("URL: %s" % url)
-        result = self.net.http_GET(url, headers=self.headers).content
-        logger.log_warning("Result: %s" % result)
-        if not result:
-            return None
-        result = json.loads(result)
-        if result.get("success"):
-            return result.get("data")
-        return None
 
     def __get_token(self) -> str:
         return self.get_setting("apikey")
 
+    def __get_hash(self, media_id) -> str | None:
+        r = re.search("""magnet:.+?urn:([a-zA-Z0-9]+):([a-zA-Z0-9]+)""", media_id, re.I)
+        if not r or len(r.groups()) < 2:
+            return None
+        return r.group(2)
+
     def get_media_url(
         self, host, media_id, cached_only=False, return_all=False
     ) -> str | None:
-        logger.log_warning("Media ID: %s" % media_id)
         # TODO: handle return_all
-        r = re.search("""magnet:.+?urn:([a-zA-Z0-9]+):([a-zA-Z0-9]+)""", media_id, re.I)
-        if not r:
-            return None
-        hash = r.group(2)
-        cached = self.__check_cache(hash)
+        # TODO: handle multiple files
+        btih = self.__get_hash(media_id)
+        logger.log_warning("BTIH: %s" % btih)
+        cached = self.__check_cache(btih)
+        logger.log_warning("Cached: %s" % cached)
+
         if not cached and cached_only:
             raise ResolverError("TorBox: {0}".format(i18n("cached_torrents_only")))
-        # TODO: this is a workaround to bypass rate limit on create torrent, even if already added
-        torrent_id = self.__check_existing(hash)
+
+        # check if it's in your list
+        torrent_id = self.__check_existing(btih)
+        logger.log_warning("Torrent ID: %s" % torrent_id)
         if not torrent_id:
+            # if not, add it
             torrent_id = self.__create_torrent(media_id).get("torrent_id")
-        # loop until check_ready is Tru
+            logger.log_warning("Torrent ID: %s" % torrent_id)
+
+        # error adding torrent, abort
+        if not torrent_id:
+            logger.log_warning("Failed to add torrent - aborting.")
+            return None
+
+        # loop until check_ready is true, skip if cached
+        # TODO: is it really always ready if cached?
         # TODO: show progress dialog
-        while not self.__check_ready(torrent_id):
-            logger.log_debug("TorBox: Torrent not ready")
-            common.kodi.sleep(5000)
+        while not (cached and self.__check_ready(torrent_id)):
+            logger.log_warning("Waiting for torrent to be ready...")
+            common.kodi.sleep(3000)
+
         download_link = self.__download_link(torrent_id)
         logger.log_warning("Download link: %s" % download_link)
         return download_link
@@ -149,13 +166,14 @@ class TorBoxResolver(ResolveUrl):
         return "torbox.app", url
 
     def valid_url(self, url, host):
-        return True
+        btih = self.__get_hash(url)
+        return bool(btih)
 
     @classmethod
     def get_settings_xml(cls):
         xml = super(cls, cls).get_settings_xml(include_login=False)
         xml.append(
-            '<setting id="%s_apikey" enable="eq(-1,true)" type="text" label="%s" option="hidden" default=""/>'
+            '<setting id="%s_apikey" enable="eq(-1,true)" type="text" label="%s" default=""/>'
             % (cls.__name__, "API Key")
         )
         return xml
