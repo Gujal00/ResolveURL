066b40cb784cd0e2b4089bdb251171bbe404772f
Gujal <17394059+Gujal00@users.noreply.github.com>
2025-03-14 07:51:14 +1300

    update doodstream, Fixes #1053 #1055

--- script.module.resolveurl/lib/resolveurl/plugins/doodstream.py
+++ script.module.resolveurl/lib/resolveurl/plugins/doodstream.py
@@ -30,8 +30,8 @@ class DoodStreamResolver(ResolveUrl):
     domains = ['dood.watch', 'doodstream.com', 'dood.to', 'dood.so', 'dood.cx', 'dood.la', 'dood.ws',
                'dood.sh', 'doodstream.co', 'dood.pm', 'dood.wf', 'dood.re', 'dood.yt', 'dooood.com',
                'dood.stream', 'ds2play.com', 'doods.pro', 'ds2video.com', 'd0o0d.com', 'do0od.com',
-               'd0000d.com', 'd000d.com', 'dood.li', 'dood.work', 'dooodster.com']
-    pattern = r'(?://|\.)((?:do*0*o*0*ds?(?:tream|ter)?|ds2(?:play|video))\.' \
+               'd0000d.com', 'd000d.com', 'dood.li', 'dood.work', 'dooodster.com', 'vidply.com']
+    pattern = r'(?://|\.)((?:do*0*o*0*ds?(?:tream|ter)?|ds2(?:play|video)|vidply)\.' \
               r'(?:com?|watch|to|s[ho]|cx|l[ai]|w[sf]|pm|re|yt|stream|pro|work))/(?:d|e)/([0-9a-zA-Z]+)'
 
     def get_media_url(self, host, media_id, subs=False):
@@ -80,6 +80,8 @@ class DoodStreamResolver(ResolveUrl):
         raise ResolverError('Video Link Not Found')
 
     def get_url(self, host, media_id):
+        if host in ['dood.la']:
+            host = 'doodstream.com'
         return self._default_get_url(host, media_id, template='https://{host}/d/{media_id}')
 
     def dood_decode(self, data):
