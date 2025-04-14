ade3baaf9d3f9d4e90e7ca7850228c8e7b87ad4d
Peter3344 <129428292+Peter3344@users.noreply.github.com>
2025-04-14 13:36:16 +0200

    Update filemoon.py

--- script.module.resolveurl/lib/resolveurl/plugins/filemoon.py
+++ script.module.resolveurl/lib/resolveurl/plugins/filemoon.py
@@ -30,9 +30,9 @@ class FileMoonResolver(ResolveUrl):
                'filemoon.wf', 'cinegrab.com', 'filemoon.eu', 'filemoon.art', 'moonmov.pro',
                'kerapoxy.cc', 'furher.in', '1azayf9w.xyz', '81u6xl9d.xyz', 'smdfs40r.skin',
                'bf0skv.org', 'z1ekv717.fun', 'l1afav.net', '222i8x.lol', '8mhlloqo.fun', '96ar.com',
-               'xcoic.com']
+               'xcoic.com', 'f51rm.com']
     pattern = r'(?://|\.)((?:filemoon|cinegrab|moonmov|kerapoxy|furher|1azayf9w|81u6xl9d|' \
-              r'smdfs40r|bf0skv|z1ekv717|l1afav|222i8x|8mhlloqo|96ar|xcoic)' \
+              r'smdfs40r|bf0skv|z1ekv717|l1afav|222i8x|8mhlloqo|96ar|xcoic|f51rm)' \
               r'\.(?:sx|to|s?k?in|link|nl|wf|com|eu|art|pro|cc|xyz|org|fun|net|lol))' \
               r'/(?:e|d|download)/([0-9a-zA-Z$:/._-]+)'
 
