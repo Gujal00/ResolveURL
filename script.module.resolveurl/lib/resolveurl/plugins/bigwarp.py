db9a0f034550706c64adec6bf247a479c2e901eb
Gujal <17394059+Gujal00@users.noreply.github.com>
2025-06-30 16:42:35 +1200

    bigwarp additional domain, Fixes #1126

--- script.module.resolveurl/lib/resolveurl/plugins/bigwarp.py
+++ script.module.resolveurl/lib/resolveurl/plugins/bigwarp.py
@@ -16,22 +16,30 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
-from resolveurl.plugins.__resolve_generic__ import ResolveGeneric
+import re
+from resolveurl import common
 from resolveurl.lib import helpers
+from resolveurl.resolver import ResolveUrl, ResolverError
 
 
-class BigWarpResolver(ResolveGeneric):
+class BigWarpResolver(ResolveUrl):
     name = 'BigWarp'
-    domains = ['bigwarp.io', 'bgwp.cc', 'bigwarp.art']
+    domains = ['bigwarp.io', 'bgwp.cc', 'bigwarp.art', 'bigwarp.cc']
     pattern = r'(?://|\.)((?:bigwarp|bgwp)\.(?:io|cc|art))/(?:e/|embed-)?([0-9a-zA-Z=]+)'
 
     def get_media_url(self, host, media_id, subs=False):
-        return helpers.get_media_url(
-            self.get_url(host, media_id),
-            patterns=[r'''file\s*:\s*['"](?P<url>[^'"]+)['"],\s*label\s*:\s*['"](?P<label>\d+p?)'''],
-            subs=subs,
-            referer=False
-        )
+        web_url = self.get_url(host, media_id)
+        headers = {'User-Agent': common.FF_USER_AGENT}
+        html = self.net.http_GET(web_url, headers=headers).content
+        sources = re.findall(r'''file\s*:\s*['"](?P<url>[^'"]+)['"],\s*label\s*:\s*['"](?P<label>\d+p?)''', html)
+        if sources:
+            sources = [(x[1], x[0]) for x in sources]
+            surl = helpers.pick_source(helpers.sort_sources_list(sources)) + helpers.append_headers(headers)
+            if subs:
+                subtitles = helpers.scrape_subtitles(html, web_url)
+                return surl, subtitles
+            return surl
+        raise ResolverError('Video Link Not Found')
 
     def get_url(self, host, media_id):
-        return self._default_get_url(host, media_id, template='https://bigwarp.art/embed-{media_id}.html')
+        return self._default_get_url(host, media_id, template='https://{host}/embed-{media_id}.html')
