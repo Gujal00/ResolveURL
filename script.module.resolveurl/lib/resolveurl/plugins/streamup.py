25a8f8eebf43cd41eb1c14f5c53197f92f10522e
Gujal <17394059+Gujal00@users.noreply.github.com>
2025-07-01 18:05:45 +1200

    streamup additional domain, Fixes #1127

--- script.module.resolveurl/lib/resolveurl/plugins/streamup.py
+++ script.module.resolveurl/lib/resolveurl/plugins/streamup.py
@@ -16,34 +16,21 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
-import re
-import json
-from resolveurl import common
+from resolveurl.plugins.__resolve_generic__ import ResolveGeneric
 from resolveurl.lib import helpers
-from resolveurl.resolver import ResolveUrl, ResolverError
 
 
-class StreamUpResolver(ResolveUrl):
+class StreamUpResolver(ResolveGeneric):
     name = 'StreamUp'
-    domains = ['streamup.ws']
-    pattern = r'(?://|\.)(streamup\.ws)/([0-9a-zA-Z]+)'
+    domains = ['streamup.ws', 'strmup.to']
+    pattern = r'(?://|\.)(stre?a?mup\.(?:ws|to))/([0-9a-zA-Z]+)'
 
     def get_media_url(self, host, media_id):
-        web_url = self.get_url(host, media_id)
-        headers = {'User-Agent': common.RAND_UA}
-        html = self.net.http_GET(web_url, headers=headers).content
-        r = re.search(r'fetch\(`([^=]+=)[^`]+`\)\.then', html)
-        if r:
-            headers.update({
-                'Referer': 'https://{0}/'.format(host),
-                'Origin': 'https://{0}'.format(host)
-            })
-            surl = r.group(1) + media_id
-            sdata = json.loads(self.net.http_GET(surl, headers=headers).content)
-            s = sdata.get('streaming_url')
-            if s:
-                return s + helpers.append_headers(headers)
-        raise ResolverError('Video Link Not Found')
+        return helpers.get_media_url(
+            self.get_url(host, media_id),
+            patterns=[r'''streaming_url:"(?P<url>[^"]+)'''],
+            generic_patterns=False
+        )
 
     def get_url(self, host, media_id):
         return self._default_get_url(host, media_id, template='https://{host}/{media_id}')
