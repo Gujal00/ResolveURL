ba63cc66e35d7b2a05f6466e6b1426aa44be57a3
pawel959 <pawprz19@o2.pl>
2025-09-06 00:30:27 +0200

    filemoon additional domain
    filemoon additional domain

--- script.module.resolveurl/lib/resolveurl/plugins/filemoon.py
+++ script.module.resolveurl/lib/resolveurl/plugins/filemoon.py
@@ -30,10 +30,10 @@ class FileMoonResolver(ResolveUrl):
                'filemoon.wf', 'cinegrab.com', 'filemoon.eu', 'filemoon.art', 'moonmov.pro',
                'kerapoxy.cc', 'furher.in', '1azayf9w.xyz', '81u6xl9d.xyz', 'smdfs40r.skin',
                'bf0skv.org', 'z1ekv717.fun', 'l1afav.net', '222i8x.lol', '8mhlloqo.fun', '96ar.com',
-               'xcoic.com', 'f51rm.com', 'c1z39.com']
+               'xcoic.com', 'f51rm.com', 'c1z39.com', 'boosteradx.online']
     pattern = r'(?://|\.)((?:filemoon|cinegrab|moonmov|kerapoxy|furher|1azayf9w|81u6xl9d|' \
-              r'smdfs40r|bf0skv|z1ekv717|l1afav|222i8x|8mhlloqo|96ar|xcoic|f51rm|c1z39)' \
-              r'\.(?:sx|to|s?k?in|link|nl|wf|com|eu|art|pro|cc|xyz|org|fun|net|lol))' \
+              r'smdfs40r|bf0skv|z1ekv717|l1afav|222i8x|8mhlloqo|96ar|xcoic|f51rm|c1z39|boosteradx)' \
+              r'\.(?:sx|to|s?k?in|link|nl|wf|com|eu|art|pro|cc|xyz|org|fun|net|lol|online))' \
               r'/(?:e|d|download)/([0-9a-zA-Z$:/._-]+)'
 
     def get_media_url(self, host, media_id):
