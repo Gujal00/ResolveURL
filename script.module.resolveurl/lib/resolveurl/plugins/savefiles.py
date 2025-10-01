35b9298f0a86f20797098b224c01d4abbcb82949
skoruppa <skoruppa@gmail.com>
2025-10-01 12:41:36 +0200

    Fixed Savefiles (#1182)
    * Fixed savefiles
    
    ---------
    
    Co-authored-by: gujal <17394059+Gujal00@users.noreply.github.com>

--- script.module.resolveurl/lib/resolveurl/plugins/savefiles.py
+++ script.module.resolveurl/lib/resolveurl/plugins/savefiles.py
@@ -16,23 +16,45 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
-from resolveurl.plugins.__resolve_generic__ import ResolveGeneric
+import re
+from six.moves import urllib_parse
+
+from resolveurl import common
 from resolveurl.lib import helpers
+from resolveurl.resolver import ResolveUrl, ResolverError
 
 
-class SaveFilesResolver(ResolveGeneric):
+class SaveFilesResolver(ResolveUrl):
     name = 'SaveFiles'
     domains = ['savefiles.com', 'streamhls.to']
     pattern = r'(?://|\.)((?:savefiles|streamhls)\.' \
               r'(?:com|to))/(?:e/)?([0-9a-zA-Z]+)'
 
     def get_media_url(self, host, media_id, subs=False):
-        return helpers.get_media_url(
-            self.get_url(host, media_id),
-            patterns=[r'''sources:\s*\[{\s*file\s*:\s*['"](?P<url>[^'"]+)'''],
-            subs=subs,
-            referer=False
-        )
+        web_url = self.get_url(host, media_id)
+        ref = urllib_parse.urljoin(web_url, '/')
+        dl_url = urllib_parse.urljoin(web_url, '/dl')
+        post_data = {
+            'op': 'embed',
+            'file_code': media_id,
+            'auto': '0'
+        }
+        headers = {
+            "User-Agent": common.RAND_UA,
+            "Referer": ref,
+            "Origin": ref[:-1]
+        }
+
+        player_html = self.net.http_POST(dl_url, form_data=post_data, headers=headers).content
+        s = re.search(r'''sources:\s*\[{\s*file\s*:\s*['"]([^'"]+)''', player_html)
+        if s:
+            stream_url = s.group(1) + helpers.append_headers(headers)
+            if subs:
+                subtitles = helpers.scrape_subtitles(player_html, web_url)
+                return stream_url, subtitles
+            return stream_url
+
+        raise ResolverError("Unable to locate stream URL.")
 
     def get_url(self, host, media_id):
-        return self._default_get_url(host, media_id, template='https://{host}/{media_id}')
+        return self._default_get_url(host, media_id, template='https://{host}/e/{media_id}')
