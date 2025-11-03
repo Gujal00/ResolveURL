3b5192302ff126966cfa75485a4959109fdf8de4
Gujal <17394059+Gujal00@users.noreply.github.com>
2025-11-03 18:46:03 +1300

    kinoger additional domain. Fixes #1212

--- script.module.resolveurl/lib/resolveurl/plugins/kinoger.py
+++ script.module.resolveurl/lib/resolveurl/plugins/kinoger.py
@@ -31,11 +31,13 @@ class KinoGerResolver(ResolveUrl):
         'wasuytm.store', 'ultrastream.online', 'moflix.rpmplay.xyz', 'tuktuk.rpmvid.com', 'w1tv.xyz'
         'filedecrypt.link', 'asianembed.cam', 'videoshar.uns.bio', 'videoland.cfd', 'dzo.vidplayer.live',
         'watch.ezplayer.me', 'watch.streamcasthub.store', 'ultra.rpmvid.site', 'securecdn.shop',
-        'srbe84.vidplayer.live', 'flimmer.rpmvip.com', 't1.p2pplay.pro'
+        'srbe84.vidplayer.live', 'flimmer.rpmvip.com', 't1.p2pplay.pro', 'flixfilmesonline.strp2p.site',
+        'filma365.strp2p.site', 'strp2p.site'
     ]
     pattern = r'(?://|\.)((?:kinoger|wasuytm|ultrastream|(?:shiid4u|player)\.upn|moflix\.(?:upns|rpmplay)|' \
-              r'(?:tuktuk|ultra)\.rpmvid|disneycdn|filedecrypt|(?:dzo|srbe84)\.vidplayer|video(?:shar\.uns|land)|w1tv|' \
-              r'watch\.(?:ezplayer|streamcasthub)|asianembed|securecdn|flimmer\.rpmvip|t1\.p2pplay)' \
+              r'(?:tuktuk|ultra)\.rpmvid|filedecrypt|(?:dzo|srbe84)\.vidplayer|video(?:shar\.uns|land)|' \
+              r'w1tv|(?:flixfilmesonline\.|filma365\.)?strp2p|flimmer\.rpmvip|t1\.p2pplay|asianembed|securecdn|' \
+              r'watch\.(?:ezplayer|streamcasthub)|disneycdn)' \
               r'\.(?:[mr]e|one|xyz|store|online|c[oa]m|net|li(?:nk|ve)|bio|cfd|site|shop|pro))/#([A-Za-z0-9]+)'
 
     def get_media_url(self, host, media_id):
