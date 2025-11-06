b111b14a646d1822a7901775504e1a8b9e2af7c0
Gujal <17394059+Gujal00@users.noreply.github.com>
2025-11-07 00:51:04 +1300

    tokyvideo new resolver, Fixes #1202

---
+++ script.module.resolveurl/lib/resolveurl/plugins/tokyvideo.py
@@ -0,0 +1,45 @@
+"""
+    Plugin for ResolveURL
+    Copyright (C) 2025 gujal
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+import re
+from resolveurl.lib import helpers
+from resolveurl.resolver import ResolveUrl, ResolverError
+from resolveurl import common
+from six.moves import urllib_parse
+
+
+class TokyVideoResolver(ResolveUrl):
+    name = 'TokyVideo'
+    domains = ['www.tokyvideo.com']
+    pattern = r'(?://|\.)(www\.tokyvideo\.com)/video/([0-9a-z-]+)'
+
+    def get_media_url(self, host, media_id):
+        web_url = self.get_url(host, media_id)
+        referer = urllib_parse.urljoin(web_url, '/')
+        headers = {'User-Agent': common.FF_USER_AGENT,
+                   'Referer': referer}
+        resp = self.net.http_GET(web_url, headers=headers)
+        cookie_string = resp.get_cookies()
+        r = re.search(r'<source\s*src="([^"]+)', resp.content)
+        if r:
+            headers.update({'Cookie': cookie_string})
+            return r.group(1) + helpers.append_headers(headers)
+        raise ResolverError('No playable video found.')
+
+    def get_url(self, host, media_id):
+        return self._default_get_url(host, media_id, template='https://{host}/video/{media_id}')
