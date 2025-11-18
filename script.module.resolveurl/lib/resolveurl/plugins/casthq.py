588e5a0e615b517fc3f8733e15bc235092d81707
bassemhelal18 <77381611+bassemhelal18@users.noreply.github.com>
2025-11-18 19:37:34 +0200

    Add 'vidoba.org' to domains and update pattern

--- script.module.resolveurl/lib/resolveurl/plugins/casthq.py
+++ script.module.resolveurl/lib/resolveurl/plugins/casthq.py
@@ -26,8 +26,8 @@ from resolveurl.resolver import ResolveUrl, ResolverError
 
 class CastHQResolver(ResolveUrl):
     name = 'CastHQ'
-    domains = ['casthq.to', 'vidspeeds.com']
-    pattern = r'(?://|\.)((?:casthq|vidspeeds)\.(?:to|com))//?(?:e/|d/|embed-)?([0-9a-zA-Z=]+)'
+    domains = ['casthq.to', 'vidspeeds.com', 'vidoba.org']
+    pattern = r'(?://|\.)((?:casthq|vidspeeds|vidoba)\.(?:to|com|org))//?(?:e/|d/|embed-)?([0-9a-zA-Z=]+)'
 
     def get_media_url(self, host, media_id, subs=False):
         web_url = self.get_url(host, media_id)
