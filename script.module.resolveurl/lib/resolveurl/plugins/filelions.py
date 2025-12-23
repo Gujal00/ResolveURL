0aa576b60dce9a2c6384167afb85a570e02171e1
bassemhelal18 <77381611+bassemhelal18@users.noreply.github.com>
2025-12-23 07:19:57 +0200

    Add 'minochinos.com' and 'earnvids.xyz' to filelions plugin domains (#1266)
    * Add 'minochinos.com' and 'earnvids.xyz' to filelions plugin domains

--- script.module.resolveurl/lib/resolveurl/plugins/filelions.py
+++ script.module.resolveurl/lib/resolveurl/plugins/filelions.py
@@ -36,13 +36,14 @@ class FileLionsResolver(ResolveUrl):
         'katomen.online', 'vidhide.fun', 'vidhidehub.com', 'dhtpre.com', '6sfkrspw4u.sbs',
         'streamvid.su', 'movearnpre.com', 'bingezove.com', 'dingtezuni.com', 'dinisglows.com',
         'ryderjet.com', 'e4xb5c2xnz.sbs', 'smoothpre.com', 'videoland.sbs', 'taylorplayer.com',
-        'mivalyo.com', 'vidhidefast.com', 'peytonepre.com', 'dintezuvio.com', 'callistanise.com'
+        'mivalyo.com', 'vidhidefast.com', 'peytonepre.com', 'dintezuvio.com', 'callistanise.com',
+        'minochinos.com','earnvids.xyz'
     ]
     pattern = r'(?://|\.)((?:filelions|ajmidyadfihayh|alhayabambi|techradar|moflix-stream|azipcdn|' \
               r'[mad]lions|lumiawatch|javplaya|javlion|fviplions|egsyxutd|fdewsdc|vidhide|peytone|' \
               r'anime7u|coolciima|gsfomqu|katomen|dht|6sfkrspw4u|ryderjet|e4xb5c2xnz|smooth|' \
               r'streamvid|movearnpre|bingezove|dingtezuni|dinisglows|motvy55|videoland|mivalyo|' \
-              r'taylorplayer|dintezuvio|callistanise)(?:pro|vip|pre|plus|hub|fast)?' \
+              r'taylorplayer|dintezuvio|callistanise|minochinos|earnvids)(?:pro|vip|pre|plus|hub|fast)?' \
               r'\.(?:su|com?|to|sbs|ink|click|pro|live|store|xyz|top|online|site|fun))' \
               r'/(?:s|v|f|d|embed|file|download)/([0-9a-zA-Z$:/.]+)'
 
