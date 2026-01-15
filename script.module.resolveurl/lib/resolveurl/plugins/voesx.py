93fc5a8850fb283d46985f44db19c5a6332ab4cc
Gujal <17394059+Gujal00@users.noreply.github.com>
2026-01-15 15:02:44 +1300

    voe use only ff ua, Fixes #1289

--- script.module.resolveurl/lib/resolveurl/plugins/voesx.py
+++ script.module.resolveurl/lib/resolveurl/plugins/voesx.py
@@ -84,7 +84,7 @@ class VoeResolver(ResolveUrl):
 
     def get_media_url(self, host, media_id, subs=False):
         web_url = self.get_url(host, media_id)
-        headers = {'User-Agent': common.RAND_UA}
+        headers = {'User-Agent': common.FF_USER_AGENT}
         html = self.net.http_GET(web_url, headers=headers).content
         if 'const currentUrl' in html:
             r = re.search(r'''window\.location\.href\s*=\s*'([^']+)''', html)
