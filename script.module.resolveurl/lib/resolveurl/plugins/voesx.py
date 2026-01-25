afe46927e635c5e419f585eccc39096139f2d47b
Gujal <17394059+Gujal00@users.noreply.github.com>
2026-01-25 18:30:01 +1300

    voe additional domain, Fixes #1299

--- script.module.resolveurl/lib/resolveurl/plugins/voesx.py
+++ script.module.resolveurl/lib/resolveurl/plugins/voesx.py
@@ -54,7 +54,7 @@ class VoeResolver(ResolveUrl):
         'richardsignfish.com', 'jennifercertaindevelopment.com', 'diananatureforeign.com', 'goofy-banana.com',
         'mariatheserepublican.com', 'johnalwayssame.com', 'kellywhatcould.com', 'jilliandescribecompany.com',
         'lukesitturn.com', 'mikaylaarealike.com', 'christopheruntilpoint.com', 'walterprettytheir.com',
-        'crystaltreatmenteast.com', 'lauradaydo.com'
+        'crystaltreatmenteast.com', 'lauradaydo.com', 'smoki.cc'
     ]
     domains += ['voeunblock{}.com'.format(x) for x in range(1, 11)]
     pattern = r'(?://|\.)((?:audaciousdefaulthouse|launchreliantcleaverriver|kennethofficialitem|' \
@@ -79,8 +79,8 @@ class VoeResolver(ResolveUrl):
               r'adrianmissionminute|nathanfromsubject|richardsignfish|jennifercertaindevelopment|' \
               r'jonathansociallike|mariatheserepublican|johnalwayssame|jilliandescribecompany|' \
               r'lukesitturn|mikaylaarealike|christopheruntilpoint|walterprettytheir|crystaltreatmenteast|' \
-              r'lauradaydo|' \
-              r'(?:v-?o-?e)?(?:-?un-?bl[o0]?c?k\d{0,2})?(?:-?voe)?)\.(?:sx|com|net))/' \
+              r'lauradaydo|smoki|' \
+              r'(?:v-?o-?e)?(?:-?un-?bl[o0]?c?k\d{0,2})?(?:-?voe)?)\.(?:sx|com|net|cc))/' \
               r'(?:e/)?([0-9A-Za-z]+)'
 
     def get_media_url(self, host, media_id, subs=False):
