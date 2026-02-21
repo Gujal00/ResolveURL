586b809a85812a5eacdb479a04d649034a9e30f1
Gujal <17394059+Gujal00@users.noreply.github.com>
2026-02-21 19:56:07 +1300

    mixdrop additional domain

--- script.module.resolveurl/lib/resolveurl/plugins/mixdrop.py
+++ script.module.resolveurl/lib/resolveurl/plugins/mixdrop.py
@@ -31,7 +31,7 @@ class MixDropResolver(ResolveUrl):
         'md3b0j6hj.com', 'mdbekjwqa.pw', 'mdfx9dc8n.net', 'mixdropjmk.pw',
         'mixdrop21.net', 'mixdrop.is', 'mixdrop.si', 'mixdrop23.net', 'mixdrop.nu',
         'mixdrop.ms', 'mdzsmutpcvykb.net', 'mixdrop.ps', 'mxdrop.to', 'mixdrop.sb',
-        'mixdrop.my', 'm1xdrop.net'
+        'mixdrop.my', 'm1xdrop.net', 'm1xdrop.com'
     ]
     pattern = r'(?://|\.)((?:mi*1*xdro*p\d*(?:jmk)?|md(?:3b0j6hj|bekjwqa|fx9dc8n|y48tn97|zsmutpcvykb))\.' \
               r'(?:c[ho]m?|to|sx|bz|gl|club|vc|ag|pw|net|is|s[ib]|nu|m[sy]|ps))/(?:f|e)/(\w+)'
