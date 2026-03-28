e1b29de16a0fae58798707c16b800d43c42d2e1c
Gujal <17394059+Gujal00@users.noreply.github.com>
2026-03-29 09:33:45 +1300

    update gofile, Fixes #1352

--- script.module.resolveurl/lib/resolveurl/plugins/gofile.py
+++ script.module.resolveurl/lib/resolveurl/plugins/gofile.py
@@ -1,7 +1,6 @@
 """
     Plugin for ResolveURL
-    Copyright (C) 2022 shellc0de
-                  2024 peter3344
+    Copyright (C) 2026 gujal
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -17,7 +16,6 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
-import re
 import json
 from resolveurl.lib import helpers
 from resolveurl import common
@@ -30,29 +28,41 @@ class GoFileResolver(ResolveUrl):
     pattern = r'(?://|\.)(gofile\.io)/(?:\?c=|d/)([0-9a-zA-Z]+)'
 
     def get_media_url(self, host, media_id):
-        headers = {'User-Agent': common.FF_USER_AGENT,
-                   'Origin': 'https://{0}'.format(host),
-                   'Referer': 'https://{0}/'.format(host)
-                   }
+        headers = {
+            'User-Agent': common.FF_USER_AGENT,
+            'Origin': 'https://{0}'.format(host),
+            'Referer': 'https://{0}/'.format(host)
+        }
         base_api = 'https://api.gofile.io'
         r = self.net.http_POST('{}/accounts'.format(base_api), form_data="{}", headers=headers)
         token = json.loads(r.content).get('data', {}).get('token')
         if not token:
             raise ResolverError('Unable to retrieve token!')
 
-        r = self.net.http_GET('https://{}/dist/js/global.js'.format(host), headers=headers).content
-        wtoken = re.search(r'wt\s*=\s*"([^"]+)', r)
-        if not wtoken:
-            raise ResolverError('Unable to retrieve websiteToken!')
-
-        headers.update({'Authorization': 'Bearer ' + token})
-        content_url = '{}/contents/{}?wt={}&cache=true'.format(base_api, media_id, wtoken.group(1))
+        headers.update({
+            'Authorization': 'Bearer ' + token,
+            'x-website-token': self.__generateWT(token),
+            'x-bl': 'en-US'
+        })
+        content_url = '{}/contents/{}?page=1'.format(base_api, media_id)
         r = self.net.http_GET(content_url, headers=headers).content
         data = json.loads(r).get('data').get('children')
         if data:
-            headers.pop('Authorization')
-            headers.update({'Cookie': 'accountToken={}'.format(token)})
+            headers = {
+                'User-Agent': common.FF_USER_AGENT,
+                'Referer': 'https://{0}/'.format(host),
+                'Cookie': 'accountToken={}'.format(token)
+            }
             sources = [(data[x].get('size'), data[x].get('link')) for x in data]
             return helpers.pick_source(sources, False) + helpers.append_headers(headers)
 
-        raise ResolverError('This file does not exist.')
+        raise ResolverError('File does not exist.')
+
+    @staticmethod
+    def __generateWT(tok):
+        import math
+        import time
+        import hashlib
+        rtime = math.floor(time.time() / 14400)
+        wtok = '{0}::en-US::{1}::{2}::5d4f7g8sd45fsd'.format(common.FF_USER_AGENT, tok, rtime)
+        return hashlib.sha256(wtok.encode('latin-1')).hexdigest()
