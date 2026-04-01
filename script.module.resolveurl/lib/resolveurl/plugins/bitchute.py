1887f75adfc2c4fe232c18d855ad55b97bbe4bce
gujal <17394059+Gujal00@users.noreply.github.com>
2026-04-01 16:58:41 +1300

    Update bitchute.py

--- script.module.resolveurl/lib/resolveurl/plugins/bitchute.py
+++ script.module.resolveurl/lib/resolveurl/plugins/bitchute.py
@@ -17,32 +17,33 @@
 """
 
 import json
+from six.moves import urllib_parse
 from resolveurl import common
-from resolveurl.plugins.__resolve_generic__ import ResolveGeneric
+from resolveurl.lib import helpers
+from resolveurl.resolver import ResolveUrl, ResolverError
 
-api_url = 'https://api.bitchute.com/api/beta/video/media'
 
-
-class BitchuteResolver(ResolveGeneric):
+class BitchuteResolver(ResolveUrl):
 
     name = 'Bitchute'
     domains = ['bitchute.com']
     pattern = r'(?://|\.)(bitchute\.com)/(?:video|embed)/([\w-]+)/'
 
     def get_media_url(self, host, media_id):
-
+        api_url = 'https://api.bitchute.com/api/beta/video/media'
         payload = {"video_id": media_id}
-
+        ref = urllib_parse.urljoin(self.get_url(host, media_id), '/')
         headers = {
-            'User-Agent': common.RAND_UA, 'Content-Type': 'application/json',
-            'Origin': 'https://www.bitchute.com', 'Referer': 'https://www.bitchute.com/'
+            'User-Agent': common.RAND_UA,
+            'Origin': ref[:-1],
+            'Referer': ref
         }
-
         res = self.net.http_POST(api_url, form_data=payload, headers=headers, jdata=True).content
+        if res:
+            video = json.loads(res).get('media_url')
+            return video + helpers.append_headers(headers)
 
-        video = json.loads(res)
-
-        return video.get('media_url')
+        raise ResolverError('Video Link Not Found')
 
     def get_url(self, host, media_id):
         return self._default_get_url(host, media_id, 'https://www.{host}/video/{media_id}')
