a8c0cc50d9e9efa4220c594f30ea2f56a5f9fae1
Twilight0 <twilight0@vivaldi.net>
2026-04-15 22:57:21 +0300

    Streamruby additional domain

--- script.module.resolveurl/lib/resolveurl/plugins/streamruby.py
+++ script.module.resolveurl/lib/resolveurl/plugins/streamruby.py
@@ -24,10 +24,14 @@ from resolveurl.resolver import ResolveUrl, ResolverError
 
 class StreamRubyResolver(ResolveUrl):
     name = 'StreamRuby'
-    domains = ['streamruby.com', 'sruby.xyz', 'rubystream.xyz', 'tuktukcimamulti.buzz',
-               'stmruby.com', 'rubystm.com', 'rubyvid.com', 'kinoger.be']
-    pattern = r'(?://|\.)((?:s?(?:tream|tm)?ruby(?:stream|stm|vid)?|kinoger|tuktukcimamulti)\.' \
-              r'(?:com|xyz|buzz|be))/(?:embed-|e/|d/)?(\w+)'
+    domains = [
+        'streamruby.com', 'sruby.xyz', 'rubystream.xyz', 'tuktukcimamulti.buzz',
+        'stmruby.com', 'rubystm.com', 'rubyvid.com', 'kinoger.be', 'rubyvidhub.com'
+    ]
+    pattern = (
+        r'(?://|\.)((?:s?(?:tream|tm)?ruby(?:stream|stm|vid(?:hub)?)?|kinoger|tuktukcimamulti)'
+        r'\.(?:com|xyz|buzz|be))/(?:embed-|e/|d/)?(\w+)'
+    )
 
     def get_media_url(self, host, media_id, subs=False):
         web_url = self.get_url(host, media_id)
