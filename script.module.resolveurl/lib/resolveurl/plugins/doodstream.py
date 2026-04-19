c005507afbc0b21451d9aa7eb4613eba24924084
Gujal <17394059+Gujal00@users.noreply.github.com>
2026-04-19 18:18:26 +1200

    ByParr Support

--- script.module.resolveurl/lib/resolveurl/plugins/doodstream.py
+++ script.module.resolveurl/lib/resolveurl/plugins/doodstream.py
@@ -16,6 +16,7 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
+import json
 import re
 import random
 import string
@@ -34,58 +35,69 @@ class DoodStreamResolver(ResolveUrl):
         'dood.stream', 'ds2play.com', 'doods.pro', 'ds2video.com', 'd0o0d.com', 'do0od.com',
         'd0000d.com', 'd000d.com', 'dood.li', 'dood.work', 'dooodster.com', 'vidply.com',
         'all3do.com', 'do7go.com', 'doodcdn.io', 'doply.net', 'vide0.net', 'vvide0.com',
-        'd-s.io', 'dsvplay.com', 'myvidplay.com'
+        'd-s.io', 'dsvplay.com', 'myvidplay.com', 'playmogo.com'
     ]
-    pattern = r'(?://|\.)((?:do*0*o*0*ds?(?:tream|ter|cdn)?|ds[2v](?:play|video)|(?:my)?v*id(?:pla?y|e0)|all3do|d-s|do(?:7go|ply))\.' \
-              r'(?:[cit]om?|watch|s[ho]|cx|l[ai]|w[sf]|pm|re|yt|stream|pro|work|net))/(?:d|e)/([0-9a-zA-Z]+)'
+    pattern = (
+        r'(?://|\.)((?:do*0*o*0*ds?(?:tream|ter|cdn)?|ds[2v](?:play|video)|(?:my)?v*id(?:pla?y|e0)|all3do|'
+        r'd-s|do(?:7go|ply)|playmogo)\.'
+        r'(?:[cit]om?|watch|s[ho]|cx|l[ai]|w[sf]|pm|re|yt|stream|pro|work|net))/(?:d|e)/([0-9a-zA-Z]+)'
+    )
 
     def get_media_url(self, host, media_id, subs=False):
-        if host not in ['doodstream.com', 'myvidplay.com']:
-            host = 'myvidplay.com'
+        if not common.BP_ENABLED:
+            raise ResolverError('BYPARR not available')
+
+        if host not in ['doodstream.com', 'myvidplay.com', 'playmogo.com']:
+            host = 'playmogo.com'
         web_url = self.get_url(host, media_id)
-        headers = {'User-Agent': common.FF_USER_AGENT,
-                   'Referer': 'https://{0}/'.format(host)}
+        bp_url = urllib_parse.urljoin(common.BP_URL, '/v1')
+        data = {
+            "cmd": "request.get",
+            "url": web_url,
+            "maxTimeout": common.BP_TIMEOUT * 1000
+        }
+        r = self.net.http_POST(bp_url, form_data=data, jdata=True, timeout=common.BP_TIMEOUT + 20).content
+        r = json.loads(r)
+        if r.get('message') == 'Success':
+            r = r.get('solution')
+            html = r.get('response')
+            if r.get('url') != web_url:
+                web_url = r.get('url')
 
-        r = self.net.http_GET(web_url, headers=headers)
-        if r.get_url() != web_url:
-            host = re.findall(r'(?://|\.)([^/]+)', r.get_url())[0]
-            web_url = self.get_url(host, media_id)
-        headers.update({'Referer': web_url})
-        html = r.content
+            headers = {
+                'User-Agent': common.FF_USER_AGENT,
+                'Referer': web_url}
 
-        match = re.search(r'<iframe\s*src="([^"]+)', html)
-        if match:
-            url = urllib_parse.urljoin(web_url, match.group(1))
-            html = self.net.http_GET(url, headers=headers).content
-        else:
-            url = web_url.replace('/d/', '/e/')
-            html = self.net.http_GET(url, headers=headers).content
-
-        if subs:
-            subtitles = {}
-            matches = re.findall(r"""dsplayer\.addRemoteTextTrack\({src:'([^']+)',\s*label:'([^']*)',kind:'captions'""", html)
-            if matches:
-                matches = [(src, label) for src, label in matches if len(label) > 1]
-                for src, label in matches:
-                    subtitles[label] = 'https:' + src if src.startswith('//') else src
-
-        match = re.search(r'''dsplayer\.hotkeys[^']+'([^']+).+?function\s*makePlay.+?return[^?]+([^"]+)''', html, re.DOTALL)
-        if match:
-            token = match.group(2)
-            url = urllib_parse.urljoin(web_url, match.group(1))
-            html = self.net.http_GET(url, headers=headers).content
-            if 'cloudflarestorage.' in html:
-                vid_src = html.strip() + helpers.append_headers(headers)
-            else:
-                vid_src = self.dood_decode(html) + token + str(int(time.time() * 1000)) + helpers.append_headers(headers)
             if subs:
-                return vid_src, subtitles
-            return vid_src
+                subtitles = {}
+                matches = re.findall(r"""dsplayer\.addRemoteTextTrack\({src:'([^']+)',\s*label:'([^']*)',kind:'captions'""", html)
+                if matches:
+                    matches = [(src, label) for src, label in matches if len(label) > 1]
+                    for src, label in matches:
+                        subtitles[label] = 'https:' + src if src.startswith('//') else src
+
+            match = re.search(r'''dsplayer\.hotkeys[^']+'([^']+).+?function\s*makePlay.+?return[^?]+([^"]+)''', html, re.DOTALL)
+            if match:
+                token = match.group(2).strip()
+                url = urllib_parse.urljoin(web_url, match.group(1))
+                data.update({'url': url})
+                resp = self.net.http_POST(bp_url, form_data=data, jdata=True, timeout=common.BP_TIMEOUT + 20).content
+                resp = json.loads(resp)
+                if resp.get('message') == 'Success':
+                    resp = resp.get('solution')
+                    str_url = re.findall(r'<body>([^<]+)', resp.get('response'))[0]
+                    if 'cloudflarestorage.' in str_url:
+                        vid_src = str_url + helpers.append_headers(headers)
+                    else:
+                        vid_src = self.dood_decode(str_url) + token + str(int(time.time() * 1000)) + helpers.append_headers(headers)
+                    if subs:
+                        return vid_src, subtitles
+                    return vid_src
 
         raise ResolverError('Video Link Not Found')
 
     def get_url(self, host, media_id):
-        return self._default_get_url(host, media_id, template='https://{host}/d/{media_id}')
+        return self._default_get_url(host, media_id, template='https://{host}/e/{media_id}')
 
     def dood_decode(self, data):
         t = string.ascii_letters + string.digits
