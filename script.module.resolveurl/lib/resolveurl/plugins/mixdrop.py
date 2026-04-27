ade4ba722e1efdd6b4a131a8b03677eb8f00406e
Gujal <17394059+Gujal00@users.noreply.github.com>
2026-04-28 11:31:10 +1200

    mixdrop additional domains, Fixes #1381

--- script.module.resolveurl/lib/resolveurl/plugins/mixdrop.py
+++ script.module.resolveurl/lib/resolveurl/plugins/mixdrop.py
@@ -31,10 +31,13 @@ class MixDropResolver(ResolveUrl):
         'md3b0j6hj.com', 'mdbekjwqa.pw', 'mdfx9dc8n.net', 'mixdropjmk.pw',
         'mixdrop21.net', 'mixdrop.is', 'mixdrop.si', 'mixdrop23.net', 'mixdrop.nu',
         'mixdrop.ms', 'mdzsmutpcvykb.net', 'mixdrop.ps', 'mxdrop.to', 'mixdrop.sb',
-        'mixdrop.my', 'm1xdrop.net', 'm1xdrop.com'
+        'mixdrop.my', 'm1xdrop.net', 'm1xdrop.com', 'm1xdrop.click', 'mxdrop.sx',
+        'mixdrop.top', 'mixdrp.click'
     ]
-    pattern = r'(?://|\.)((?:mi*1*xdro*p\d*(?:jmk)?|md(?:3b0j6hj|bekjwqa|fx9dc8n|y48tn97|zsmutpcvykb))\.' \
-              r'(?:c[ho]m?|to|sx|bz|gl|club|vc|ag|pw|net|is|s[ib]|nu|m[sy]|ps))/(?:f|e)/(\w+)'
+    pattern = (
+        r'(?://|\.)((?:mi*1*xdro*p\d*(?:jmk)?|md(?:3b0j6hj|bekjwqa|fx9dc8n|y48tn97|zsmutpcvykb))\.'
+        r'(?:c[ho]m?|top?|bz|gl|club|click|vc|ag|pw|net|is|s[ibx]|nu|m[sy]|ps))/(?:f|e)/(\w+)'
+    )
 
     def get_media_url(self, host, media_id):
         if host.endswith('.club'):
