0021036c8714b30ca8ddb86389155762de9d3ce0
icarok99-alt <icarok00@gmail.com>
2026-06-02 12:53:44 -0300

    New resolve flyfile

---
+++ script.module.resolveurl/lib/resolveurl/plugins/flyfile.py
@@ -0,0 +1,96 @@
+"""
+    Plugin for ResolveURL
+    Copyright (C) 2026 icarok99
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+import re
+import json
+from resolveurl import common
+from resolveurl.lib import helpers
+from resolveurl.resolver import ResolveUrl, ResolverError
+
+
+class FlyFileResolver(ResolveUrl):
+    name = 'FlyFile'
+    domains = ['flyfile.app']
+    pattern = r'(?://|\.)(flyfile\.app)/embed/([A-Za-z0-9]+)'
+
+    def get_media_url(self, host, media_id, subs=False):
+        headers = {
+            'User-Agent': common.FF_USER_AGENT,
+            'Accept': 'application/json, text/plain, */*',
+            'Origin': 'https://flyfile.app',
+            'Referer': 'https://flyfile.app/',
+            'X-Adblock-Detected': '1',
+            'X-Embed-Referrer': '',
+        }
+
+        file_numeric_id = None
+        if subs:
+            try:
+                signal_url = 'https://api.flyfile.app/api/stats/adblock-signal'
+                signal_resp = self.net.http_GET(signal_url, headers=headers).content
+                signal_data = json.loads(signal_resp)
+                file_numeric_id = (signal_data.get('fileid')
+                                   or signal_data.get('fileId')
+                                   or signal_data.get('id'))
+            except Exception:
+                pass
+
+        api_url = 'https://api.flyfile.app/api/streaming/assign/{0}'.format(media_id)
+        resp = self.net.http_GET(api_url, headers=headers).content
+        data = json.loads(resp)
+
+        if not data.get('url') or not data.get('token'):
+            raise ResolverError('FlyFile: missing url or token in response')
+
+        stream_url = '{0}/hls/{1}/master.m3u8'.format(
+            data['url'].rstrip('/'),
+            data['token']
+        )
+
+        stream_headers = {
+            'User-Agent': common.FF_USER_AGENT,
+            'Referer': 'https://flyfile.app/',
+            'Origin': 'https://flyfile.app',
+        }
+        final_url = stream_url + helpers.append_headers(stream_headers)
+
+        if subs:
+            subtitles = self._get_subtitles(file_numeric_id, headers) if file_numeric_id else {}
+            return final_url, subtitles
+
+        return final_url
+
+    def _get_subtitles(self, file_numeric_id, headers):
+        subtitles = {}
+        try:
+            subs_url = 'https://api.flyfile.app/api/public/subtitles/{0}'.format(file_numeric_id)
+            resp = self.net.http_GET(subs_url, headers=headers).content
+            data = json.loads(resp)
+            sub_list = data if isinstance(data, list) else data.get('subtitles', [])
+            for item in sub_list:
+                lang = item.get('lang') or item.get('language', '')
+                label = item.get('label') or item.get('name') or lang
+                url = item.get('url') or item.get('src', '')
+                if label and url:
+                    subtitles[label] = url
+        except Exception:
+            pass
+        return subtitles
+
+    def get_url(self, host, media_id):
+        return self._default_get_url(host, media_id, template='https://{host}/embed/{media_id}')
