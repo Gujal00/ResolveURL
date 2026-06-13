ccc98561a8966c91315566a66bf0b0646d5ee350
Gujal <17394059+Gujal00@users.noreply.github.com>
2026-06-14 01:02:30 +1200

    fix crash when resolving magnet and no mimetype

--- plugin.video.smr_link_tester/default.py
+++ plugin.video.smr_link_tester/default.py
@@ -255,6 +255,7 @@ def play_link(link):
 
     try:
         subs = {}
+        mimetype = ''
         if link.endswith('$$all'):
             allfiles = hmf.resolve()
             names = [x.get('name') for x in allfiles]
@@ -264,7 +265,6 @@ def play_link(link):
             stream_url = allfiles[item].get('link')
             if resolveurl.HostedMediaFile(stream_url):
                 stream_url = resolveurl.resolve(stream_url)
-                mimetype = ''
         else:
             resp = hmf.resolve()
             if resp:
