54c07ea99e74429334c002fd8ad2f14f412603ac
icarok99-alt <icarok00@gmail.com>
2026-06-18 13:33:52 -0300

    Fix uqload

--- script.module.resolveurl/lib/resolveurl/plugins/uqload.py
+++ script.module.resolveurl/lib/resolveurl/plugins/uqload.py
@@ -16,8 +16,9 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
+import re
 from resolveurl.plugins.__resolve_generic__ import ResolveGeneric
-from resolveurl.lib import helpers
+from resolveurl.lib import jsunpack
 
 
 class UQLoadResolver(ResolveGeneric):
@@ -30,11 +31,20 @@ class UQLoadResolver(ResolveGeneric):
     pattern = r'(?://|\.)(uqload\.(?:[ict]om?|[iw]s|net|cx|bz|org))/(?:embed-)?([0-9a-zA-Z]+)'
 
     def get_media_url(self, host, media_id):
-        return helpers.get_media_url(
-            self.get_url(host, media_id),
-            patterns=[r'''sources:\s*\[['"](?P<url>[^'"]+)'''],
-            referer=False
+        web_url = self.get_url(host, media_id)
+        html = self.net.http_GET(web_url).content
+
+        if jsunpack.detect(html):
+            html = jsunpack.unpack(html)
+
+        match = re.search(
+            r'sources\s*:\s*\[\s*\{\s*file\s*[:\s\'"]+(?P<url>https?://[^\'\"]+\.m3u8[^\'\"]*)',
+            html
         )
+        if match:
+            return match.group('url')
+
+        raise self.ResolveException('Could not locate stream URL on {0}'.format(web_url))
 
     def get_url(self, host, media_id):
         return self._default_get_url(host, media_id, 'https://uqload.is/embed-{media_id}.html')
