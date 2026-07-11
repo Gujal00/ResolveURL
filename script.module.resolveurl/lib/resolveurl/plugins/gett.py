ed10775f99c7268edadd8e8c6b095d0eb8e1568b
Gujal <17394059+Gujal00@users.noreply.github.com>
2026-07-12 11:56:02 +1200

    r

--- script.module.resolveurl/lib/resolveurl/plugins/gett.py
+++ script.module.resolveurl/lib/resolveurl/plugins/gett.py
@@ -33,6 +33,5 @@ class GettResolver(ResolveGeneric):
             referer=False
         )
 
-
     def get_url(self, host, media_id):
         return self._default_get_url(host, media_id, template='http://{host}/file/{media_id}/embed.html')
