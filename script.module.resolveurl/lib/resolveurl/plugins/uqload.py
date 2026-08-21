6611b6b4464223eda4c4f701537906735ff9ead3
Gujal <17394059+Gujal00@users.noreply.github.com>
2026-08-21 23:46:48 +1200

    uqload additional embed pattern, Fixes #1466

--- script.module.resolveurl/lib/resolveurl/plugins/uqload.py
+++ script.module.resolveurl/lib/resolveurl/plugins/uqload.py
@@ -25,9 +25,9 @@ class UQLoadResolver(ResolveGeneric):
     domains = [
         'uqload.com', 'uqload.co', 'uqload.io', 'uqload.to',
         'uqload.ws', 'uqload.net', 'uqload.cx', 'uqload.bz',
-        'uqload.org', 'uqload.is'
+        'uqload.org', 'uqload.is', 'uqload.vc'
     ]
-    pattern = r'(?://|\.)(uqload\.(?:[ict]om?|[iw]s|net|cx|bz|org))/(?:embed-)?([0-9a-zA-Z]+)'
+    pattern = r'(?://|\.)(uqload\.(?:[ict]om?|[iw]s|net|cx|bz|org|vc))/(?:e/|embed-)?([0-9a-zA-Z]+)'
 
     def get_media_url(self, host, media_id):
         return helpers.get_media_url(
@@ -37,4 +37,4 @@ class UQLoadResolver(ResolveGeneric):
         )
 
     def get_url(self, host, media_id):
-        return self._default_get_url(host, media_id, 'https://uqload.is/embed-{media_id}.html')
+        return self._default_get_url(host, media_id, 'https://uqload.vc/embed-{media_id}.html')
