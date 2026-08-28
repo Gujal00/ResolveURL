18885f6bbfad8d4049107cc22b63c88207bde71a
TempleLain <56979828+TempleLain@users.noreply.github.com>
2026-08-28 15:56:58 +0200

    vidsonic add vixeo.io and new player config format

--- script.module.resolveurl/lib/resolveurl/plugins/vidsonic.py
+++ script.module.resolveurl/lib/resolveurl/plugins/vidsonic.py
@@ -16,7 +16,9 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
+import base64
 import binascii
+import json
 import re
 from six.moves import urllib_parse
 from resolveurl.lib import helpers
@@ -26,8 +28,8 @@ from resolveurl.resolver import ResolveUrl, ResolverError
 
 class VidSonicResolver(ResolveUrl):
     name = 'VidSonic'
-    domains = ['vidsonic.net']
-    pattern = r'(?://|\.)(vidsonic\.net)/(?:e|d)/([0-9a-zA-Z]+)'
+    domains = ['vidsonic.net', 'vixeo.io']
+    pattern = r'(?://|\.)(vidsonic\.net|vixeo\.io)/(?:e|d)/([0-9a-zA-Z]+)'
 
     def get_media_url(self, host, media_id):
         web_url = self.get_url(host, media_id)
@@ -38,9 +40,20 @@ class VidSonicResolver(ResolveUrl):
             'Origin': ref[:-1]
         }
         html = self.net.http_GET(web_url, headers=headers).content
+        src = ''
         r = re.search(r"const\s*_0x1\s*=\s*'([^']+)", html)
         if r:
-            src = binascii.unhexlify(r.group(1).replace('|', '')).decode()[::-1]
+            src = r.group(1)
+        else:
+            # newer player builds carry the same reversed hex string in a
+            # base64 encoded json blob on the player root element instead
+            r = re.search(r'data-config="([^"]+)"', html)
+            if r:
+                blob = r.group(1)
+                cfg = json.loads(base64.b64decode(blob + '=' * (-len(blob) % 4)))
+                src = cfg.get('source', '')
+        if src:
+            src = binascii.unhexlify(src.replace('|', '')).decode()[::-1]
             return src + helpers.append_headers(headers)
         raise ResolverError('Video Link Not Found')
 
