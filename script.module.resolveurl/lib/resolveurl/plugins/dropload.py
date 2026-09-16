8a453bb6d290b9b0bc6b4d83c0d846ff7e32fdcf
TempleLain <56979828+TempleLain@users.noreply.github.com>
2026-09-16 07:19:09 +0200

    2Captcha Implementation

--- script.module.resolveurl/lib/resolveurl/plugins/dropload.py
+++ script.module.resolveurl/lib/resolveurl/plugins/dropload.py
@@ -16,8 +16,14 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
-from resolveurl.plugins.__resolve_generic__ import ResolveGeneric
+import re
+from six.moves import urllib_parse
+
+from resolveurl import common
+from resolveurl.lib import captcha_lib
 from resolveurl.lib import helpers
+from resolveurl.plugins.__resolve_generic__ import ResolveGeneric
+from resolveurl.resolver import ResolverError
 
 
 class DropLoadResolver(ResolveGeneric):
@@ -26,12 +32,38 @@ class DropLoadResolver(ResolveGeneric):
     pattern = r'(?://|\.)(dr[0o]p(?:load|stream)\.(?:io|tv|com?|pro))/(?:embed-|e/|d/)?([0-9a-zA-Z]+)'
 
     def get_media_url(self, host, media_id):
-        return helpers.get_media_url(
-            self.get_url(host, media_id),
+        web_url = self.get_url(host, media_id)
+        headers = {'User-Agent': common.RAND_UA}
+        response = self.net.http_GET(web_url, headers=headers)
+        html = response.content
+        # the embed page is gated by a Cloudflare Turnstile widget (reCAPTCHA
+        # compatibility mode) whose form posts back to the embed page;
+        # the player is only served after a valid token has been posted
+        sitekey = re.search(r'''class=["']g-recaptcha["'][^>]+data-sitekey=["']([^"']+)''', html)
+        if sitekey:
+            web_url = response.get_url()  # secondary domains redirect to the main one
+            captcha = captcha_lib.do_turnstile(sitekey.group(1), web_url)
+            if not captcha:
+                raise ResolverError('DropLoad requires a 2Captcha API key, see ResolveURL settings')
+            data = helpers.get_hidden(html)
+            data.update(captcha)
+            rurl = urllib_parse.urljoin(web_url, '/')
+            headers.update({'Referer': web_url, 'Origin': rurl[:-1]})
+            html = self.net.http_POST(web_url, form_data=data, headers=headers).content
+
+        rurl = urllib_parse.urljoin(web_url, '/')
+        headers.update({'Referer': rurl, 'Origin': rurl[:-1]})
+        sources = helpers.scrape_sources(
+            html,
             patterns=[r'''sources:\s*\[{\s*file:\s*["'](?P<url>[^"']+)'''],
             generic_patterns=False,
-            referer=False
+            url=web_url
         )
+        if sources:
+            source = helpers.pick_source(sources)
+            return urllib_parse.quote(source, '/:?=&!') + helpers.append_headers(headers)
+
+        raise ResolverError('Unable to locate stream URL.')
 
     def get_url(self, host, media_id):
         return self._default_get_url(host, media_id, template='https://{host}/e/{media_id}')
