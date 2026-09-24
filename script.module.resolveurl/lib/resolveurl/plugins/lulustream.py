1b0059b6d129d623c21203da0311945eecda8805
TempleLain <56979828+TempleLain@users.noreply.github.com>
2026-09-24 21:09:43 +0200

    lulustream additional domain

--- script.module.resolveurl/lib/resolveurl/plugins/lulustream.py
+++ script.module.resolveurl/lib/resolveurl/plugins/lulustream.py
@@ -24,9 +24,9 @@ class LuluStreamResolver(ResolveGeneric):
     name = 'LuluStream'
     domains = [
         'lulustream.com', 'luluvdo.com', 'lulu.st', 'luluvid.com', '732eg54de642sa.sbs',
-        'cdn1.site', 'streamhihi.com', 'luluvdoo.com', 'd00ds.site'
+        'cdn1.site', 'streamhihi.com', 'luluvdoo.com', 'd00ds.site', 'lulust.com'
     ]
-    pattern = r'(?://|\.)((?:lulu(?:stream|vi*do*)?|732eg54de642sa|cdn1|streamhihi|d00ds)\.(?:com|sbs|si?te?))/(?:e/|d/)?([0-9a-zA-Z]+)'
+    pattern = r'(?://|\.)((?:lulu(?:st(?:ream)?|vi*do*)?|732eg54de642sa|cdn1|streamhihi|d00ds)\.(?:com|sbs|si?te?))/(?:e/|d/)?([0-9a-zA-Z]+)'
 
     def get_media_url(self, host, media_id, subs=False):
         return helpers.get_media_url(
